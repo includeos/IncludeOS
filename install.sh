b6c05ee7ccdfa6df4c89293f73043158d24bd20f
Alfred Bratterud <alfred.bratterud@hioa.no>
2014-09-11 10:01:41 +0200

    Automated (I hope) installation

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 #Enough stuff to create a basic image and run it
 sudo apt-get install -y gcc g++ build-essential emacs qemu-kvm make nasm
 
+echo -e ">>> BUILDING CROSS COMPILER \n"
+./etc/cross_compiler.sh
+
+echo -e ">>> BUILDING NEWLIB \n"
+./etc/bulid_newlib.sh
