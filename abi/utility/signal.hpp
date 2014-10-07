38e8093737e1bfe9fff09e37a073dc0ec71fb5b6
fwsgonzo <fwsgonzo@hotmail.com>
2014-10-07 15:22:05 +0200

    removed namespace from signal

--- abi/utility/signal.hpp
+++ abi/utility/signal.hpp
@@ -1,32 +1,29 @@
-#ifndef STD_SIGNAL_HPP
-#define STD_SIGNAL_HPP
+#ifndef OSABI_SIGNAL_HPP
+#define OSABI_SIGNAL_HPP
 
 #include <functional>
 #include <vector>
 
-namespace std
+template<typename F>
+class signal
 {
-	template<typename F>
-	class signal
+public:
+	//! \brief Connect a function to this signal
+	void connect(function<F> fn)
 	{
-	public:
-		//! \brief Connect a function to this signal
-		void connect(function<F> fn)
-		{
-			funcs.push_back(fn);
-		}
-		
-		//! \brief Emit this signal by executing all the connected functions
-		template<typename... Args>
-		void emit(Args... args)
-		{
-			for(auto fn : funcs)
-				fn(args...);
-		}
-		
-	private:
-		vector<function<F>> funcs;
-	};
-}
+		funcs.push_back(fn);
+	}
+	
+	//! \brief Emit this signal by executing all the connected functions
+	template<typename... Args>
+	void emit(Args... args)
+	{
+		for(auto fn : funcs)
+			fn(args...);
+	}
+	
+private:
+	vector<function<F>> funcs;
+};
 
 #endif
