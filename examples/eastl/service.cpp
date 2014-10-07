bce8aa0a9ba394bc9f7b4152d25599e9ae8d2f5d
fwsgonzo <fwsgonzo@hotmail.com>
2014-10-07 15:33:48 +0200

    enable_if

--- examples/eastl/service.cpp
+++ examples/eastl/service.cpp
@@ -66,7 +66,7 @@ public:
 	signal<void()> test;
 };
 
-//#include <delegate>
+#include <delegate>
 
 void Service::start()
 {
--- stdlib/type_traits.hpp
+++ stdlib/type_traits.hpp
@@ -51,6 +51,13 @@ namespace std
 	template <typename T>
 	typename add_rvalue_reference<T>::type declval();
 	
+	// http://en.cppreference.com/w/cpp/types/enable_if
+	template<bool B, class T = void>
+	struct enable_if {};
+	
+	template<class T>
+	struct enable_if<true, T> { typedef T type; };
+	
 }
 
 #include "forward.hpp"
