3f60efdcf88287b73a7806c2f0b5790c865fda6d
Alfred Bratterud <alfred.bratterud@hioa.no>
2014-10-20 12:30:53 +0200

    cleanup

--- src/net/class_ip4.cpp
+++ src/net/class_ip4.cpp
@@ -32,33 +32,6 @@ int IP4::bottom(uint8_t* data, int len){
 
 
 
-unsigned short checksum1(const char *buf, unsigned size)
-{
-  unsigned sum = 0;
-  unsigned int i;
-
-  /* Accumulate checksum */
-  for (i = 0; i < size - 1; i += 2)
-    {
-      unsigned short word16 = *(unsigned short *) &buf[i];
-      sum += word16;
-    }
-
-  /* Handle odd-sized case */
-  if (size & 1)
-    {
-      unsigned short word16 = (unsigned char) buf[i];
-      sum += word16;
-    }
-
-  /* Fold to get the ones-complement result */
-  while (sum >> 16) sum = (sum & 0xFFFF)+(sum >> 16);
-
-  /* Invert to get the negative in ones-complement arithmetic */
-  return ~sum;
-}
-
-
 uint16_t IP4::checksum(ip_header* hdr){
   
   union sum{
