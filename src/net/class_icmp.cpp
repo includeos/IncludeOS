d9be880e70ebabc20224e19fc55cb9514343a309
Alfred Bratterud <alfred.bratterud@hioa.no>
2014-10-24 11:47:39 +0200

    Now answers to Ping (close #51) + moved checksum to inet.cpp

--- src/net/class_icmp.cpp
+++ src/net/class_icmp.cpp
@@ -1,5 +1,6 @@
 #define DEBUG
 #include <os>
+#include <net/inet.hpp>
 #include <net/class_icmp.hpp>
 
 using namespace net;
@@ -28,16 +29,21 @@ int ICMP::bottom(uint8_t* data, int len){
 
 void ICMP::ping_reply(full_header* full_hdr){
   // @todo We can't just assume the last one has been sent by now... but we do.
+  /*
   if (buf)
-    free(buf);
+  free(buf);*/
   
   buf = (uint8_t*)malloc(sizeof(full_header));
   memset(buf,0,sizeof(full_header));
   
-  icmp_header* hdr = (icmp_header*)buf;
-  hdr->type = ICMP_ECHO_REPLY;
-  
-  hdr->checksum = 0xffff;
+  icmp_header* hdr = &((full_header*)buf)->icmp_hdr;
+  hdr->type = ICMP_ECHO_REPLY;  
+  hdr->rest = full_hdr->icmp_hdr.rest;
+  hdr->checksum = 0;
+  hdr->checksum = net::checksum((uint16_t*)hdr,sizeof(icmp_header));
+
+  debug("<ICMP> Rest of header IN: 0x%lx OUT: 0x%lx \n",
+        full_hdr->icmp_hdr.rest, hdr->rest);
   
   debug("<ICMP> Transmitting answer\n");
   _network_layer_out(full_hdr->ip_hdr.daddr,full_hdr->ip_hdr.saddr,
