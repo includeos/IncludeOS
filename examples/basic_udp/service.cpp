d6ba6289bbec668df0089c2d1d0536da0cd58c10
Alfred Bratterud <alfred.bratterud@hioa.no>
2014-10-30 18:13:24 +0100

    Updated UDP example

--- examples/basic_udp/service.cpp
+++ examples/basic_udp/service.cpp
@@ -11,21 +11,74 @@ uint8_t* buf = 0;
 int bufsize = 0;
 uint8_t* prev_data = 0;
 
+class global {
+  static int i;
+public:
+  global(){
+    printf("GLOBAL CONSTRUCTOR IN SERVICE %i \n",++i);
+  }
+  
+  void test(){
+    printf("I have %i instances \n",i);
+  }
+  
+  int instances(){ return i; }
+};
+
+int global::i = 0;
+
+global glob1;
+
+int _test_glob2 = 1;
+
+__attribute__ ((constructor)) void foo(void)
+{
+  printf("foo is running and printf is available at this point\n");
+}
+
+/* @todo - make configuration happen here
+void Service::init(){
+
+  
+  Inet net;
+  auto eth0 = Dev::eth(0);
+  auto mac = eth0.mac();
+  
+  net.ifconfig(ETH0,{192,168,mac.part[4],mac.part[5]},{255,255,0,0});
+  //net.ifconfig(ETH0,{192,168,0,10}); => netmask == 255.255.255.0.
+  //net.route("*",{192.168.0.1});
+  
+  
+  Dev::eth(ETH0)
+        
+  //Dev::eth(1).dhclient();
+  
+  }*/
+
+
 
 void Service::start()
 {
 
 
+
+  assert(_test_glob2 == 1);
+  
   cout << "*** Service is up - with OS Included! ***" << endl;    
+  //global glob2;
+  //global glob3;
+  glob1.test();
+  assert(glob1.instances() == 1);
   
   auto& mac = Dev::eth(0).mac();
-  IP_stack::ifconfig(net::ETH0,{192,168,mac.part[4],mac.part[5]},{255,255,0,0});
+  Inet::ifconfig(net::ETH0,{10,0,mac.part[4],mac.part[5]},{255,255,0,0});
   
   /** Trying to access non-existing nic will cause a panic */
   //auto& mac1 = Dev::eth(1).mac();
-  //IP_stack::ifconfig(net::ETH1,{192,168,mac1.part[4],mac1.part[5]},{255,255,0,0});
+  //Inet::ifconfig(net::ETH1,{192,168,mac1.part[4],mac1.part[5]},{255,255,0,0});
   
-  shared_ptr<IP_stack> net(IP_stack::up());
+  //Inet* net 
+  shared_ptr<Inet> net(Inet::up());
   
 
   cout << "...Starting UDP server on IP " 
@@ -41,7 +94,12 @@ void Service::start()
 
       int data_len = __builtin_bswap16(hdr->length) - sizeof(UDP::udp_header);
       auto data_loc = data + sizeof(UDP::full_header);
-            
+      
+      // Netcat doesn't necessariliy zero-pad the string in UDP
+      // ... But this buffer is const
+      // auto data_end = data + hdr->length - sizeof(UDP::udp_header);
+      // *data_end = 0; 
+      
       debug("<APP SERVER> Got %i b of data (%i b frame) from %s:%i -> %s:%i\n",
             data_len, len, full_hdr->ip_hdr.saddr.str().c_str(), 
             __builtin_bswap16(hdr->sport),
@@ -81,7 +139,9 @@ void Service::start()
       return 0;
     });
   
-  cout << "<APP SERVER> Listening to UDP port 8080 " << endl;  
+  cout << "<APP SERVER> Listening to UDP port 8080 " << endl;
+  
+  // Hook up to I/O events and do something useful ...
   
   cout << "Service out! " << endl; 
 }
