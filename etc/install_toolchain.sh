6572cab551a6322b28425980c89c32f0cdc62c9a
Hårek Haugerud <you@example.com>
2014-11-28 11:33:35 +0100

    Added apt-get bridge-utils

--- etc/install_toolchain.sh
+++ etc/install_toolchain.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Enough stuff to create a basic image and run it
-sudo apt-get install -y gcc g++ build-essential emacs qemu-kvm make nasm
+sudo apt-get install -y gcc g++ build-essential emacs qemu-kvm make nasm bridge-utils 
 
 echo -e "\n\n>>> BUILDING CROSS COMPILER \n"
 sudo ./etc/cross_compiler.sh
