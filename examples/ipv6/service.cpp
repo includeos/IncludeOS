4890472a168ce55108967700ef7a77d6a5f4a79a
fwsgonzo <fwsgonzo@hotmail.com>
2015-08-14 15:11:10 +0200

    Reduced test case

--- examples/ipv6/service.cpp
+++ examples/ipv6/service.cpp
@@ -1,47 +1,7 @@
 #include <os>
 #include <iostream>
-#include <net/inet>
-#include <net/class_ip6.hpp>
 
 using namespace std;
-
-#define SERVICE_PORT 5555
-net::Inet* network;
-/*
-int listener(std::shared_ptr<net::Packet>& pckt)
-{
-  cout << "<DNS SERVER> got packet..." << endl;
-  using namespace net;
-  
-  UDP::full_header& udp = *(UDP::full_header*) pckt->buffer();
-  //DNS::header& hdr      = *(DNS::header*) (pckt->buffer() + sizeof(UDP::full_header));
-  
-  /// create response ///
-  int packetlen = 0;
-  
-  /// send response back to client ///
-  
-  // set source & return address
-  udp.udp_hdr.dport = udp.udp_hdr.sport;
-  udp.udp_hdr.sport = htons(SERVICE_PORT);
-  udp.udp_hdr.length = htons(sizeof(UDP::udp_header) + packetlen);
-  
-  // Populate outgoing IP header
-  udp.ip_hdr.daddr = udp.ip_hdr.saddr;
-  udp.ip_hdr.saddr = network->ip4(ETH0);
-  udp.ip_hdr.protocol = IP4::IP4_UDP;
-  
-  // packet length (??)
-  int res = pckt->set_len(sizeof(UDP::full_header) + packetlen); 
-  if(!res)
-    cout << "<DNS_SERVER> ERROR setting packet length failed" << endl;
-  std::cout << "Returning " << packetlen << "b to " << udp.ip_hdr.daddr.str() << std::endl;  
-  std::cout << "Full packet size: " << pckt->len() << endl;
-  // return packet (as DNS response)
-  network->udp_send(pckt);
-  
-  return 0;
-}*/
 
 struct TestAddr
 {
@@ -52,14 +12,13 @@ struct TestAddr
     : i32{a, b, c, d} {}
   TestAddr(uint64_t top, uint64_t bot)
     : i64{top, bot} {}
-  //TestAddr(__m128i address)
-  //  : i128(address) {}
+  
   // copy-constructor
   TestAddr(const TestAddr& a)
   {
     printf("TestAddr copy constructor\n");
     printf("TestAddr %s\n", a.to_string().c_str());
-    //i128 = a.i128;
+    
     this->i64[0] = a.i64[0];
     this->i64[1] = a.i64[1];
   }
@@ -68,7 +27,7 @@ struct TestAddr
   {
     printf("TestAddr move constructor\n");
     printf("TestAddr %s\n", a.to_string().c_str());
-    //i128 = a.i128;
+    
     this->i64[0] = a.i64[0];
     this->i64[1] = a.i64[1];
     return *this;
@@ -76,30 +35,33 @@ struct TestAddr
   // returns this IPv6 address as a string
   std::string to_string() const;
   
-  //__m128i  i128;
+  // fields
   uint64_t i64[2];
   uint32_t i32[4];
   uint16_t i16[8];
   uint8_t  i8[16];
-}; // __attribute__((aligned(16)));
+  
+};
 
 const std::string lut = "0123456789abcdef";
 
 std::string TestAddr::to_string() const
 {
-  std::string ret(40, 0);
+  //const std::string lut = "0123456789abcdef";
+  std::string ret(40, '\0');
   int counter = 0;
   
   const uint8_t* octet = i8;
   
   for (int i = 0; i < 16; i++)
   {
-    ret[counter++] = lut[(octet[i] & 0xF0) >> 4];
-    ret[counter++] = lut[(octet[i] & 0x0F) >> 0];
+    ret[counter++] = 'c'; //lut[(octet[i] & 0xF0) >> 4];
+    ret[counter++] = 'd'; //lut[(octet[i] & 0x0F) >> 0];
     if (i & 1)
       ret[counter++] = ':';
   }
-  ret.resize(counter-1);
+  printf("string: %s  len: %d\n", ret.c_str(), counter);
+  ret[counter-1] = 0;
   return ret;
 }
 
@@ -108,24 +70,12 @@ void Service::start()
 {
 	std::cout << "*** Service is up - with OS Included! ***" << std::endl;
 	
-  //using namespace net;
-  //auto& mac = Dev::eth(ETH0).mac();
-  
-  //Inet::ifconfig(ETH0, // Interface
-  //    {mac.part[2],mac.part[3],mac.part[4],mac.part[5]}, // IP
-  //    {255,255,0,0}, // Netmask
-  //    IP6::addr(255, 255) ); // IPv6
-  
   TestAddr ip6(1234, 1234);
   
   register void* sp asm ("sp");
   printf("stack: %p\n", sp);
   
-  //TestAddr test = ip6;
+  TestAddr test = ip6;
   std::cout << "ip6 = " << ip6.to_string() << std::endl;
-  //printf("ipv6 %s\n", ip6.to_string().c_str());
-  
-	//network = Inet::up();
-  //std::cout << "Starting UDP server on IP " 
-  //          << network->ip4(ETH0).str() << std::endl;
+  printf("ipv6 %s\n", ip6.to_string().c_str());
 }
