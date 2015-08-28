d7817558da7223cc2ee416c0e8f53eb751eb92f0
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-08-28 15:56:50 +0200

    Improved installscripts. Fixed #157

--- etc/build_newlib.sh
+++ etc/build_newlib.sh
@@ -1,39 +1,46 @@
 # Configure for an "unspecified x86 elf" target, 
 # (using no prefix to denote location, we'll levave the libs here for now)
-export PREFIX="/usr/local/IncludeOS"
-export TARGET=i686-elf
-export PATH="$PREFIX/bin:$PATH"
 
-LIBVER=2.2.0-1
+# Utils
+. ./bash_functions.sh
 
-mkdir -p $HOME/cross-dev
-cd $HOME/cross-dev
+cd $build_dir
 
 echo -e "\n\n >>> Getting newlib \n"
-wget -c --trust-server-name ftp://sourceware.org/pub/newlib/newlib-$LIBVER.tar.gz
+wget -c --trust-server-name ftp://sourceware.org/pub/newlib/newlib-$newlib_version.tar.gz
 
-echo -e "\n\n >>> Extracting "
-tar -xf newlib-$LIBVER.tar.gz
+echo -e "\n\n >>> Extracting newlib \n"
+tar -xf newlib-$newlib_version.tar.gz
 
-echo -e "\n\n >>> Installing dependencies"
-sudo apt-get install -y texinfo
+# MOVED UP to IncludeOS prereqs. Gcc asks for texinfo as well
+# echo -e "\n\n >>> Installing dependencies"
+# sudo apt-get install -y texinfo
 
-echo -e "\n\n >>> Configuring \n"
+
+echo -e "\n\n >>> Configuring newlib \n"
 mkdir -p build_newlib
 cd build_newlib
 
 # Clean out config cache in case the cross-compiler has changed
 make distclean
-
-../newlib-$LIBVER/configure --target=$TARGET --prefix=$PREFIX
+../newlib-$newlib_version/configure --target=$TARGET --prefix=$PREFIX
 
 #It expects the c compiler to be called 'i686-elf-cc', but ours is called gcc.
 shopt -s expand_aliases
 alias i686-elf-cc="$PREFIX/bin/i686-elf-gcc"
 
+
 echo ""
 echo $PATH
-echo -e "\n\n---------- STARTING BUILD --------------\n\n"
+export PATH=$PATH:$PREFIX/bin
+
+echo -e "\n\n >>> BUILDING NEWLIB \n\n"
 
 #i686-elf-cc --version
-make all install
+make all 
+
+echo -e "\n\n >>> INSTALLING NEWLIB \n\n"
+#shopt -s expand_aliases
+sudo -E PATH=$PATH:$PREFIX/bin make install
+
+
--- install.sh
+++ install.sh
@@ -1,29 +1,79 @@
 #! /bin/bash
 
-OSDIR=/usr/local/IncludeOS/
+INSTALL_DIR=/usr/local/IncludeOS/
 
-echo -e "\n>>> Looking for toolchain"
-#if [ ! -f $OSDIR/bin/i686-elf-c++ ]  
-#then
-    echo "  * Toolchain missing - installing..."
-    ./etc/install_toolchain.sh
-#else
-#    echo "  * Toolchain seems OK "
-#fi
+export PREFIX=$INSTALL_DIR
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+export build_dir=$HOME/cross-dev
 
-echo -e "\n>>> Creates IncludeOS-bridge"
-./etc/create_bridge.sh
-cp ./etc/qemu-ifup /etc
+# Multitask-parameter to make
+export num_jobs=-j8
 
-echo -e "\n>>> Installing vmbuilder"
+export binutils_version=2.25
+export gcc_version=5.1.0
+export newlib_version=2.2.0-1
+
+
+# Utilities
+. ./etc/bash_functions.sh
+
+
+# TODO: Implement checks to see which steps can be skipped!
+
+
+# BUILDING IncludeOS
+
+PREREQS_BUILD="gcc g++ build-essential make nasm texinfo"
+
+# Get prerequisite packages, such as a compiler, GNU Make, etc.
+echo -e "\n\n >>> Trying to install prerequisites for *building* IncludeOS"
+echo -e  "        Packages: $PREREQS_BUILD \n"
+sudo apt-get install -y $PREREQS_BUILD
+or_die "Couldn't install required packages"
+
+
+# Get and build the actual toolchain
+echo -e "\n\n >>> GETTING / BUILDING CROSS COMPILER \n"
+ ./etc/cross_compiler.sh
+or_die "Couldn't install cross compiler"
+
+
+echo -e "\n\n >>> GETTING / BUILDING NEWLIB \n"
+./etc/build_newlib.sh
+or_die "Couldn't install newlib"
+
+# Build and install the vmbuilder 
+echo -e "\n >>> Installing vmbuilder"
 cd vmbuild
 make
 sudo cp vmbuild $OSDIR/
-
-echo -e "\n>>> Installing IncludeOS"
+or_die "Couldn't install vmbuilder"
+ 
+echo -e "\n >>> Building IncludeOS"
 cd ../src
 make 
+or_die "Couldn't build IncludeOS"
+
+echo -e "\n >>> Installing IncludeOS"
 sudo make install
+cd ..
+or_die "Couldn't install IncludeOS"
+
+
+# RUNNING IncludeOS
+PREREQS_RUN="bridge-utils qemu-kvm"
+echo -e "\n\n >>> Trying to install prerequisites for *running* IncludeOS"
+echo -e   "        Packages: $PREREQS_RUN \n"
+sudo apt-get install -y $PREREQS_RUN
+or_die "Couldn't install packages needed to run IncludeOS"
+
+# Set up the IncludeOS network bridge
+echo -e "\n\n >>> Create IncludeOS network bridge "`pwd`"\n"
+sudo ./etc/create_bridge.sh
+sudo cp ./etc/qemu-ifup /etc
+or_die "Couldn't set up the network bridge"
+
 
 echo -e "\n >>> Done. Test the installation by running ./test.sh \n"
 
