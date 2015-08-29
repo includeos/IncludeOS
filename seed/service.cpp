473655597353035de47636c71c01881bf3df25cd
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-08-29 11:38:50 +0200

    Some working libc++ tests

--- seed/service.cpp
+++ seed/service.cpp
@@ -1,15 +1,31 @@
 #include <os>
 #include <iostream>
+#include <vector>
 #include <exception>
 
-using namespace std;
+//using namespace std;
 
 void Service::start()
 {
+  
+  std::vector<int> integers={1,2,3};
+  std::map<const char*, int> map_of_ints={std::make_pair("First",42) , std::make_pair("Second",43)};
+  
+  //std::string str = "Hello strings";
+  
   printf("Hello world\n");
   
   [] (void) { printf("Hello lambda\n"); } ();
   
-  //throw "hei";
+  for (auto i : integers)
+    printf("Integer %i \n",i);
+  
+  printf("First from map: %i \n", map_of_ints["First"]);
+  printf("Second from map: %i \n", map_of_ints["Second"]);
+  try {
+    throw "hei";
+  }catch(const char* str){
+    printf("Caught exception: %s \n",str);
+  }
   
 }
