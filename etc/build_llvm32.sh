c4e343c2dbf79df3bea9a607059f8183aeb1a08d
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-09-07 23:24:06 +0200

    compiler-rt is now back in

--- etc/build_llvm32.sh
+++ etc/build_llvm32.sh
@@ -18,7 +18,7 @@ cd $llvm/projects
 
 # Compiler-rt
 # git clone http://llvm.org/git/llvm compiler-rt
-#svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
 
 # libc++abi
 #svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
@@ -51,35 +51,37 @@ OPTS+="-DCMAKE_BUILD_TYPE=MinSizeRel "
 
 # Can't build libc++ with g++ unless it's a cross compiler (need to specify target)
 #OPTS+=-DCMAKE_C_COMPILER=clang" "
-#OPTS+=-DCMAKE_CXX_COMPILER=clang++ -std=c++11" "
+OPTS+=-DCMAKE_CXX_COMPILER=clang++" " # -std=c++11" "
 
 #
 # WARNING: It seems imossible to pass in cxx-flags like this; I've tried \' \\" \\\" etc.
 # OPTS+="-DCMAKE_CXX_FLAGS='-I/home/alfred/IncludeOS/stdlib/support -I/usr/local/IncludeOS/i686-elf/include -I/home/alfred/IncludeOS/stdlib/support/newlib -I/home/alfred/IncludeOS/src/include' " 
 # OPTS+='-DCMAKE_CXX_FLAGS=-I/home/alfred/IncludeOS/stdlib/support -I/usr/local/IncludeOS/i686-elf/include  -I/home/alfred/IncludeOS/stdlib/support/newlib -I/home/alfred/IncludeOS/src/include '
 
-OPTS+="-DTARGET_TRIPLE=i686-elf "
+TRIPLE=i686-pc-none-elf
+
+OPTS+=-DTARGET_TRIPLE=$TRIPLE" "
 #OPTS+="-DLLVM_TARGETS_TO_BUILD=i686-elf "
-OPTS+="-DLLVM_BUILD_32_BITS=ON "
-OPTS+="-DLLVM_INCLUDE_TESTS=OFF "
-OPTS+="-DLLVM_ENABLE_THREADS=OFF "
-OPTS+="-DLLVM_DEFAULT_TARGET_TRIPLE=i686-elf "
+OPTS+=-DLLVM_BUILD_32_BITS=ON" "
+OPTS+=-DLLVM_INCLUDE_TESTS=OFF" "
+OPTS+=-DLLVM_ENABLE_THREADS=OFF" "
+OPTS+=-DLLVM_DEFAULT_TARGET_TRIPLE=$TRIPLE" "
 
 # libc++-specific options
-OPTS+="-DLIBCXX_ENABLE_SHARED=OFF "
-OPTS+="-DLIBCXX_ENABLE_THREADS=OFF "
-OPTS+="-DLIBCXX_TARGET_TRIPLE=i686-elf "
-OPTS+="-DLIBCXX_BUILD_32_BITS=ON "
+OPTS+=-DLIBCXX_ENABLE_SHARED=OFF" "
+OPTS+=-DLIBCXX_ENABLE_THREADS=OFF" "
+OPTS+=-DLIBCXX_TARGET_TRIPLE=$TRIPLE" "
+OPTS+=-DLIBCXX_BUILD_32_BITS=ON" "
 
 # OPTS+=-DLIBCXX_GCC_TOOLCHAIN=/usr/local/IncludeOS/i686/" "
-# OPTS+=-DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON" "
+OPTS+=-DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON" "
 
-OPTS+="-DLIBCXX_CXX_ABI=libcxxabi "
-OPTS+="-DLIBCXX_CXX_ABI_INCLUDE_PATHS=/home/alfred/IncludeOS/src/include "
+OPTS+=-DLIBCXX_CXX_ABI=libcxxabi" "
+OPTS+=-DLIBCXX_CXX_ABI_INCLUDE_PATHS=/home/alfred/IncludeOS/src/include" "
 
 # libunwind-specific options
-#OPTS+="-DLIBUNWIND_ENABLE_SHARED=OFF "
-#OPTS+="-LIBCXXABI_USE_LLVM_UNWINDER=ON "
+OPTS+=-DLIBUNWIND_ENABLE_SHARED=OFF" "
+OPTS+=-LIBCXXABI_USE_LLVM_UNWINDER=ON" "
 
 
 # TODO: Libunwind! There's an option in the CMakeLists.txt of libcxx, specifying to use llvm's libunwind
@@ -89,9 +91,13 @@ echo "LLVM CMake Build options:" $OPTS
 
 # -nostdinc -nostdlib -nodefaultlibs -ffreestanding -isystem /usr/local/IncludeOS/i686/ --sysroot=/usr/local/IncludeOS/i686
 #time cmake -G"Unix Makefiles" $OPTS  ../$llvm
-cmake -GNinja $OPTS -DCMAKE_CXX_FLAGS='-std=c++11 -I/usr/local/IncludeOS/i686-elf/include -I/home/alfred/IncludeOS/stdlib/support -I/home/alfred/IncludeOS/stdlib/support/newlib  -I/home/alfred/IncludeOS/src/include' ../$llvm #  -DCMAKE_CXX_COMPILER='clang++ -std=c++11' # -I/home/alfred/IncludeOS/stdlib
+cmake -GNinja $OPTS -DCMAKE_CXX_FLAGS='-std=c++11 -I/usr/local/IncludeOS/i686-elf/include -I/home/alfred/IncludeOS/stdlib/support -I/home/alfred/IncludeOS/stdlib/support/newlib  -I/home/alfred/IncludeOS/src/include' ../$llvm #  -DCMAKE_CXX_COMPILER='clang++ -std=c++11' 
 
-#ninja libc++abi.a
+
+# WARNING: This will cause the "requires std::atomic" error. For some reason - the headers should be the same 
+# -I/home/alfred/IncludeOS/stdlib
+
+ninja libc++abi.a
 #ninja libc++.a
 
 #time make cxx
