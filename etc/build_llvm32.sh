9bc98ac1d76fa32d929a94fb34a69628df8c5a35
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-09-07 22:34:13 +0200

    libc++ configures correctly on 64-bit. Still isues with build

--- etc/build_llvm32.sh
+++ etc/build_llvm32.sh
@@ -1,14 +1,12 @@
-$llvm = llvmes
-$llvm_build = llvm_build
+llvm=llvm
+llvm_build=llvm_build
 
 # Dependencies
-sudo apt-get install cmake subversion zlib1g-dev:i386 libtinfo-dev:i386
-
-# On 64-bit:                                                                                                                      
-zlib1g-dev:i386 libtinfo-dev:i386
+#sudo apt-get install cmake ninja-build subversion zlib1g-dev:i386 libtinfo-dev:i386
 
 # Clone LLVM                                                                                                                      
-svn co http://llvm.org/svn/llvm-project/llvm/trunk $llvm
+#time svn co http://llvm.org/svn/llvm-project/llvm/trunk $llvm
+# git clone http://llvm.org/git/llvm 
 
 # Clone CLANG - not necessary to build only libc++ and libc++abi                                                                  
 # cd llvm/tools                                                                                                                   
@@ -18,36 +16,84 @@ svn co http://llvm.org/svn/llvm-project/llvm/trunk $llvm
 # Clone libc++, libc++abi, and some extra stuff (recommended / required for clang)                                                
 cd $llvm/projects
 
-svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
-svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-svn co http://llvm.org/svn/llvm-project/libunwind/trunk libunwind
+# Compiler-rt
+# git clone http://llvm.org/git/llvm compiler-rt
+#svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
 
-# Make a build-directory                                                                                                          
-cd
-mkdir $llvm_build
+# libc++abi
+#svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+# git clone http://llvm.org/git/libcxxabi
+
+# libc++
+#svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+# git clone http://llvm.org/git/libcxx
+
+# libunwind
+#svn co http://llvm.org/svn/llvm-project/libunwind/trunk libunwind
+#git clone http://llvm.org/git/libunwind
+ 
+# Back to start
+cd ../../
+
+# Make a build-directory
+mkdir -p $llvm_build
 cd $llvm_build
 
-OPTS=-DBUILD_SHARED_LIBS=OFF" "
-OPTS+=-DLIBCXX_ENABLE_SHARED=OFF" "
-OPTS+=-DCMAKE_BUILD_TYPE=MinSizeRel" "
-OPTS+=-DCMAKE_C_COMPILER=clang" "
-OPTS+=-DCMAKE_CXX_COMPILER=clang++" "
-OPTS+=-DLIBCXX_ENABLE_THREADS=OFF" "
-OPTS+=-DTARGET_TRIPLE="i686-elf "
-OPTS+=-DLLVM_BUILD_32_BITS=ON" "
-OPTS+=-DLIBCXX_BUILD_32_BITS=ON" "
+rm CMakeCache.txt
+
+# General options
+OPTS="-DCMAKE_EXPORT_COMPILE_COMMANDS=ON "
+
+# LLVM General options
+OPTS+="-DBUILD_SHARED_LIBS=OFF "
+OPTS+="-DCMAKE_BUILD_TYPE=MinSizeRel "
+#OPTS+="-DCMAKE_BUILD_TYPE=Release "
+
+# Can't build libc++ with g++ unless it's a cross compiler (need to specify target)
+#OPTS+=-DCMAKE_C_COMPILER=clang" "
+#OPTS+=-DCMAKE_CXX_COMPILER=clang++ -std=c++11" "
+
+#
+# WARNING: It seems imossible to pass in cxx-flags like this; I've tried \' \\" \\\" etc.
+# OPTS+="-DCMAKE_CXX_FLAGS='-I/home/alfred/IncludeOS/stdlib/support -I/usr/local/IncludeOS/i686-elf/include -I/home/alfred/IncludeOS/stdlib/support/newlib -I/home/alfred/IncludeOS/src/include' " 
+# OPTS+='-DCMAKE_CXX_FLAGS=-I/home/alfred/IncludeOS/stdlib/support -I/usr/local/IncludeOS/i686-elf/include  -I/home/alfred/IncludeOS/stdlib/support/newlib -I/home/alfred/IncludeOS/src/include '
+
+OPTS+="-DTARGET_TRIPLE=i686-elf "
+#OPTS+="-DLLVM_TARGETS_TO_BUILD=i686-elf "
+OPTS+="-DLLVM_BUILD_32_BITS=ON "
+OPTS+="-DLLVM_INCLUDE_TESTS=OFF "
+OPTS+="-DLLVM_ENABLE_THREADS=OFF "
+OPTS+="-DLLVM_DEFAULT_TARGET_TRIPLE=i686-elf "
+
+# libc++-specific options
+OPTS+="-DLIBCXX_ENABLE_SHARED=OFF "
+OPTS+="-DLIBCXX_ENABLE_THREADS=OFF "
+OPTS+="-DLIBCXX_TARGET_TRIPLE=i686-elf "
+OPTS+="-DLIBCXX_BUILD_32_BITS=ON "
+
+# OPTS+=-DLIBCXX_GCC_TOOLCHAIN=/usr/local/IncludeOS/i686/" "
+# OPTS+=-DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON" "
+
+OPTS+="-DLIBCXX_CXX_ABI=libcxxabi "
+OPTS+="-DLIBCXX_CXX_ABI_INCLUDE_PATHS=/home/alfred/IncludeOS/src/include "
+
+# libunwind-specific options
+#OPTS+="-DLIBUNWIND_ENABLE_SHARED=OFF "
+#OPTS+="-LIBCXXABI_USE_LLVM_UNWINDER=ON "
+
 
 # TODO: Libunwind! There's an option in the CMakeLists.txt of libcxx, specifying to use llvm's libunwind
 # TODO: How to enable newlib support : set _NEWLIB_VERSION=1 ? 
 
-# General options
-OPTS+=-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" "
-
 echo "LLVM CMake Build options:" $OPTS
 
-cmake -G"Unix Makefiles" $OPTS  ../$llvm
+# -nostdinc -nostdlib -nodefaultlibs -ffreestanding -isystem /usr/local/IncludeOS/i686/ --sysroot=/usr/local/IncludeOS/i686
+#time cmake -G"Unix Makefiles" $OPTS  ../$llvm
+cmake -GNinja $OPTS -DCMAKE_CXX_FLAGS='-std=c++11 -I/usr/local/IncludeOS/i686-elf/include -I/home/alfred/IncludeOS/stdlib/support -I/home/alfred/IncludeOS/stdlib/support/newlib  -I/home/alfred/IncludeOS/src/include' ../$llvm #  -DCMAKE_CXX_COMPILER='clang++ -std=c++11' # -I/home/alfred/IncludeOS/stdlib
 
-make cxx
-make cxxabi
-make libunwind
+#ninja libc++abi.a
+#ninja libc++.a
+
+#time make cxx
+#time make cxxabi
+#time make unwind
--- etc/build_newlib.sh
+++ etc/build_newlib.sh
@@ -1,9 +1,6 @@
 # Configure for an "unspecified x86 elf" target, 
 # (using no prefix to denote location, we'll levave the libs here for now)
 
-# Utils
-. ./bash_functions.sh
-
 cd $build_dir
 
 echo -e "\n\n >>> Getting newlib \n"
@@ -23,7 +20,7 @@ cd build_newlib
 
 # Clean out config cache in case the cross-compiler has changed
 make distclean
-../newlib-$newlib_version/configure --target=$TARGET --prefix=$PREFIX
+../newlib-$newlib_version/configure --target=$TARGET --prefix=$PREFIX AS_FOR_TARGET=$PREFIX/bin/$TARGET-as LD_FOR_TARGET=$PREFIX/bin/$TARGET-ld AR_FOR_TARGET=$PREFIX/bin/$TARGET-ar RANLIB_FOR_TARGET=$PREFIX/bin/$TARGET-ranlib #CC_FOR_TARGET="clang -ffreestanding --target=i686-elf -ccc-gcc-name "$PREFIX/bin/$TARGET"-gcc"
 
 #It expects the c compiler to be called 'i686-elf-cc', but ours is called gcc.
 shopt -s expand_aliases
@@ -37,7 +34,7 @@ export PATH=$PATH:$PREFIX/bin
 echo -e "\n\n >>> BUILDING NEWLIB \n\n"
 
 #i686-elf-cc --version
-make all 
+make $num_jobs all 
 
 echo -e "\n\n >>> INSTALLING NEWLIB \n\n"
 #shopt -s expand_aliases
