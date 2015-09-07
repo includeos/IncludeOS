79a7c34db77ac829fd81450115f2e2205ca5d716
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-09-07 13:32:31 +0200

    Clone llvm / libc++ from svn

--- etc/build_llvm32.sh
+++ etc/build_llvm32.sh
@@ -1,8 +1,32 @@
-# No shared libs - just .a
-# ...Not for libcxx either
-# Could use higher, but this is current high from ubuntu repo
-# Building for 32-bit
+$llvm = llvmes
+$llvm_build = llvm_build
 
+# Dependencies
+sudo apt-get install cmake subversion zlib1g-dev:i386 libtinfo-dev:i386
+
+# On 64-bit:                                                                                                                      
+zlib1g-dev:i386 libtinfo-dev:i386
+
+# Clone LLVM                                                                                                                      
+svn co http://llvm.org/svn/llvm-project/llvm/trunk $llvm
+
+# Clone CLANG - not necessary to build only libc++ and libc++abi                                                                  
+# cd llvm/tools                                                                                                                   
+# svn co http://llvm.org/svn/llvm-project/cfe/trunk clang                                                                         
+# svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra                                                           
+
+# Clone libc++, libc++abi, and some extra stuff (recommended / required for clang)                                                
+cd $llvm/projects
+
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+svn co http://llvm.org/svn/llvm-project/libunwind/trunk libunwind
+
+# Make a build-directory                                                                                                          
+cd
+mkdir $llvm_build
+cd $llvm_build
 
 OPTS=-DBUILD_SHARED_LIBS=OFF" "
 OPTS+=-DLIBCXX_ENABLE_SHARED=OFF" "
@@ -11,22 +35,18 @@ OPTS+=-DCMAKE_C_COMPILER=clang" "
 OPTS+=-DCMAKE_CXX_COMPILER=clang++" "
 OPTS+=-DLIBCXX_ENABLE_THREADS=OFF" "
 OPTS+=-DTARGET_TRIPLE="i686-elf "
-#OPTS+=-DCMAKE_CXX_FLAGS=-target" "
 OPTS+=-DLLVM_BUILD_32_BITS=ON" "
 OPTS+=-DLIBCXX_BUILD_32_BITS=ON" "
-OPTS+=-DCMAKE_CXX_FLAGS=-O2" "
-OPTS+=-D_NEWLIB_VERSION=1" "
 
-# Libunwind!
-
+# TODO: Libunwind! There's an option in the CMakeLists.txt of libcxx, specifying to use llvm's libunwind
+# TODO: How to enable newlib support : set _NEWLIB_VERSION=1 ? 
 
 # General options
 OPTS+=-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" "
 
-echo "OPTIONS:" $OPTS
+echo "LLVM CMake Build options:" $OPTS
 
-cmake -G"Unix Makefiles" $OPTS  ../llvm 
-
+cmake -G"Unix Makefiles" $OPTS  ../$llvm
 
 make cxx
 make cxxabi
