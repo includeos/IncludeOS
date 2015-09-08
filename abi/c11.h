057bb111319f76da3a7d2e6f7e18e697ea8112f8
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-09-08 12:23:41 +0200

    libcxx/stdlib/cstdlib expects quick_exit to be a file

--- abi/c11.h
+++ abi/c11.h
@@ -1,8 +1,7 @@
 #ifndef C11_H
 #define C11_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <quick_exit>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,9 +9,6 @@ extern "C" {
   
   //New stuff in C11, required by libunwind, compiler-rt etc. in llvm
   
-int at_quick_exit (void (*func)(void));
-_Noreturn void quick_exit (int status);
-
 void *aligned_alloc( size_t alignment, size_t size );
 
 #ifdef __cplusplus
