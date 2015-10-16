f52fd6f93e7b77002c2a469ab26f0b81649459c2
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-10-16 09:08:10 +0200

    crti.o and crtn.o are now built by install.sh

--- install.sh
+++ install.sh
@@ -88,17 +88,25 @@ if [ ! -z $do_includeos ]; then
     # Build and install the vmbuilder 
     echo -e "\n >>> Installing vmbuilder"
     pushd $IncludeOS_src/vmbuild
-    make
+    make 
     sudo cp vmbuild $INSTALL_DIR/
     popd
     
     echo -e "\n >>> Building IncludeOS"
     pushd $IncludeOS_src/src
-    make 
+    make $num_jobs
     
     echo -e "\n >>> Installing IncludeOS"
     sudo make install
+    
+    echo -e "\n >>> Linking IncludeOS test-service"
+    sudo make $INSTALL_DIR/crt/crti.o
+    sudo make $INSTALL_DIR/crt/crtn.o
+    make test
+    
     popd
+
+    
     
     # RUNNING IncludeOS
     PREREQS_RUN="bridge-utils qemu-kvm"
