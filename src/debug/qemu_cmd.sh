e1058f7781abf98905cd44ae469a0d897b551ccc
Hårek Haugerud <haugerud@hioa.no>
2015-10-29 11:38:22 +0100

    qemu-system-x86_64 --enable-kvm

--- src/debug/qemu_cmd.sh
+++ src/debug/qemu_cmd.sh
@@ -1,4 +1,5 @@
-export QEMU="qemu-system-i386 -smp 1"
+export QEMU="qemu-system-x86_64 --enable-kvm -smp 1"
+#export QEMU="qemu-system-i386 -smp 1"
 
 #DEV_NET="-net nic,model=virtio,macaddr=fa:16:3e:db:40:3a"
 #OPENSTACK="-netdev tap,fd=29,id=hostnet0 " #Bad file descriptor
