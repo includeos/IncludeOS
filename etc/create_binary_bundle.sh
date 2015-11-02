80b7f401ece66dcb742a8e5eb5a573b504892609
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-11-02 15:02:55 +0100

    Install to local bundle first

--- etc/create_binary_bundle.sh
+++ etc/create_binary_bundle.sh
@@ -1,38 +1,34 @@
-INSTALL_DIR=/usr/local/IncludeOS
-newlib=$INSTALL_DIR/i686-elf/lib
+#! /bin/bash
 
+[ ! -v BUILD_DIR ] && BUILD_DIR=$HOME/IncludeOS_build
+[ ! -v TMP_INSTALL_DIR ] && TMP_INSTALL_DIR=$BUILD_DIR/install
 
-FOLDER="IncludeOS_bin"
+newlib=$TMP_INSTALL_DIR/i686-elf/lib
+
+FOLDER="IncludeOS_install"
 OUTFILE="$FOLDER.tar.gz"
 
 # Libraries
 libc=$newlib/libc.a
 libm=$newlib/libm.a
-libcpp=$INSTALL_DIR/lib/libc++.a
-
-## libgcc - required by newlib
-GPP=$INSTALL_DIR/bin/i686-elf-g++
-GCC_VER=`$GPP -dumpversion`
-libgcc=$INSTALL_DIR/lib/gcc/i686-elf/$GCC_VER/libgcc.a
+libcpp=$BUILD_DIR/llvm_build/lib/libc++.a
 
 # Includes
-include_newlib=$INSTALL_DIR/i686-elf/include
-include_libcxx=$INSTALL_DIR/stdlib
+include_newlib=$TMP_INSTALL_DIR/i686-elf/include
+include_libcxx=$BUILD_DIR/llvm_build/include/c++/v1
 
 
 # Make directory-tree
 mkdir -p $FOLDER
 mkdir -p $FOLDER/newlib
 mkdir -p $FOLDER/libcxx
-mkdir -p $FOLDER/libgcc
 mkdir -p $FOLDER/crt
 
 # Copy binaries
 cp $libcpp $FOLDER/libcxx/
 cp $libm $FOLDER/newlib/
 cp $libc $FOLDER/newlib/
-cp $libgcc $FOLDER/libgcc/
-cp $INSTALL_DIR/crt/*.o $FOLDER/crt/
+cp $BUILD_DIR/crt/*.o $FOLDER/crt/
 
 # Copy includes
 cp -r $include_newlib $FOLDER/newlib/
