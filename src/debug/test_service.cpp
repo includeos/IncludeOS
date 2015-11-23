ae0367f4941b2b191174ac6662457fafadcff56a
fwsgonzo <fwsgonzo@hotmail.com>
2015-11-23 20:42:28 +0100

    Renamed UDP socket write() to sendto()

--- src/debug/test_service.cpp
+++ src/debug/test_service.cpp
@@ -54,7 +54,7 @@ void Service::start() {
           data.size(), addr.str().c_str(), port);
       
       auto& sock = stack.udp().bind(port);
-      sock.write(addr, port, data.c_str(), data.size());
+      sock.sendto(addr, port, data.c_str(), data.size());
       
       printf("Done. You can Ctrl-A + X now.\n");
     });
