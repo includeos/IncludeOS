75cfb9152644afc30bf3b948cdcc7721817ae172
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-11-30 23:26:06 +0100

    Added License

--- etc/apply_license.sh
+++ etc/apply_license.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
 licenselen=$(wc -l < license.txt)
 extensions="hpp cpp h c"
-donotcheck="./api/SQLite ./src/crt/cxxabi ./src/include/cxxabi.h ./src/include/__cxxabi_config.h *sanos* ./doc"
-for extension in $extensions ; do
-	for line in $(find .. -iname "*.$extension"); do 
-	     echo "Now processing file: $line"
-	     for item in $donotcheck; do
-		if [[ $(echo $line | grep $item) ]] ; then
-			echo "Bypassing file"; 
-			break;
-		fi
-		head -n $licenselen $line > ./tmpfile
-		if [[ $(diff license.txt ./tmpfile) ]]; then	
-	    		 head -n 10 $line; 
-			 PS3='What would you like to do? (1-2)? '; 
-			 select answer in "Add license text from license.txt" "Do nothing and proceed to next file"; do
-			 if [[ $answer = "Add license text from license.txt" ]]; then
-				{ echo Adding license;
-				( cat license.txt; echo; cat $line ) > /tmp/file; 
-				mv /tmp/file $line 
-				break; }
-			elif [[ $answer = "Do nothing and proceed to next file" ]]; then {
-				echo Okay, doing nothing; 
-				break; }
-			else {
-				echo Please select one of the alternatives.
-			} fi
-	     	done
-	
-		else	
-			echo "License OK";
-			break;
-		fi
 
+# Enable recursive wildcards
+shopt -s globstar
+donotcheck="$(echo  ../src/crt/cxxabi/* ../src/include/cxxabi.h ../src/include/__cxxabi_config.h *sanos* ../doc ../mod/protobuf/include/**/* ../mod/SQLite/* ../examples/jansson/jansson/* ../examples/STREAM/stream.cpp ../src/elf/* ../examples/tcc/libtcc.h *poker.pb.h ../examples/protobuf/**/* ../mod/protobuf/api.pb.h ../api/utility/delegate.hpp)"
+
+echo "IGNORE: $donotcheck"
+
+license_txt=$(cat license.txt);
+
+for extension in $extensions ; do
+    echo "Processing extension $extension"
+    file_list=$(find .. -iname "*.$extension")
+    for line in $file_list; do 
+	echo "Now processing file: $line"
+	bypass=""
+	for item in $donotcheck; do
+	    [[ $line ==  $item ]] && bypass=1 && break;
 	done
-done
+	
+	# Break and continue if 
+	[ ! -z $bypass ] && echo "Bypassing file" && continue
+	
+
+	current_head=$(head -n $licenselen $line)
+	[[ $license_txt == $current_head ]] && echo "License OK" && continue;
+	
+	head -n 10 $line; 
+	PS3='What would you like to do? (1-2)? '; 
+	select answer in "Add license text from license.txt" "Do nothing and proceed to next file"; do
+	    if [[ $answer = "Add license text from license.txt" ]]; 
+	    then
+		echo Adding license;
+		( cat license.txt; echo; cat $line ) > /tmp/file; 
+		mv /tmp/file $line 
+		break; 
+	    elif [[ $answer = "Do nothing and proceed to next file" ]]; then 
+		echo Okay, doing nothing; 
+		break; 
+		
+	    else 
+		echo Please select one of the alternatives.
+	    fi
+	done		
+    done
 done
--- src/include/pic_defs.h
+++ src/include/pic_defs.h
@@ -1,3 +1,21 @@
+// Part of the IncludeOS Unikernel  - www.includeos.org
+//
+// Copyright 2015 Oslo and Akershus University College of Applied Sciences
+// and  Alfred Bratterud. 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 // Note: These are required for interrupts.s
 #define PIC1 0x20 /* IO base address for master PIC */
 #define PIC2 0xA0 /* IO base address for slave PIC */
