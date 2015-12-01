ddb8ada2dcbe36a07ef859436b8574a14b164b45
Alfred Bratterud <alfred.bratterud@hioa.no>
2015-12-01 02:06:27 +0100

    Updated license text

--- examples/eastl/test.hpp
+++ examples/eastl/test.hpp
@@ -1,4 +1,4 @@
-// Part of the IncludeOS unikernel - www.includeos.org
+// This file is a part of the IncludeOS unikernel - www.includeos.org
 //
 // Copyright 2015 Oslo and Akershus University College of Applied Sciences
 // and Alfred Bratterud
