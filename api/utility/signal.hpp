22367c6bafd21632923563003ba7b52b43dd0984
RicoAntonioFelix <ricoantoniofelix@yahoo.com>
2015-12-07 15:16:16 -0400

    Refined the signal class

--- api/utility/signal.hpp
+++ api/utility/signal.hpp
@@ -25,23 +25,44 @@ template<typename F>
 class signal
 {
 public:
-	//! \brief Connect a function to this signal
-	template <class Fn>
-	void connect(Fn fn)
+	//! \brief Callable type of the signal handlers
+	using handler = std::function<F>;
+
+	//! \brief Default constructor
+	explicit signal() = default;
+
+	//! \brief Default destructor
+	~signal() noexcept = default;
+
+	//! \brief Default move constructor
+	explicit signal(signal&&) noexcept = default;
+
+	//! \brief Default assignment operator
+	signal& operator=(signal&&) = default;
+
+	//! \brief Connect a callable object to this signal
+	void connect(handler&& fn)
 	{
-		funcs.emplace_back(fn);
+		funcs.emplace_back(std::forward<handler>(fn));
 	}
 	
-	//! \brief Emit this signal by executing all the connected functions
+	//! \brief Emit this signal by executing all connected callable objects
 	template<typename... Args>
-	void emit(Args... args)
+	void emit(Args&&... args)
 	{
-		for(auto fn : funcs)
-			fn(args...);
+		for(auto& fn : funcs)
+			fn(std::forward<Args>(args)...);
 	}
 	
 private:
-	std::vector<std::function<F>> funcs;
+	// Set of callable objects registered to be called on demand
+	std::vector<handler> funcs;
+
+	// Avoid copying
+	signal(signal const&) = delete;
+
+	// Avoid assignment
+	signal& operator=(signal const&) = delete;
 };
 
 #endif
