6628f53164217e790a7b0fc694bb0e8cf43bc12c
Mars Saxman <mars@redecho.org>
2016-01-02 16:56:18 -0800

    deduplicated all the run.sh scripts into a common etc/run.sh, made run.sh work under osx bash (doesn't have -v operator)

--- etc/copy-scripts.sh
+++ etc/copy-scripts.sh
@@ -5,4 +5,5 @@ INSTALL_DIR=${INSTALL_DIR-$HOME/IncludeOS_install}
 mkdir -p $INSTALL_DIR/etc
 cp $INCLUDEOS_SRC/etc/qemu-ifup $INSTALL_DIR/etc/
 cp $INCLUDEOS_SRC/etc/qemu_cmd.sh $INSTALL_DIR/etc/
+cp $INCLUDEOS_SRC/etc/run.sh $INSTALL_DIR/etc/
 
