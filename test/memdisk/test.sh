e795b580577d926e4542ab1a2af799b0c8a9af78
fwsgonzo <fwsgonzo@hotmail.com>
2016-03-04 22:29:51 +0100

    test: test_base script for automating tests

--- test/memdisk/test.sh
+++ test/memdisk/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-make DISK=sector0.disk FILES=nosector.cpp
-./run.sh MemdiskTest.img
+source ../test_base
 
-make DISK=sector2.disk FILES=twosector.cpp
-./run.sh MemdiskTest.img
+make DISK=sector0.disk FILES=nosector.cpp > output.txt
+start "Memdisk: Empty disk test"
+make DISK=sector2.disk FILES=twosector.cpp > output.txt
+start "Memdisk: Multiple sectors test"
