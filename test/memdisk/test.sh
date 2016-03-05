06dd24a4ba1dc1f89d18597282cecf19ab06ef1a
fwsgonzo <fwsgonzo@hotmail.com>
2016-03-05 13:48:11 +0100

    test: Custom image name

--- test/memdisk/test.sh
+++ test/memdisk/test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 source ../test_base
 
-make DISK=sector0.disk FILES=nosector.cpp > output.txt
-start "Memdisk: Empty disk test"
-make DISK=sector2.disk FILES=twosector.cpp > output.txt
-start "Memdisk: Multiple sectors test"
+make SERVICE=Test DISK=sector0.disk FILES=nosector.cpp
+start Test.img "Memdisk: Empty disk test"
+make SERVICE=Test FILES=nosector.cpp clean
+
+make SERVICE=Test DISK=sector2.disk FILES=twosector.cpp
+start Test.img "Memdisk: Multiple sectors test"
+make SERVICE=Test FILES=twosector.cpp clean
