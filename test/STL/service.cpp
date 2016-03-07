e3f91a8d6a8f381a3ab1b586581676b0de9171d7
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-03-07 03:20:25 +0100

    Integrated lest test framework

--- test/STL/service.cpp
+++ test/STL/service.cpp
@@ -30,6 +30,71 @@
 #include <functional>
 #include <cstring>
 #include <cassert>
+#include <lest.hpp>
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp){
+  (void*)clk_id;
+  (void*)tp;
+  return 0;
+};
+
+using namespace std;
+
+const lest::test specification[] =
+{
+  {
+    SCENARIO( "vectors can be sized and resized" "[vector]" )
+    {
+
+      GIVEN( "A vector with some items" ) {
+        std::vector<int> v( 5 );
+
+        EXPECT( v.size() == 5u );
+        EXPECT( v.capacity() >= 5u );
+
+        WHEN( "the size is increased" ) {
+          v.resize( 10 );
+
+          THEN( "the size and capacity change" ) {
+            EXPECT( v.size() == 10u);
+            EXPECT( v.capacity() >= 10u );
+          }
+        }
+        WHEN( "the size is reduced" ) {
+          v.resize( 0 );
+
+          THEN( "the size changes but not capacity" ) {
+            EXPECT( v.size() == 0u );
+            EXPECT( v.capacity() >= 5u );
+          }
+        }
+        WHEN( "more capacity is reserved" ) {
+          v.reserve( 10 );
+
+          THEN( "the capacity changes but not the size" ) {
+            EXPECT( v.size() == 5u );
+            EXPECT( v.capacity() >= 10u );
+          }
+          WHEN( "less capacity is reserved again" ) {
+            v.reserve( 7 );
+
+            THEN( "capacity remains unchanged" ) {
+              EXPECT( v.capacity() >= 10u );
+            }
+          }
+        }
+        WHEN( "less capacity is reserved" ) {
+          v.reserve( 0 );
+
+          THEN( "neither size nor capacity are changed" ) {
+            EXPECT( v.size() == 5u );
+            EXPECT( v.capacity() >= 5u );
+          }
+        }
+      }
+    }
+  }
+};
 
 #define MYINFO(X,...) INFO("Test STL",X,##__VA_ARGS__)
 
@@ -72,6 +137,9 @@ void Service::start()
 
   CHECKSERT(strcmp(str.c_str(),orig) == 0, "std::string to c-string conversion works");
 
-  MYINFO("SUCCESS");
+  char* argv[2] {(char*)"test",(char*)"-p"};
 
+  lest::run(specification, 2, argv);
+
+  MYINFO("SUCCESS");
 }
