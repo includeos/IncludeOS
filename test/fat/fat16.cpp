21aa82a189c7892ae0e6fc0c668a9ad3e5cc3f03
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-04-04 10:04:18 +0200

    Save the FAT-tests ascii art from future formatting

--- test/fat/fat16.cpp
+++ test/fat/fat16.cpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,44 +21,47 @@
 
 #include <memdisk>
 
+// Includes std::string internal_banana
+#include "banana.ascii"
+
 void Service::start()
 {
   INFO("FAT16", "Running tests for FAT16");
   auto disk = fs::new_shared_memdisk();
   assert(disk);
-  
+
   // verify that the size is indeed N sectors
   CHECKSERT(disk->dev().size() == 16500, "Disk size 16500 sectors");
-  
+
   // which means that the disk can't be empty
   CHECKSERT(!disk->empty(), "Disk not empty");
-  
+
   // auto-mount filesystem
   disk->mount(
               [disk] (fs::error_t err)
               {
                 CHECKSERT(!err, "Filesystem auto-mounted");
-    
+
                 auto& fs = disk->fs();
                 printf("\t\t%s filesystem\n", fs.name().c_str());
-    
+
                 auto vec = fs::new_shared_vector();
                 err = fs.ls("/", vec);
                 CHECKSERT(!err, "List root directory");
-    
+
                 CHECKSERT(vec->size() == 1, "Exactly one ent in root dir");
-    
+
                 auto& e = vec->at(0);
                 CHECKSERT(e.is_file(), "Ent is a file");
                 CHECKSERT(e.name() == "banana.txt", "Ents name is 'banana.txt'");
-    
+
               });
   // re-mount on VBR1
   disk->mount(disk->VBR1,
               [disk] (fs::error_t err)
               {
                 CHECKSERT(!err, "Filesystem mounted on VBR1");
-    
+
                 // verify that we can read file
                 auto& fs = disk->fs();
                 auto ent = fs.stat("/banana.txt");
@@ -66,39 +69,24 @@ void Service::start()
                 CHECKSERT(ent.is_file(), "Entity is file");
                 CHECKSERT(!ent.is_dir(), "Entity is not directory");
                 CHECKSERT(ent.name() == "banana.txt", "Name is 'banana.txt'");
-    
-                std::string internal_banana = 
-                  R"(     ____                           ___
-    |  _ \  ___              _   _.' _ `.
- _  | [_) )' _ `._   _  ___ ! \ | | (_) |    _
-|:;.|  _ <| (_) | \ | |' _ `|  \| |  _  |  .:;|
-|   `.[_) )  _  |  \| | (_) |     | | | |.',..|
-':.   `. /| | | |     |  _  | |\  | | |.' :;::'
- !::,   `-!_| | | |\  | | | | | \ !_!.'   ':;!
- !::;       ":;:!.!.\_!_!_!.!-'-':;:''    '''!
- ';:'        `::;::;'             ''     .,  .
-      `:     .,.    `'    .::... .      .::;::;'
-      `..:;::;:..      ::;::;:;:;,    :;::;'
-       "-:;::;:;:      ':;::;:''     ;.-'
-           ""`---...________...---'""
-                                         )";
-    printf("%s\n", internal_banana.c_str());
-    
+
+                printf("%s\n", internal_banana.c_str());
+
                 // try reading banana-file
                 auto buf = fs.read(ent, 0, ent.size);
                 auto banana = buf.to_string();
-    
+
     CHECKSERT(banana == internal_banana, "Correct banana #1");
-    
+
     bool test = true;
-    
+
     for (size_t i = 0; i < internal_banana.size(); i++)
     {
       // read one byte at a time
       buf = fs.read(ent, i, 1);
       /// @buf should evaluate to 'true' if its valid
       CHECKSERT(buf, "Validate buffer");
-      
+
       // verify that it matches the same location in test-string
       test = ((char) buf.buffer.get()[0] == internal_banana[i]);
       if (!test)
@@ -108,11 +96,11 @@ void Service::start()
       }
     }
     CHECKSERT(test, "Validate random access sync read");
-    
+
     buf = fs.readFile("/banana.txt");
     banana = buf.to_string();
     CHECKSERT(banana == internal_banana, "Correct banana #2");
   });
-  
+
   INFO("FAT16", "SUCCESS");
 }
