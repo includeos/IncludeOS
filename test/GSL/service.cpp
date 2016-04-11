e014d280d79cd99405fa87b87deadb8a2f0ea1de
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-04-11 14:43:45 +0200

    Lest now taks command line params as vector

--- test/GSL/service.cpp
+++ test/GSL/service.cpp
@@ -189,10 +189,8 @@ const lest::test test_basic_gsl[] = {
 void Service::start()
 {
 
-  // Lest takes command line params
-  char* argv[2] = {(char*)"test", (char*)"-p"};
-
-  auto failed = lest::run(test_basic_gsl, 2, argv);
+  // Lest takes command line params as vector
+  auto failed = lest::run(test_basic_gsl, {"-p"});
 
   assert(not failed);
 
