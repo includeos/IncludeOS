0f682371dd97495d90a6c90474afa1e7aa557a70
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-04-12 09:56:08 +0200

    example: repaired demo example

--- examples/demo_service/service.cpp
+++ examples/demo_service/service.cpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,23 +26,57 @@ std::unique_ptr<net::Inet4<VirtioNet> > inet;
 
 using namespace std::chrono;
 
+std::string HTML_RESPONSE() {
+  int color = rand();
+  std::stringstream stream;
+
+  /* HTML Fonts */
+  std::string ubuntu_medium  = "font-family: \'Ubuntu\', sans-serif; font-weight: 500; ";
+  std::string ubuntu_normal  = "font-family: \'Ubuntu\', sans-serif; font-weight: 400; ";
+  std::string ubuntu_light  = "font-family: \'Ubuntu\', sans-serif; font-weight: 300; ";
+
+  /* HTML */
+  stream << "<html><head>"
+         << "<link href='https://fonts.googleapis.com/css?family=Ubuntu:500,300' rel='stylesheet' type='text/css'>"
+         << "</head><body>"
+         << "<h1 style= \"color: " << "#" << std::hex << (color >> 8) << "\">"
+         <<  "<span style=\""+ubuntu_medium+"\">Include</span><span style=\""+ubuntu_light+"\">OS</span> </h1>"
+         <<  "<h2>Now speaks TCP!</h2>"
+    // .... generate more dynamic content
+         << "<p>  ...and can improvise http. With limitations of course, but it's been easier than expected so far </p>"
+         << "<footer><hr /> &copy; 2015, Oslo and Akershus University College of Applied Sciences </footer>"
+         << "</body></html>\n";
+
+  std::string html = stream.str();
+
+  std::string header="HTTP/1.1 200 OK \n "        \
+    "Date: Mon, 01 Jan 1970 00:00:01 GMT \n"      \
+    "Server: IncludeOS prototype 4.0 \n"        \
+    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT \n"   \
+    "Content-Type: text/html; charset=UTF-8 \n"     \
+    "Content-Length: "+std::to_string(html.size())+"\n"   \
+    "Accept-Ranges: bytes\n"          \
+    "Connection: close\n\n";
+  return header + html;
+}
+
 void Service::start() {
   // Assign a driver (VirtioNet) to a network interface (eth0)
   // @note: We could determine the appropirate driver dynamically, but then we'd
   // have to include all the drivers into the image, which  we want to avoid.
   hw::Nic<VirtioNet>& eth0 = hw::Dev::eth<0,VirtioNet>();
-  
+
   // Bring up a network stack, attached to the nic
   // @note : No parameters after 'nic' means we'll use DHCP for IP config.
   inet = std::make_unique<net::Inet4<VirtioNet> >(eth0);
-  
+
   // Static IP configuration, until we (possibly) get DHCP
   // @note : Mostly to get a robust demo service that it works with and without DHCP
   inet->network_config( {{ 10,0,0,42 }},      // IP
                         {{ 255,255,255,0 }},  // Netmask
                         {{ 10,0,0,1 }},       // Gateway
                         {{ 8,8,8,8 }} );      // DNS
-  
+
   srand(OS::cycles_since_boot());
 
   // Set up a TCP server on port 80
@@ -51,56 +85,33 @@ void Service::start() {
   hw::PIT::instance().onRepeatedTimeout(30s, []{
       printf("<Service> TCP STATUS:\n%s \n", inet->tcp().status().c_str());
     });
-  
+
   // Add a TCP connection handler - here a hardcoded HTTP-service
   server.onAccept([](auto conn) -> bool {
-      printf("<Service> @onAccept - Connection attempt from: %s \n", 
-             conn->to_string().c_str());
-      return true; // allow all connections
-      
-    }).onConnect([](auto) {
-        printf("<Service> @onConnect - Connection successfully established.\n");
+    printf("<Service> @onAccept - Connection attempt from: %s \n",
+           conn->to_string().c_str());
+    return true; // allow all connections
 
-      }).onReceive([](auto conn, bool push) {
-          std::string data = conn->read(1024);
-          printf("<Service> @onData - PUSH: %d, Data read: \n%s\n", push, data.c_str());
-          int color = rand();
-          std::stringstream stream;
- 
-          /* HTML Fonts */
-          std::string ubuntu_medium  = "font-family: \'Ubuntu\', sans-serif; font-weight: 500; ";
-          std::string ubuntu_normal  = "font-family: \'Ubuntu\', sans-serif; font-weight: 400; ";
-          std::string ubuntu_light  = "font-family: \'Ubuntu\', sans-serif; font-weight: 300; ";
-      
-          /* HTML */
-          stream << "<html><head>"
-                 << "<link href='https://fonts.googleapis.com/css?family=Ubuntu:500,300' rel='stylesheet' type='text/css'>"
-                 << "</head><body>"
-                 << "<h1 style= \"color: " << "#" << std::hex << (color >> 8) << "\">"  
-                 <<  "<span style=\""+ubuntu_medium+"\">Include</span><span style=\""+ubuntu_light+"\">OS</span> </h1>"
-                 <<  "<h2>Now speaks TCP!</h2>"
-            // .... generate more dynamic content 
-                 << "<p>  ...and can improvise http. With limitations of course, but it's been easier than expected so far </p>"
-                 << "<footer><hr /> &copy; 2015, Oslo and Akershus University College of Applied Sciences </footer>"
-                 << "</body></html>\n";
-      
-          /* HTTP-header */
-          std::string html = stream.str();
-          std::string header="HTTP/1.1 200 OK \n "        \
-            "Date: Mon, 01 Jan 1970 00:00:01 GMT \n"      \
-            "Server: IncludeOS prototype 4.0 \n"        \
-            "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT \n"   \
-            "Content-Type: text/html; charset=UTF-8 \n"     \
-            "Content-Length: "+std::to_string(html.size())+"\n"   \
-            "Accept-Ranges: bytes\n"          \
-            "Connection: close\n\n";
-      
-          std::string output{header + html};
-          conn->write(output.data(), output.size());
+  }).onConnect([](auto conn) {
+      printf("<Service> @onConnect - Connection successfully established.\n");
+      // read async with a buffer size of 1024 bytes
+      // define what to do when data is read
+      conn->read(1024, [conn](net::TCP::buffer_t buf, size_t n) {
+        // create string from buffer
+        std::string data { (char*)buf.get(), n };
+        printf("<Service> @read:\n%s\n", data.c_str());
 
-        }).onDisconnect([](auto, auto reason) {
-            printf("<Service> @onDisconnect - Reason: %s \n", reason.to_string().c_str());
-          });
+        // create response
+        std::string response = HTML_RESPONSE();
+        // write the data from the string with the strings size
+        conn->write(response.data(), response.size(), [](size_t n) {
+          printf("<Service> @write: %u bytes written\n", n);
+        });
+      });
+
+  }).onDisconnect([](auto, auto reason) {
+      printf("<Service> @onDisconnect - Reason: %s \n", reason.to_string().c_str());
+  });
 
   printf("*** TEST SERVICE STARTED *** \n");
 }
