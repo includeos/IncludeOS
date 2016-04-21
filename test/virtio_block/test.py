739647ee6f20aa3b397f814d63613252c69ee805
fwsgonzo <fwsgonzo@hotmail.com>
2016-04-21 17:02:25 +0200

    test: VirtioBlk test now reads a file!

--- test/virtio_block/test.py
+++ test/virtio_block/test.py
@@ -1,7 +1,14 @@
 #! /usr/bin/python
-
 import sys
+import subprocess
+
 sys.path.insert(0,"..")
+subprocess.call(['./image.sh'])
+
+def cleanup():
+  subprocess.call(['./cleanup.sh'])
 
 import vmrunner
+vmrunner.on_success = cleanup
+vmrunner.on_panic = cleanup
 vmrunner.vms[0].boot()
