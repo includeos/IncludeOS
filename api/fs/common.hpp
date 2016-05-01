068fddce70486ffdcecb7bb5a2f8675647b1b771
RicoAntonioFelix <ricoantoniofelix@yahoo.com>
2016-04-30 23:03:24 -0400

    Augmented filesystem components...

--- api/fs/common.hpp
+++ api/fs/common.hpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,76 +24,172 @@
 
 namespace fs {
 
-  typedef std::shared_ptr<uint8_t> buffer_t;
+  ///
+  /// Underlying type for filesystem buffer representation
+  ///
+  using buffer_t = std::shared_ptr<uint8_t>;
 
-  struct error_t
-  {
+  ///
+  /// Data-structure used to represent a filesystem error
+  ///
+  struct error_t {
+
+    ///
+    /// Various types of errors that can occur during filesystem
+    /// operations
+    ///
     enum token_t {
       NO_ERR = 0,
-      E_IO, // general I/O error
+      E_IO, // General I/O error
       E_MNT,
-      
+
       E_NOENT,
       E_NOTDIR,
     };
-    
+
+    ///
+    /// Constructor to create a filesystem error object
+    ///
+    /// @param tk
+    ///   Type of error
+    ///
+    /// @param rsn
+    ///   Reason for the error
+    ///
     error_t(token_t tk, const std::string& rsn)
-      : token_(tk), reason_(rsn) {}
-    
-    // error code to string
+      : token_(tk)
+      , reason_(rsn)
+    {}
+
+    ///
+    /// Get a human-readable representation for the type of error
+    ///
+    /// @return
+    ///   An STL-string describing the error
+    ///
     std::string token() const noexcept;
-    // show explanation for error
-    std::string reason() const noexcept {
-      return reason_;
-    }
-    
-    // returns "description": "reason"
-    std::string to_string() const noexcept {
-      return token() + ": " + reason();
-    }
-    
-    // returns true when it's an error
-    operator bool () const noexcept {
-      return token_ != NO_ERR;
-    }
-    
+
+    ///
+    /// Get an explanation for the generated error
+    ///
+    /// @return
+    ///   An STL-string explaining the reason why the error was generated
+    ///
+    std::string reason() const noexcept
+    { return reason_; }
+
+    ///
+    /// Get a string representation of this data-structure
+    ///
+    /// @return
+    ///   An STL-string in the following format:
+    ///     "description: reason"
+    ///
+    std::string to_string() const noexcept
+    { return token() + ": " + reason(); }
+
+    ///
+    /// Conversion of this data-structure to a boolean value
+    ///
+    /// @return
+    ///   true when it's an error, false otherwise
+    ///
+    operator bool () const noexcept
+    { return token_ != NO_ERR; }
+
   private:
-    token_t     token_;
-    std::string reason_;
-  };
-  
-  struct Buffer
-  {
-    Buffer(error_t e, buffer_t b, size_t l)
-      : err(e), buffer(b), len(l) {}
-      
-    // returns true if this buffer is valid
-    bool is_valid() const noexcept {
-      return buffer != nullptr;
-    }
-    operator bool () const noexcept {
-      return is_valid();
-    }
-    
-    uint8_t* data() {
-      return buffer.get();
-    }
-    size_t   size() const noexcept {
-      return len;
-    }
-    
-    // create a std::string from the stored buffer and return it
-    std::string to_string() const noexcept {
-      return std::string((char*) buffer.get(), size());
-    }
-    
-    error_t  err;
-    buffer_t buffer;
-    uint64_t len;
-  };
-  
-  /** @var no_error: Always returns boolean false when used in expressions */
-  extern error_t no_error;
+    const token_t     token_;
+    const std::string reason_;
+  }; //< struct error_t
+
+  ///
+  /// Data-structure for filesystem buffer representation
+  ///
+  struct Buffer {
+
+    ///
+    /// Constructor to create a filesystem buffer object
+    ///
+    /// @param e
+    ///   An error object to represent the state of the buffer
+    ///
+    /// @param b
+    ///   The underlying buffer to encapsulate
+    ///
+    /// @param s
+    ///   The size of the underlying buffer in bytes
+    ///
+    explicit Buffer(error_t e, buffer_t b, size_t s)
+      : err_(e)
+      , buffer_(b)
+      , size_(s)
+    {}
+
+    ///
+    /// Check to see if the buffer is in a valid state
+    ///
+    /// @return
+    ///   true if this buffer is valid, false otherwise
+    ///
+    bool is_valid() const noexcept
+    { return (buffer_ not_eq nullptr) and (err_ == false; }
+
+    ///
+    /// Conversion of this data-structure to a boolean value
+    ///
+    /// @return
+    ///   true if this buffer is valid, false otherwise
+    ///
+    operator bool () const noexcept
+    { return is_valid(); }
+
+    ///
+    /// Get the object that represents the state of the buffer
+    ///
+    /// @return
+    ///   The object that represents the state of the buffer
+    ///
+    error_t err() const noexcept
+    { return err_; }
+
+    ///
+    /// Get a raw handle (pointer) to the underlying buffer
+    ///
+    /// @return
+    ///   A raw handle (pointer) to the underlying buffer
+    ///
+    uint8_t* data()
+    { return buffer_.get(); }
+
+    ///
+    /// Get the size of the underlying buffer
+    ///
+    /// @return
+    ///   The size of the underlying buffer in bytes
+    ///
+    size_t size() const noexcept
+    { return size_; }
+
+    ///
+    /// Get a string representation of this data-structure
+    ///
+    /// @return
+    ///   An STL-string that contains the information in the underlying buffer
+    ///
+    std::string to_string() const noexcept
+    { return std::string{static_cast<char*>(buffer_.get()), size()}; }
+
+  private:
+    const error_t  err_;
+    buffer_t       buffer_;
+    const uint64_t size_;
+  }; //< struct Buffer
+
+  ///
+  /// @var no_error
+  ///   Always returns a boolean value of false when used in expressions
+  ///
+  extern const error_t no_error;
 
 } //< namespace fs
 
--- api/fs/filesystem.hpp
+++ api/fs/filesystem.hpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,30 +27,29 @@
 #include <functional>
 
 namespace fs {
-  
+
   class FileSystem {
   public:
     struct Dirent; //< Generic structure for directory entries
-  
+
     using dirvector = std::vector<Dirent>;
     using dirvec_t  = std::shared_ptr<dirvector>;
-    using buffer_t  = std::shared_ptr<uint8_t>;
-  
+
     using on_mount_func = std::function<void(error_t)>;
     using on_ls_func    = std::function<void(error_t, dirvec_t)>;
     using on_read_func  = std::function<void(error_t, buffer_t, uint64_t)>;
     using on_stat_func  = std::function<void(error_t, const Dirent&)>;
-  
+
     enum Enttype {
       FILE,
       DIR,
       /** FAT puts disk labels in the root directory, hence: */
       VOLUME_ID,
       SYM_LINK,
-    
+
       INVALID_ENTITY
     }; //< enum Enttype
-  
+
     /** Generic structure for directory entries */
     struct Dirent {
       /** Default constructor */
@@ -65,25 +64,25 @@ namespace fs {
         attrib    {attr},
         timestamp {0}
       {}
-    
+
       Enttype type() const noexcept
       { return ftype; }
-    
+
       // true if this dirent is valid
       // if not, it means don't read any values from the Dirent as they are not
       bool is_valid() const
       { return ftype != INVALID_ENTITY; }
-    
+
       // most common types
       bool is_file() const noexcept
       { return ftype == FILE; }
       bool is_dir() const noexcept
       { return ftype == DIR; }
-    
+
       // the entrys name
       const std::string& name() const noexcept
       { return fname; }
-    
+
       // type converted to human-readable string
       std::string type_string() const {
         switch (ftype) {
@@ -93,14 +92,14 @@ namespace fs {
           return "Directory";
         case VOLUME_ID:
           return "Volume ID";
-        
+
         case INVALID_ENTITY:
           return "Invalid entity";
         default:
           return "Unknown type";
         } //< switch (type)
       }
-      
+
       Enttype     ftype;
       std::string fname;
       uint64_t    block;
@@ -109,26 +108,26 @@ namespace fs {
       uint32_t    attrib;
       int64_t     timestamp;
     }; //< struct Dirent
-  
+
     /** Mount this filesystem with LBA at @base_sector */
     virtual void mount(uint64_t lba, uint64_t size, on_mount_func on_mount) = 0;
-    
+
     /** @param path: Path in the mounted filesystem */
     virtual void    ls(const std::string& path, on_ls_func) = 0;
     virtual error_t ls(const std::string& path, dirvec_t e) = 0;
-    
+
     /** Read an entire file into a buffer, then call on_read */
     virtual void   readFile(const std::string&, on_read_func) = 0;
     virtual Buffer readFile(const std::string&) = 0;
-    
+
     /** Read @n bytes from direntry from position @pos */
     virtual void   read(const Dirent&, uint64_t pos, uint64_t n, on_read_func) = 0;
     virtual Buffer read(const Dirent&, uint64_t pos, uint64_t n) = 0;
-    
+
     /** Return information about a file or directory */
     virtual void   stat(const std::string& ent, on_stat_func) = 0;
     virtual Dirent stat(const std::string& ent) = 0;
-    
+
     /** Returns the name of this filesystem */
     virtual std::string name() const = 0;
 
@@ -141,9 +140,9 @@ namespace fs {
   {
     return std::make_shared<FileSystem::dirvector> ();
   }
-  
+
   using Dirent = FileSystem::Dirent;
-  
+
 } //< namespace fs
 
 #endif //< FS_FILESYSTEM_HPP
