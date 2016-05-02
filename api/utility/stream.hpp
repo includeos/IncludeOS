36062c6aa3bb78931de781665d360d654a06a1e3
fwsgonzo <fwsgonzo@hotmail.com>
2016-05-02 16:24:11 +0200

    stream: Add disk_transfer, rename to Async

--- api/utility/stream.hpp
+++ api/utility/stream.hpp
@@ -6,7 +6,7 @@
 #include <fs/disk.hpp>
 #include <functional>
 
-class Stream
+class Async
 {
   static const size_t PAYLOAD_SIZE = 64000;
   
@@ -14,14 +14,24 @@ class Stream
   typedef fs::Disk_ptr             Disk;
   typedef fs::FileSystem::Dirent   Dirent;
   
+  typedef std::function<void(bool)> next_func;
   typedef std::function<void(fs::error_t, bool)> on_after_func;
+  typedef std::function<void(fs::buffer_t, size_t, next_func)> on_write_func;
   
-  static void upload(
-      Connection, 
+  static void upload_file(
       Disk, 
       const Dirent&, 
+      Connection, 
       on_after_func, 
       size_t = PAYLOAD_SIZE);
+  
+  static void disk_transfer(
+      Disk, 
+      const Dirent&, 
+      on_write_func, 
+      on_after_func, 
+      size_t = PAYLOAD_SIZE);
+  
 };
 
 #endif
--- src/util/stream.cpp
+++ src/util/stream.cpp
@@ -2,10 +2,37 @@
 
 #include <memory>
 
-void Stream::upload(
-    Connection    conn, 
+#define likely(x)       __builtin_expect(!!(x), 1)
+#define unlikely(x)     __builtin_expect(!!(x), 0)
+
+void Async::upload_file(
     Disk          disk, 
     const Dirent& ent, 
+    Connection    conn, 
+    on_after_func callback,
+    const size_t  CHUNK_SIZE)
+{
+  disk_transfer(disk, ent,
+  [conn, callback, CHUNK_SIZE] (fs::buffer_t buffer, 
+                                size_t       length, 
+                                next_func    next)
+  {
+    // write chunk to TCP connection
+    conn->write(buffer.get(), length, 
+    [callback, CHUNK_SIZE, next] (size_t n) {
+      
+      // if all data written, go to next chunk
+      next(n == CHUNK_SIZE);
+      
+    }, true);
+    
+  }, callback, CHUNK_SIZE);
+}
+
+void Async::disk_transfer(
+    Disk          disk, 
+    const Dirent& ent, 
+    on_write_func write_func, 
     on_after_func callback,
     const size_t  CHUNK_SIZE)
 {
@@ -13,7 +40,7 @@ void Stream::upload(
   auto next = std::make_shared<next_func_t> ();
   
   *next =
-  [next, conn, disk, ent, callback, CHUNK_SIZE] (size_t pos) {
+  [next, disk, ent, write_func, callback, CHUNK_SIZE] (size_t pos) {
     
     // number of write calls necessary
     const size_t writes = ent.size / CHUNK_SIZE;
@@ -26,33 +53,30 @@ void Stream::upload(
     
     // read chunk from file
     disk->fs().read(ent, pos * CHUNK_SIZE, CHUNK_SIZE,
-    [next, pos, conn, ent, callback] (
+    [next, pos, write_func, callback, CHUNK_SIZE] (
         fs::error_t  err, 
         fs::buffer_t buffer, 
         uint64_t     length)
     {
       if (err) {
-        printf("BAD\n");
+        printf("%s\n", err.to_string().c_str());
         callback(err, false);
         return;
       }
       
-      // write chunk to TCP connection
-      conn->write(buffer.get(), length, 
-      [next, pos, ent, callback] (size_t n) {
-        
-        // if all data written, go to next chunk
-        if (n == ent.size)
+      // call write callback with data
+      write_func(buffer, length,
+      [next, pos, callback] (bool good) {
+        // if the write succeeded, call next
+        if (likely(good))
           (*next)(pos+1);
         else
           // otherwise, fail
           callback(fs::no_error, false);
-        
-      }, true);
-      
+      });
     });
     
   };
-  
+  // start async loop
   (*next)(0);
 }
