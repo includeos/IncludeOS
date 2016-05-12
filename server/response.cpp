53c204fd1372f9438783495306f4e3b56280ca8f
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-05-12 14:08:09 +0200

    Reduced Response responsibility

--- server/response.cpp
+++ server/response.cpp
@@ -31,29 +31,14 @@ void Response::write_to_conn(bool close_on_written) const {
 
 void Response::send_code(const Code code) {
   set_status_code(code);
-  send();
+  send(!keep_alive);
 }
 
 void Response::send_file(const File& file) {
-  auto& fname = file.entry.fname;
+  auto& entry = file.entry;
 
   /* Content Length */
-  add_header(http::header_fields::Entity::Content_Length, std::to_string(file.entry.size()));
-
-  /* Setup MIME type */
-  std::string ext = "txt"; // fallback
-  http::Mime_Type mime;
-  // find dot before extension
-  auto ext_i = fname.find_last_of(".");
-  // extension found
-  if(ext_i != std::string::npos) {
-    // get the extension
-    ext = fname.substr(ext_i + 1);
-  }
-  // find the correct mime type
-  mime = http::extension_to_type(ext);
-
-  add_header(http::header_fields::Entity::Content_Type, mime);
+  add_header(http::header_fields::Entity::Content_Length, std::to_string(entry.size()));
 
   /* Send header */
   auto res = to_string();
@@ -61,18 +46,20 @@ void Response::send_file(const File& file) {
 
   /* Send file over connection */
   auto conn = conn_;
-  printf("<Response::send_file> Asking to send %llu bytes.\n", file.entry.size());
+  printf("<Response> Sending file: %s (%llu B).\n",
+    entry.name().c_str(), entry.size());
+
   Async::upload_file(file.disk, file.entry, conn,
-    [conn](fs::error_t err, bool good)
+    [conn, entry](fs::error_t err, bool good)
   {
       if(good) {
-        printf("<Response::send_file> %s - Success!\n",
-          conn->to_string().c_str());
+        printf("<Response> Success sending %s => %s\n",
+          entry.name().c_str(), conn->remote().to_string().c_str());
         //conn->close();
       }
       else {
-        printf("<Response::send_file> %s - Error: %s\n",
-          conn->to_string().c_str(), err.to_string().c_str());
+        printf("<Response> Error sending %s => %s [%s]\n",
+          entry.name().c_str(), conn->remote().to_string().c_str(), err.to_string().c_str());
       }
   });
 
