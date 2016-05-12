f394c60b7f6b8f97a0f97c875387b0d9d832d701
Martin Nordsletten <mnordsletten@gmail.com>
2016-05-12 18:00:00 +0200

    Added option to get only name of complete tests
    Used by jenkins to only run tests that are ready to run. This means that as soon as tests are complete they will be added to jenkins.

--- test/validate_all.sh
+++ test/validate_all.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-for t in `ls -d */`; do ./validate_test.py $t; done
+if [ $# -eq 0 ]
+    then
+    for t in `ls -d */`; do ./validate_test.py $t; done
+    exit
+fi
+
+for t in `ls -d */`; do ./validate_test.py $t | grep PASS | rev | cut -d "/" -f1 | rev; done
