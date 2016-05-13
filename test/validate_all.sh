3c2a3dbfc19c1e71db593ab4b1b70c169425bc6f
Martin Nordsletten <mnordsletten@gmail.com>
2016-05-13 15:12:57 +0200

    Made validate_all.sh a little more easy to read
    Added an actual name to the option.

--- test/validate_all.sh
+++ test/validate_all.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if [ $# -eq 0 ]
+if [ "$1" == "-onlyNames" ]
     then
-    for t in `ls -d */`; do ./validate_test.py $t; done
+    for t in `ls -d */`; do ./validate_test.py $t | grep PASS | rev | cut -d "/" -f1 | rev; done
     exit
 fi
 
-for t in `ls -d */`; do ./validate_test.py $t | grep PASS | rev | cut -d "/" -f1 | rev; done
+for t in `ls -d */`; do ./validate_test.py $t; done
