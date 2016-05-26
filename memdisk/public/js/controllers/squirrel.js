85a8370e5cb451c8664e8f4c6af3d4613b733bae
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-05-26 16:42:12 +0200

    Create Squirrel for front end

--- memdisk/public/js/controllers/squirrel.js
+++ memdisk/public/js/controllers/squirrel.js
@@ -3,7 +3,19 @@
 angular.module('acornWebApp')
   .controller('SquirrelCtrl', ['$scope', 'Squirrel', function($scope, Squirrel) {
 
+    $scope.squirrel = new Squirrel();
     $scope.squirrels = [];
 
     $scope.squirrels = Squirrel.query(function() {});
+
+    $scope.create = function() {
+      $scope.squirrel.$save(
+        function(squirrel, headers) {
+
+        },
+        function(error) {
+          console.log("error");
+        }
+      );
+    };
   }]);
