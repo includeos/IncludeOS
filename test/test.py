c85b58a09a82b8021d170b17bf23d0c293a9bbba
Martin Nordsletten <mnordsletten@gmail.com>
2016-06-30 08:22:45 +0000

    Now exits with proper exit code if one of the tests fail

--- test/test.py
+++ test/test.py
@@ -2,11 +2,10 @@
 
 import subprocess
 import sys
-import time
 import os
 import argparse
 
-sys.path.insert(0,".")
+sys.path.insert(0, ".")
 
 """
 Script used for running all the valid tests in the terminal.
@@ -14,7 +13,7 @@ Script used for running all the valid tests in the terminal.
 
 parser = argparse.ArgumentParser(description="Runs all the tests that are \
                                   ready in the test folder")
-parser.add_argument("-s", "--skip_test", nargs="*", dest="skip", \
+parser.add_argument("-s", "--skip_test", nargs="*", dest="skip",
                     help="Tests to skip")
 
 args = parser.parse_args()
@@ -32,6 +31,7 @@ def valid_tests():
                 valid_tests.remove(test_to_skip)
     return valid_tests
 
+
 def print_result(result_list):
     """ Used for printing the result of the tests performed """
 
@@ -45,18 +45,22 @@ def print_result(result_list):
 
         print '{0:15} ==> {3} {1} {2}'.format(name, result, background_color_end, color)
 
+
 def main():
     """
     Loops over all valid tests as defined by the ./validate_all.sh script. Runs them one by one and gives an update of the statuses at the end.
     """
 
-    print ">>> This script runs all the valid tests in the tests folder. These are given by the ./validate_all.sh script"
+    print ">>> This script runs all the valid tests in the tests folder."
+    print ">>> These are found by the ./validate_all.sh script"
+    print ">>> To skip certain tests use -s <tests to skip>\n\n"
     print ">>> Will perform the following tests:"
     for test in valid_tests():
         print test,
     print "\n"
 
     result_list = []
+    all_tests_pass = True
     for test in valid_tests():
         print ">> Now testing {0}".format(test)
 
@@ -72,11 +76,21 @@ def main():
             test_result = "PASS"
         else:
             test_result = "FAIL"
+            print "one test failed, setting False"
+            all_tests_pass = False
 
         result_list.append((test, test_result))
         os.chdir("..")
 
     print_result(result_list)
+    print all_tests_pass
+    if not all_tests_pass:
+        print 'exiting with 1'
+        sys.exit(1)
+    else:
+        print 'exiting with 0'
+        sys.exit(0)
+
 
 if  __name__ == '__main__':
     main()
