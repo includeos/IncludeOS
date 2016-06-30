e0f4a7ff6284cb6f666fde55d8793e491e62f5d7
Martin Nordsletten <mnordsletten@gmail.com>
2016-06-30 14:10:47 +0000

    Added a relative path option instead

--- test/build/test.sh
+++ test/build/test.sh
@@ -3,13 +3,31 @@
 # Script that builds all the files in the example folder
 # Will print the output if the test fails
 
-INCLUDEOS_SRC=${INCLUDEOS_SRC-$HOME/IncludeOS}
+function getScriptAbsoluteDir {
+    # @description used to get the script path
+    # @param $1 the script $0 parameter
+    local script_invoke_path="$1"
+    local cwd=`pwd`
+
+    # absolute path ? if so, the first character is a /
+    if test "x${script_invoke_path:0:1}" = 'x/'
+    then
+        RESULT=`dirname "$script_invoke_path"`
+    else
+        RESULT=`dirname "$cwd/$script_invoke_path"`
+    fi
+}
+
+script_invoke_path="$0"
+script_name=`basename "$0"`
+getScriptAbsoluteDir "$script_invoke_path"
+script_absolute_dir=$RESULT
 
 errors_present=0
-
 echo -e ">>> Will now attempt to make all examples. Outpt from make will only be present if an error occured"
 
-for dir in `ls -d $INCLUDEOS_SRC/examples/*`
+#for dir in `ls -d $INCLUDEOS_SRC/examples/*`
+for dir in `ls -d $script_absolute_dir/../../examples/*`
 do
     cd $dir
     BASE=`basename $dir`
