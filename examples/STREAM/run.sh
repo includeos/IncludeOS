3fdd18bff6e2766b9475d4c6d21533d6fefae97c
Gonzo <fwsgonzo@hotmail.com>
2016-07-05 13:55:31 +0200

    STREAM: use OS uptime instead of C time

--- examples/STREAM/run.sh
+++ examples/STREAM/run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-MEM="-m 256" source ${INCLUDEOS_HOME-$HOME/IncludeOS_install}/etc/run.sh test_STREAM.img
+MEM="-m 256" source ${INCLUDEOS_HOME-$HOME/IncludeOS_install}/etc/run.sh STREAM.img
