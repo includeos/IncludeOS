5a4a7f2f0ec0fadebfa25daf1b4a86ac04bc74b6
Gonzo <fwsgonzo@hotmail.com>
2016-07-05 16:10:46 +0200

    test: Transmit now builds again

--- test/transmit/service.cpp
+++ test/transmit/service.cpp
@@ -24,30 +24,23 @@
 
 using namespace std;
 using namespace net;
-
-std::shared_ptr<net::Inet4<VirtioNet> > inet;
+auto& timer = hw::PIT::instance();
 
 void Service::start()
 {
-  // Assign an IP-address, using Hårek-mapping :-)
-  auto& eth0 = hw::Dev::eth<0,VirtioNet>();
-  auto& mac = eth0.mac();
-
-  inet = std::make_shared<net::Inet4<VirtioNet>>(eth0);
-  auto& timer = hw::PIT::instance();
-
-  inet->network_config({ mac.part[2],mac.part[3],mac.part[4],mac.part[5] },
-                       { 255,255,0,0 }, // Netmask
-                       { 10,0,0,1 }, // Gateway
-                       { 8,8,8,8 });  // DNS
+  static auto inet = 
+    net::new_ipv4_stack<>({ 10,0,0,42 },      // IP
+                          { 255,255,255,0 },  // Netmask
+                          { 10,0,0,1 });      // Gateway
 
   printf("Service IP address: %s \n", inet->ip_addr().str().c_str());
 
   // UDP
-  UDP::port_t port = 4242;
+  const UDP::port_t port = 4242;
   auto& conn = inet->udp().bind(port);
 
-  conn.on_read([&] (UDP::addr_t addr, UDP::port_t port, const char* data, int len) {
+  conn.on_read(
+  [&] (auto addr, auto port, const char* data, int len) {
       string received = std::string(data,len);
 
       if (received == "SUCCESS") {
@@ -66,53 +59,39 @@ void Service::start()
       // Send the first packet, and then wait for ARP
       conn.sendto(addr, port, first_reply.c_str(), first_reply.size());
 
-      timer.onTimeout(1s, [&conn, addr, port, data, len]() {
+      timer.on_timeout_ms(1s, 
+      [&conn, addr, port, data, len] {
+        size_t packetsize = inet->ip_obj().MDDS() - sizeof(UDP::udp_header);
+        INFO("Test 2", "Trying to transmit %i UDP packets of size %i at maximum throttle",
+             packets, packetsize);
 
-          auto bufcount = inet->buffers_available();
-          size_t packetsize = inet->ip_obj().MDDS() - sizeof(UDP::udp_header);
-          INFO("Test 2", "Trying to transmit %i UDP packets of size %i at maximum throttle",
-               packets, packetsize);
+        for (int i = 0; i < packets; i++) {
+          char c = (char)('A' + (i % 26));
+          string send (packetsize, c);
+          //printf("<TEST> %i nr. of  %c \n", send.size(), c);
+          conn.sendto(addr, port, send.c_str() , send.size());
+        }
 
-          for (int i = 0; i < packets; i++) {
-            char c = (char)('A' + (i % 26));
-            string send (packetsize, c);
-            //printf("<TEST> %i nr. of  %c \n", send.size(), c);
-            conn.sendto(addr, port, send.c_str() , send.size());
-          }
-
-          CHECK(1,"UDP-transmission didn't panic");
-          auto bufcount2 = inet->buffers_available();
-
-          INFO("UDP Transmision tests","OK");
-        });
+        CHECK(1,"UDP-transmission didn't panic");
+        INFO("UDP Transmision tests","OK");
+      });
     });
 
+  timer.on_timeout_ms(200ms,
+  [=] {
+    const int packets { 600 };
+    INFO("Test 1", "Trying to transmit %i ethernet packets at maximum throttle", packets);
+    for (int i=0; i < packets; i++){
+      auto pckt = inet->createPacket(inet->MTU());
+      Ethernet::header* hdr = reinterpret_cast<Ethernet::header*>(pckt->buffer());
+      hdr->dest.major = Ethernet::addr::BROADCAST_FRAME.major;
+      hdr->dest.minor = Ethernet::addr::BROADCAST_FRAME.minor;
+      hdr->type = Ethernet::ETH_ARP;
+      inet->link().transmit(pckt);
+    }
 
-  /*
-    WARNING: Subscribing to this event will overwrite the Inet delegate. So Don't
-
-    eth0.on_transmit_queue_available([](size_t s){
-    CHECKSERT(s,"There is now room for %i packets in transmit queue", s);
-    }); */
-
-  timer.onTimeout(200ms,[=](){
-      const int packets { 600 };
-      INFO("Test 1", "Trying to transmit %i ethernet packets at maximum throttle", packets);
-      for (int i=0; i < packets; i++){
-        auto pckt = inet->createPacket(inet->MTU());
-        Ethernet::header* hdr = reinterpret_cast<Ethernet::header*>(pckt->buffer());
-        hdr->dest.major = Ethernet::addr::BROADCAST_FRAME.major;
-        hdr->dest.minor = Ethernet::addr::BROADCAST_FRAME.minor;
-        hdr->type = Ethernet::ETH_ARP;
-        inet->link().transmit(pckt);
-      }
-
-      CHECK(1,"Transmission didn't panic");
-      INFO("Test 1", "Done. Send some UDP-data to %s:%i to continue test",
-           inet->ip_addr().str().c_str(), port);
-
-
-    });
-
-
+    CHECK(1,"Transmission didn't panic");
+    INFO("Test 1", "Done. Send some UDP-data to %s:%i to continue test",
+         inet->ip_addr().str().c_str(), port);
+  });
 }
