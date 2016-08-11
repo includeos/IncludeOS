8631f717b8855acff7cf2a11862feddc608e0e2b
Andreas Happe <andreashappe@snikt.net>
2016-08-11 19:30:04 +0200

    move linux build setup into own script and refactor install.sh

---
+++ etc/install_build_requirements.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Install dependencies
+
+SYSTEM=$1
+RELEASE=$2
+
+case $SYSTEM in
+    "Darwin")
+        return 0;
+        ;;
+    "Linux")
+        case $RELEASE in
+            "Ubuntu")
+                UBUNTU_VERSION=`lsb_release -rs`
+                if [ $(echo "$UBUNTU_VERSION < 16.04" | bc) -eq 1 ]; then
+                    clang_version="3.6"
+                else
+                    clang_version="3.8"
+                fi
+
+                DEPENDENCIES="curl make clang-$clang_version nasm bridge-utils qemu"
+                echo ">>> Installing dependencies (requires sudo):"
+                echo "    Packages: $DEPENDENCIES"
+                sudo apt-get update
+                sudo apt-get install -y $DEPENDENCIES
+                exit 0;
+                ;;
+            "Fedora")
+                DEPENDENCIES="curl make clang nasm bridge-utils qemu"
+                echo ">>> Installing dependencies (requires sudo):"
+                echo "    Packages: $DEPENDENCIES"
+                sudo dnf install $DEPENDENCIES
+                exit 0;
+                ;;
+        esac
+esac
+
+exit 1
