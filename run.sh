652d0ec6fa568fda83633f58ee389417737aaa15
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2016-08-14 20:33:54 -0400

    Updated build scripts

--- run.sh
+++ run.sh
@@ -1,19 +1,41 @@
-#! /bin/bash
+# This file is a part of the IncludeOS unikernel - www.includeos.org
+#
+# Copyright 2015-2016 Oslo and Akershus University College of Applied Sciences
+# and Alfred Bratterud
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Make mnt/ if not exist
+#!/bin/bash
+
+# Make mnt/ if it doesn't exist
 mkdir -p mnt
 
-if ! mountpoint -q -- mnt/ ;
+# Mount disk if not mounted
+if [ ! mountpoint -q -- mnt/ ];
 then
-  echo "Mounting memdisk.fat"
+  echo ">>> Mounting memdisk.fat"
   sudo mount -o rw memdisk.fat mnt/
   sync
 fi
 
+# Copy web content to mounted disk
 sudo cp -r disk1/. mnt/
 sync
+
+# Remove stale object file
 rm -f memdisk.o
 
+# Build service
 make -j
 export CPU=""
 export HDB=" -drive file=./memdisk.fat,if=virtio,media=disk "
--- setup.sh
+++ setup.sh
@@ -1,17 +1,42 @@
-#! /bin/bash
-FAT_DISK=disk1.fat
+# This file is a part of the IncludeOS unikernel - www.includeos.org
+#
+# Copyright 2015-2016 Oslo and Akershus University College of Applied Sciences
+# and Alfred Bratterud
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+# Specify the name of disk image for serving content
+FAT_DISK=memdisk.fat
+
+# Check if the specified disk image exists
+# If it doesn't then try to create it
 if [ ! -f $FAT_DISK ];
 then
   echo -e ">>> Creating FAT disk $FAT_DISK"
-  fallocate -l 100000000 $FAT_DISK
+  dd if=/dev/zero of=$FAT_DISK bs=1M seek=50 count=0
   mkfs.fat $FAT_DISK
 fi
 
+# Create a directory for mounting the disk image
 mkdir -p mnt
 
+# Try to mount the disk image
 echo -e ">>> Mounting $FAT_DISK in mnt/"
 sudo mount -o rw $FAT_DISK mnt/
 
+# Copy web content into the disk image
 echo -e ">>> Copy content from disk1/ to $FAT_DISK"
 sudo cp -r disk1/. mnt/
 sync
