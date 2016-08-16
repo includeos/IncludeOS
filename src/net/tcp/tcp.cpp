d51e0e71bd1a62fde7083c8a457d6b434a4b0be2
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-08-16 11:12:39 +0200

    tcp: Added getters for port and SynQ in Listener, reformat TCP::to_string

--- src/net/tcp/tcp.cpp
+++ src/net/tcp/tcp.cpp
@@ -252,9 +252,10 @@ size_t TCP::send(Connection_ptr conn, const char* buffer, size_t n) {
 string TCP::to_string() const {
   // Write all connections in a cute list.
   stringstream ss;
-  ss << "LISTENERS:\n";
+  ss << "LISTENERS:\n" << "Port\t" << "Queued\n";
   for(auto& listen_it : listeners_) {
-    ss << listen_it.second.to_string() << "\n";
+    auto& l = listen_it.second;
+    ss << l.port() << "\t" << l.syn_queue_size() << "\n";
   }
   ss << "\nCONNECTIONS:\n" <<  "Proto\tRecv\tSend\tIn\tOut\tLocal\t\t\tRemote\t\t\tState\n";
   for(auto& con_it : connections_) {
