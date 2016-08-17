076e530bcf14cc69d192b02c620d7e6c18089d48
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2016-08-17 13:18:12 -0400

    Added API DOC comment blocks

--- json.hpp
+++ json.hpp
@@ -15,12 +15,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef JSON_HPP
-#define JSON_HPP
+#ifndef JSON_JSON_HPP
+#define JSON_JSON_HPP
 
 #define RAPIDJSON_HAS_STDSTRING 1
 #define RAPIDJSON_THROWPARSEEXCEPTION 1
 
+/**
+ *
+ */
 class AssertException : public std::logic_error {
 public:
   AssertException(const char* w) : std::logic_error(w) {}
@@ -34,22 +37,36 @@ public:
 
 namespace json {
 
+/**
+ *
+ */
 struct Serializable {
+  /**
+   *
+   */
   virtual void serialize(rapidjson::Writer<rapidjson::StringBuffer>& writer) const = 0;
+
+  /**
+   *
+   */
   virtual bool deserialize(const rapidjson::Document& doc) = 0;
-};
+}; //< struct Serializable
 
-
+/**
+ *
+ */
 class JsonDoc : public server::Attribute {
 public:
+  /**
+   *
+   */
   rapidjson::Document& doc()
   { return document_; }
 
 private:
   rapidjson::Document document_;
+}; //< class JsonDoc
 
-};
+}; //< namespace json
 
-}; // < namespace json
-
-#endif
+#endif //< JSON_JSON_HPP
