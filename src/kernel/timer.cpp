043a8a6d5c0813fed40239076b404c70c6df140e
Gonzo <fwsgonzo@hotmail.com>
2016-08-17 14:51:31 +0200

    timers: Fix test again, because of RTC a timer is constantly alive

--- src/kernel/timer.cpp
+++ src/kernel/timer.cpp
@@ -136,8 +136,8 @@ void Timers::timers_handler()
   while (!scheduled.empty())
   {
     auto it = scheduled.begin();
-    auto when   = it->first;
-    id_t id = it->second;
+    auto when = it->first;
+    id_t id   = it->second;
     
     // remove dead timers
     if (timers[id].deferred_destruct) {
@@ -155,33 +155,26 @@ void Timers::timers_handler()
         // erase immediately
         scheduled.erase(it);
         
-        // only process timer if still alive
-        if (timers[id].is_alive()) {
-          // call the users callback function
-          timers[id].callback(id);
-          // if the timers struct was modified in callback, eg. due to
-          // creating a timer, then the timer reference below would have
-          // been invalidated, hence why its BELOW, AND MUST STAY THERE
-          auto& timer = timers[id];
-          
-          // oneshot timers are automatically freed
-          if (timer.deferred_destruct || timer.is_oneshot())
-          {
-            timer.reset();
-            free_timers.push_back(id);
-          }
-          else if (timer.is_oneshot() == false)
-          {
-            // if the timer is recurring, we will simply reschedule it
-            // NOTE: we are carefully using (when + period) to avoid drift
-            scheduled.emplace(std::piecewise_construct,
-                      std::forward_as_tuple(when + timer.period),
-                      std::forward_as_tuple(id));
-          }
-        } else {
-          // timer was already dead
-          timers[id].reset();
+        // call the users callback function
+        timers[id].callback(id);
+        // if the timers struct was modified in callback, eg. due to
+        // creating a timer, then the timer reference below would have
+        // been invalidated, hence why its BELOW, AND MUST STAY THERE
+        auto& timer = timers[id];
+        
+        // oneshot timers are automatically freed
+        if (timer.deferred_destruct || timer.is_oneshot())
+        {
+          timer.reset();
           free_timers.push_back(id);
+        }
+        else if (timer.is_oneshot() == false)
+        {
+          // if the timer is recurring, we will simply reschedule it
+          // NOTE: we are carefully using (when + period) to avoid drift
+          scheduled.emplace(std::piecewise_construct,
+                    std::forward_as_tuple(when + timer.period),
+                    std::forward_as_tuple(id));
         }
         
       } else {
--- test/kernel/integration/timers/timers.cpp
+++ test/kernel/integration/timers/timers.cpp
@@ -29,13 +29,15 @@ static int repeat2 = 0;
 void test_timers()
 {
   INFO("Timers", "Testing one-shot timers");
+  // RTC is using a timer to calibrate itself over large periods of time
+  assert(Timers::active() == 1);
   
   // 30 sec. - Test End
   Timers::oneshot(30s, [] (auto) {
       printf("One-shots fired: %i \n", one_shots);
       CHECKSERT(one_shots == 5, "5 one-shot-timers fired");
       CHECKSERT(repeat1 == 25 and repeat2 == 10, "1s. timer fired 25 times, 2s. timer fired 10 times");
-      CHECKSERT(Timers::active() == 0, "This was the last active timer");
+      CHECKSERT(Timers::active() == 1, "This was the last active timer (except RTC)");
       INFO("Timers", "SUCCESS");
     });
 
@@ -99,9 +101,13 @@ void test_timers()
                 "After 25 sec, 1s timer x 30 == %i times, 2s timer x 15 == %i times",
                 repeat1, repeat2);
 
-      // there are no timers active right now, because the current
-      // timer does not count towards the total active
-      CHECKSERT(Timers::active() == 0, "There are no timers left");
+      // Make sure this timer iterator is valid
+      Timers::stop(timer1s);
+      // there are still 3 timers left, because the stopped timer above
+      // will be removed deferred (when it would ordinarily trigger)
+      // ALSO: the current timer does not count towards the total active
+      // AND: RTC uses a timer to calibrate itself over time
+      CHECKSERT(Timers::active() == 3, "There are still 3 timers left");
 
       Timers::oneshot(1s, 
       [] (auto) {
