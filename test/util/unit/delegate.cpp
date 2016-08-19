c0a37359af7c65499e39892d04a3d63e7ea1210b
AnnikaH <annika_limfe@hotmail.com>
2016-08-19 14:16:15 +0200

    Started unit test utility/delegate.hpp

---
+++ test/util/unit/delegate.cpp
@@ -0,0 +1,133 @@
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2015-2016 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <common.cxx>
+#include <utility/delegate.hpp>
+
+CASE("A delegate can be compared with other delegates and itself")
+{
+  delegate<void(const std::string s)> d{};
+  delegate<void(const std::string s)> d2{};
+  delegate<void(const std::string s)> d3{};
+
+  EXPECT(d < d2);
+  EXPECT(d < d3);
+  EXPECT(d2 < d3);
+
+  EXPECT(d == d);
+  EXPECT(d != d2);
+  EXPECT(d != d3);
+  EXPECT(d2 != d3);
+
+  EXPECT_NOT(d == d2);
+  EXPECT_NOT(d == d3);
+  EXPECT_NOT(d == nullptr);
+}
+
+CASE("A delegate can be set to be the same as another delegate (= operator overload)")
+{
+  delegate<void(const std::string s)> d{};
+  delegate<void(const std::string s)> d2{};
+
+  EXPECT(d != d2);
+
+  d = d2;
+
+  EXPECT(d == d2);
+}
+
+CASE("A delegate can swap values with another delegate (1)")
+{
+  delegate<void(const std::string s)> d{};
+  delegate<void(const std::string s)> d2{};
+
+  EXPECT(d < d2);
+
+  d.swap(d2); // std::swap: the objects swap values (d becomes d2, d2 becomes d)
+
+  EXPECT(d2 < d);
+}
+
+CASE("A delegate can swap values with another delegate (2)")
+{
+  delegate<int()> d2 = []{ return 53; };
+  delegate<int()> d3 = []{ return 35; };
+
+  EXPECT(d2() == 53);
+  EXPECT(d3() == 35);
+
+  d2.swap(d3);
+
+  EXPECT(d2() == 35);
+  EXPECT(d3() == 53);
+}
+
+CASE("A delegate returns correct value (1)")
+{
+  std::vector<std::string> v = {"Something", "Something else"};
+  delegate<size_t()> d = [v]{ return v.size(); };
+
+  size_t v_size = v.size();
+  size_t d_vector_size = d();
+
+  EXPECT(v_size == 2u);
+  EXPECT(v_size == d_vector_size);
+}
+
+CASE("A delegate returns correct value (2)")
+{
+  std::vector<std::string> v2;
+  delegate<size_t()> d2 = [v2]{ return v2.size(); };
+
+  size_t v2_size = v2.size();
+  size_t d2_vector_size = d2();
+
+  EXPECT(v2_size == 0u);
+  EXPECT(v2_size == d2_vector_size);
+}
+
+CASE("A delegate returns correct value (3)")
+{
+  delegate<std::string(const int i)> d3 = [](const int i){ return std::to_string(i); };
+  int i = 12;
+  std::string d3_string = d3(i);
+
+  EXPECT(d3_string == "12");
+}
+
+CASE("A delegate can be true or false (bool operator overload) and when it is reset it is false")
+{
+  GIVEN("A delegate")
+  {
+    delegate<void(const char c)> d{};
+
+    THEN("The delegate is true")
+    {
+      EXPECT(d);
+    }
+
+    WHEN("A delegate is reset")
+    {
+      d.reset();
+
+      THEN("The delegate is false")
+      {
+        EXPECT(not d);
+      }
+    }
+  }
+}
