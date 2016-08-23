97ec41293f28f83f8a4d5922a9e847a803fe3dee
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-08-23 12:48:39 +0200

    tcp: Cleaned up Socket class

--- api/net/tcp/socket.hpp
+++ api/net/tcp/socket.hpp
@@ -19,72 +19,66 @@
 #ifndef NET_TCP_SOCKET_HPP
 #define NET_TCP_SOCKET_HPP
 
-#include <sstream>
 #include "common.hpp"
 
 namespace net {
 namespace tcp {
 
-/*
+/**
   An IP address and a Port.
 */
 class Socket {
 public:
-  /*
-    Intialize an empty socket.
-  */
-  inline Socket() : address_(), port_(0) { address_.whole = 0; };
+  /** Intialize an empty socket. */
+  Socket() : address_(), port_(0)
+  { address_.whole = 0; }
 
-  /*
-    Create a socket with a Address and Port.
-  */
-  inline Socket(Address address, port_t port) : address_(address), port_(port) {};
+  /** Create a socket with a Address and Port. */
+  Socket(Address address, port_t port)
+    : address_(address), port_(port)
+  {}
 
-  /*
-    Returns the Socket's address.
-  */
-  inline const Address address() const { return address_; }
+  /** Returns the Socket's address. */
+  const Address address() const
+  { return address_; }
 
-  /*
-    Returns the Socket's port.
-  */
-  inline port_t port() const { return port_; }
+  /** Returns the Socket's port. */
+  port_t port() const
+  { return port_; }
 
-  /*
-    Returns a string in the format "Address:Port".
-  */
-  std::string to_string() const {
-    std::stringstream ss;
-    ss << address_.str() << ":" << port_;
-    return ss.str();
-  }
+  /** Returns a string in the format "Address:Port". */
+  std::string to_string() const
+  { return address_.str() + ":" + std::to_string(port_); }
 
-  inline bool is_empty() const { return (address_.whole == 0 and port_ == 0); }
+  bool is_empty() const
+  { return (address_ == 0 and port_ == 0); }
 
-  /*
-    Comparator used for vector.
-  */
-  inline bool operator ==(const Socket &s2) const {
-    return address().whole == s2.address().whole
+  /** Standard comparators */
+  bool operator ==(const Socket& s2) const
+  {
+    return address() == s2.address()
       and port() == s2.port();
   }
 
-  /*
-    Comparator used for map.
-  */
-  inline bool operator <(const Socket& s2) const {
-    return address().whole < s2.address().whole
-                             or (address().whole == s2.address().whole and port() < s2.port());
+  bool operator <(const Socket& s2) const
+  {
+    return address() < s2.address()
+           or (address() == s2.address() and port() < s2.port());
   }
 
+  bool operator !=(const Socket& s2) const
+  { return !(*this == s2); }
+
+  bool operator >(const Socket& s2) const
+  { return !(*this < s2); }
+
 private:
-  //SocketID id_; // Maybe a hash or something. Not sure if needed (yet)
   Address address_;
   port_t port_;
 
 }; // < class Socket
 
-} // < namespace net
 } // < namespace tcp
+} // < namespace net
 
 #endif // < NET_TCP_SOCKET_HPP
