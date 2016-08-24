217f560b4fc8780fb228d72b6d57d7b62952342a
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-08-24 14:05:05 +0200

    Connection now closes on_close rather than on_disconnect

--- connection.cpp
+++ connection.cpp
@@ -29,11 +29,10 @@ Connection::Connection(Server& serv, Connection_ptr conn, size_t idx)
 {
   conn_->on_read(BUFSIZE, OnData::from<Connection, &Connection::on_data>(this));
   conn_->on_disconnect(OnDisconnect::from<Connection, &Connection::on_disconnect>(this));
+  conn_->on_close(OnClose::from<Connection, &Connection::close>(this));
   conn_->on_error(OnError::from<Connection, &Connection::on_error>(this));
-  conn_->on_rtx_timeout([](auto, auto) { printf("<TCP> RtxTimeout\n"); });
-  conn_->on_error([](auto err) { printf("<TCP> Error: %s\n", err.what()); });
+  //conn_->on_rtx_timeout([](auto, auto) { printf("<TCP> RtxTimeout\n"); });
   on_connection_();
-  //conn_->onPacketDropped(OnPacketDropped::from<Connection, &Connection::on_packet_dropped>(this));
 }
 
 void Connection::on_data(buffer_t buf, size_t n) {
@@ -92,7 +91,8 @@ void Connection::on_disconnect(Connection_ptr, Disconnect reason) {
   printf("<%s> Disconnect: %s\n",
     to_string().c_str(), reason.to_string().c_str());
   #endif
-  close();
+  if(!conn_->is_closing())
+    conn_->close();
 }
 
 void Connection::on_error(TCPException err) {
@@ -108,7 +108,6 @@ void Connection::on_packet_dropped(Packet_ptr, std::string reason) {
 void Connection::close() {
   request_ = nullptr;
   response_ = nullptr;
-  conn_->close();
   server_.close(idx_);
 }
 
--- connection.hpp
+++ connection.hpp
@@ -33,15 +33,16 @@ using Connection_ptr = std::shared_ptr<Connection>;
 class Connection {
 private:
   const static size_t BUFSIZE = 1460;
-  using Connection_ptr = net::tcp::Connection_ptr;
-  using buffer_t = net::tcp::buffer_t;
-  using OnData = net::tcp::Connection::ReadCallback;
-  using Disconnect = net::tcp::Connection::Disconnect;
-  using OnDisconnect = net::tcp::Connection::DisconnectCallback;
-  using OnError = net::tcp::Connection::ErrorCallback;
-  using TCPException = net::tcp::TCPException;
-  using OnPacketDropped = net::tcp::Connection::PacketDroppedCallback;
-  using Packet_ptr = net::tcp::Packet_ptr;
+  using Connection_ptr    = net::tcp::Connection_ptr;
+  using buffer_t          = net::tcp::buffer_t;
+  using OnData            = net::tcp::Connection::ReadCallback;
+  using Disconnect        = net::tcp::Connection::Disconnect;
+  using OnDisconnect      = net::tcp::Connection::DisconnectCallback;
+  using OnClose           = net::tcp::Connection::CloseCallback;
+  using OnError           = net::tcp::Connection::ErrorCallback;
+  using TCPException      = net::tcp::TCPException;
+  using OnPacketDropped   = net::tcp::Connection::PacketDroppedCallback;
+  using Packet_ptr        = net::tcp::Packet_ptr;
 
   using OnConnection = std::function<void()>;
 
