ec550d70eee75a1aa6bf470e9ee442e5e2164aab
Martin Nordsletten <mnordsletten@gmail.com>
2016-08-25 09:57:56 +0000

    all_tests: Now uses the new correct file structure for validate_all.sh

--- test/validate_all.sh
+++ test/validate_all.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 if [ "$1" == "-onlyNames" ]
     then
-    for t in `ls -d */`; do ./validate_test.py $t | grep PASS | rev | cut -d "/" -f1 | rev; done
+    for t in `ls -d */integration/*`; do ./validate_test.py $t | grep PASS | rev | cut -d " " -f1 | rev | while read x; do echo ${x##*/test/}; done; done
     exit
 fi
 
-for t in `ls -d */`; do ./validate_test.py $t; done
+for t in `ls -d */integration/*`; do ./validate_test.py $t; done
