b6f1500ae68cca227723784653ff0797787a6a33
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-09-01 12:42:07 +0200

    web: Now displays OS status

--- disk1/public/js/controllers/dashboard.js
+++ disk1/public/js/controllers/dashboard.js
@@ -7,10 +7,12 @@ angular.module('acornWebApp')
     $scope.memmap = [];
     $scope.statman = [];
     $scope.stack_sampler = [];
+    $scope.status = {};
 
     var data = Dashboard.query(function() {
       $scope.memmap = data.memmap;
       $scope.statman = data.statman;
       $scope.stack_sampler = data.stack_sampler;
+      $scope.status = data.status;
     });
   }]);
