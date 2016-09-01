dc021d8f4587f7073ed8fdd43d56ebf115c2e98e
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2016-09-01 10:46:14 -0400

    Modified includes in cookie_test.cpp

--- test/cookie/cookie_test.cpp
+++ test/cookie/cookie_test.cpp
@@ -15,8 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "../../cookie/cookie.hpp"
 #include "../lest/include/lest/lest.hpp"
-#include "../../cookie/cookie.cpp"
 
 using namespace std;
 using namespace cookie;
