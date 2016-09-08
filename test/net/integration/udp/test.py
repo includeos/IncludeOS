3257f73fb4bdc151b3aefb36c3adb23c20100b72
Martin Nordsletten <mnordsletten@gmail.com>
2016-09-08 08:14:41 +0000

    Tests: Removed need for specifying IncludeOS_SRC in integration tests

--- test/net/integration/udp/test.py
+++ test/net/integration/udp/test.py
@@ -3,7 +3,9 @@
 import sys
 import os
 
-includeos_src = os.environ['INCLUDEOS_SRC']
+includeos_src = os.environ.get('INCLUDEOS_SRC',
+                               os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))).split('/test')[0])
+print 'includeos_src: {0}'.format(includeos_src)
 sys.path.insert(0,includeos_src + "/test")
 
 import vmrunner
