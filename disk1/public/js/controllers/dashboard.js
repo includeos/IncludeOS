6956428154d0e14048a79e7d3fcc4ce07fd2854d
AnnikaH <annika_limfe@hotmail.com>
2016-09-08 13:16:03 +0200

    Dashboard controller and html: Cleanup and added IncludeOS colors to graphs

--- disk1/public/js/controllers/dashboard.js
+++ disk1/public/js/controllers/dashboard.js
@@ -2,27 +2,15 @@
 
 angular.module('acornWebApp')
   .controller('DashboardCtrl',
-    ['$scope', 'Dashboard', '$timeout', '$http', '$interval',
-    function($scope, Dashboard, $timeout, $http, $interval) {
-
-    // TODO: How access bytes-function in app.js here?
-    function bytes(bytes, precision) {
-      if (bytes === 0) { return '0 bytes' }
-      if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-      if (typeof precision === 'undefined') precision = 1;
-
-      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-          number = Math.floor(Math.log(bytes) / Math.log(1024)),
-          val = (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision);
-
-      return  (val.match(/\.0*$/) ? val.substr(0, val.indexOf('.')) : val) +  ' ' + units[number];
-    }
+    ['$scope', 'Dashboard', '$timeout', '$http', '$interval', 'bytesFilter',
+    function($scope, Dashboard, $timeout, $http, $interval, bytesFilter) {
 
     // CPU usage chart
     var date = new Date();
     var total_data = ['total'];
     var active_data = ['active'];
     var time_data = ['x', date];
+    var color_palette = ['#A061F2', '#3A8BF1', '#3452CB', '#0D1230', '#EE4053', '#F87E0C', '#F8D20B', '#B5D63B'];
 
     var cpu_usage_chart = c3.generate({
       bindto: '#cpu_usage_chart',
@@ -36,6 +24,10 @@ angular.module('acornWebApp')
           total_data,
           active_data
         ],
+        colors: {
+          total: '#3A8BF1',
+          active: '#F87E0C'
+        },
         types: {
           total: 'area-spline',
           active: 'area-spline'
@@ -75,19 +67,25 @@ angular.module('acornWebApp')
       success(function (memmap) {
         var memmap_columns = [];
         var groups = [];
-
-        // Only room for 10 elements in chart
-        /*var empty = ['Unused', memmap[0].addr_start];
-        memmap_columns.push(empty);
-        groups.push('Unused');*/
+        var group_with_colors = {};
+        var color_index = 0;
 
         angular.forEach(memmap, function(element, index) {
-          var element_data = [(index + 1) + ': ' + element.name, (element.addr_end - element.addr_start) + 0x1];
-          memmap_columns.push(element_data);
+          var element_name = (index + 1) + ': ' + element.name;
+          var element_data = [element_name, (element.addr_end - element.addr_start) + 0x1];
 
-          // If group name not identical with element name
-          // - one bar per element instead of one bar for all
-          groups.push((index + 1) + ': ' + element.name);
+          memmap_columns.push(element_data);
+          groups.push(element_name);
+
+          if(index < color_palette.length) {
+            group_with_colors[element_name] = color_palette[index];
+          }
+          else {
+            if(color_index >= color_palette.length)
+              color_index = 0;
+
+            group_with_colors[element_name] = color_palette[color_index++];
+          }
         });
 
         // Memory map chart
@@ -99,7 +97,7 @@ angular.module('acornWebApp')
           },
           data: {
             columns: memmap_columns,
-            // colors: {name: '#ff0000', name2: '#ff0000'},
+            colors: group_with_colors,
             type: 'bar',
             groups: [
               groups
@@ -108,14 +106,11 @@ angular.module('acornWebApp')
           },
           axis: {
             x: {
-              show: false,
-              label: {
-                text: "Elements"
-              }
+              show: false
             },
             y: {
               tick: {
-                format: function (y) { return bytes(y); }
+                format: function (y) { return bytesFilter(y); }
               },
               padding: {
                 top: 0
@@ -137,15 +132,12 @@ angular.module('acornWebApp')
                 var map_element = memmap[index];
                 return '0x' + map_element.addr_start.toString(16) +
                   ' - 0x' + map_element.addr_end.toString(16) + ' In use: ' +
-                  bytes(map_element.in_use) + ' (' + ((map_element.in_use / value)*100).toFixed(0) + '%)';
+                  bytesFilter(map_element.in_use) + ' (' + ((map_element.in_use / value)*100).toFixed(0) + '%)';
               }
             }
           }
         });
-      }).
-      error(function (data, status) {
-
-      });
+      }).error(function (data, status) {});
 
     // Update CPU usage chart at interval
     $scope.update_cpu_chart = function() {
@@ -173,11 +165,11 @@ angular.module('acornWebApp')
 
           var d = new Date();
 
-          total_data.push(total);
-          active_data.push(active);
+          total_data.push(total.toFixed(3));
+          active_data.push(active.toFixed(3));
           time_data.push(d);
 
-          cpu_usage_chart.axis.labels({x: 'CPU data updated at an interval of ' + interval + ' seconds'});
+          cpu_usage_chart.axis.labels({x: 'CPU data updated at an interval of ' + interval + ((interval > 1) ? ' seconds' : ' second')});
           cpu_usage_chart.load({
             columns: [
               time_data,
@@ -186,41 +178,10 @@ angular.module('acornWebApp')
             ]
           });
         }).
-        error(function (data, status) {
-
-        });
+        error(function (data, status) {});
     };
 
-    var cpu_update = $interval($scope.update_cpu_chart, 3000);
-
-    /* Polling CPU usage data and updating chart
-    var polling_cpu;
-
-    (function poll_cpu() {
-      var data = Dashboard.query(function() {
-        var total = data.cpu_usage.total;
-        var halt = data.cpu_usage.halt;
-        var active = total - halt;
-        var d = new Date();
-
-        total /= 1000000;  // 1 million
-        active /= 1000000;
-
-        total_data.push(total);
-        active_data.push(active);
-        time_data.push(d);
-
-        cpu_usage_chart.load({
-          columns: [
-            time_data,
-            total_data,
-            active_data
-          ]
-        });
-
-        polling_cpu = $timeout(poll_cpu, 2500);
-      });
-    })();*/
+    var cpu_update = $interval($scope.update_cpu_chart, 1500);
 
     // Polling dashboard data
     $scope.dashboard = new Dashboard();
