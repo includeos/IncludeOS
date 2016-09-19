d73d36843f82624cd147b54eee75ccc353122db3
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2016-09-18 20:42:47 -0400

    acorn_fs: Count files and directories

--- fs/acorn_fs.hpp
+++ fs/acorn_fs.hpp
@@ -26,7 +26,7 @@ namespace acorn {
 
 using Disk_ptr = fs::Disk_ptr;
 
-void recursive_fs_dump(Disk_ptr disk, std::vector<fs::Dirent> entries, const int depth = 1);
+void recursive_fs_dump(Disk_ptr disk, const std::vector<fs::Dirent>& entries, const int depth = 1);
 
 void list_static_content(Disk_ptr disk);
 
