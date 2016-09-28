1b08578a4229ce1dfad4731c2bd4eded16d4725b
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-09-28 10:25:07 +0200

    Updated include to reflect new folder structure

--- src/dashboard.cpp
+++ src/dashboard.cpp
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "../include/dashboard.hpp"
+#include "../include/dashboard/dashboard.hpp"
 
 using namespace dashboard;
 using namespace mana;
