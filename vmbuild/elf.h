76a288ba95c628df1be5411c92c32ca4eccbd003
Andreas Happe <andreashappe@snikt.net>
2016-10-04 23:30:18 +0200

    Make cmake-compilation work again
    * add elf_syms binary
    * fix demo_service (MAX_MEM)
    * fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines* fix included sources
    * add missing __DECL_BEGIN/__DECL_END #defines

--- vmbuild/elf.h
+++ vmbuild/elf.h
@@ -28,6 +28,15 @@
 #ifndef _ELF_H
 #define	_ELF_H 1
 
+/* C++ needs to know that types and declarations are C, not C++.  */
+#ifdef __cplusplus
+# define __BEGIN_DECLS extern "C" {
+# define __END_DECLS }
+#else
+# define __BEGIN_DECLS
+# define __END_DECLS
+#endif
+
 __BEGIN_DECLS
 
 /* Standard ELF types.  */
