552c99414b3919012e3fd79f98f64adde315e02c
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-10-05 15:20:27 +0200

    test: Updated logger unit test to fit the current test system

--- test/util/unit/logger_test.cpp
+++ test/util/unit/logger_test.cpp
@@ -15,247 +15,231 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "../logger.hpp"
-#include "../../test/lest/include/lest/lest.hpp"
+#include <common.cxx>
+#include <util/logger.hpp>
 
-const lest::test test_logger[] =
+CASE("Creating a Logger")
 {
-
-  CASE("Creating a Logger")
+  GIVEN("A gsl::span<char>")
   {
-    GIVEN("A gsl::span<char>")
+    const int N = 100;
+    char buffer[N] = { 0 };
+    //char buffer[N];
+    gsl::span<char> log{buffer, N};
+    Logger logger{log};
+
+    WHEN("A Logger is created")
     {
-      const int N = 100;
-      char buffer[N] = { 0 };
-      //char buffer[N];
-      gsl::span<char> log{buffer, N};
       Logger logger{log};
 
-      WHEN("A Logger is created")
+      THEN("Logger returns the correct size")
       {
-        Logger logger{log};
-
-        THEN("Logger returns the correct size")
-        {
-          EXPECT( logger.size() == N );
-        }
-        AND_THEN("Logger generates no entries")
-        {
-          auto entries = logger.entries();
-          EXPECT( entries.empty() );
-        }
+        EXPECT( logger.size() == N );
+      }
+      AND_THEN("Logger generates no entries")
+      {
+        auto entries = logger.entries();
+        EXPECT( entries.empty() );
       }
     }
-  },
-  CASE("Adding one entry")
+  }
+}
+CASE("Adding one entry")
+{
+  GIVEN("An initialized Logger")
   {
-    GIVEN("An initialized Logger")
+    const int N = 100;
+    char buffer[N] = { 0 };
+    gsl::span<char> log{buffer, N};
+    Logger logger{log};
+
+    WHEN("An entry is logged")
     {
-      const int N = 100;
-      char buffer[N] = { 0 };
-      gsl::span<char> log{buffer, N};
-      Logger logger{log};
+      const std::string entry{"test"};
+      logger.log(entry);
 
-      WHEN("An entry is logged")
+      THEN("The logger have one entry which is the one logged")
       {
-        const std::string entry{"test"};
-        logger.log(entry);
+        auto entries = logger.entries();
 
-        THEN("The logger have one entry which is the one logged")
+        EXPECT( entries.size() == 1 );
+        EXPECT( entries[0] == entry );
+
+        WHEN("The logger is flushed")
         {
-          auto entries = logger.entries();
+          logger.flush();
 
-          EXPECT( entries.size() == 1 );
-          EXPECT( entries[0] == entry );
-
-          WHEN("The logger is flushed")
+          THEN("The logger has no entries")
           {
-            logger.flush();
+            auto entries = logger.entries();
 
-            THEN("The logger has no entries")
-            {
-              auto entries = logger.entries();
-
-              EXPECT( entries.empty() );
-            }
-          }
-        }
-      }
-      WHEN("An empty entry is logged")
-      {
-        logger.log("");
-
-        THEN("The entry is never logged")
-        {
-          auto entries = logger.entries();
-
-          EXPECT( entries.empty() );
-        }
-      }
-    }
-  },
-  CASE("Adding several entries")
-  {
-    GIVEN("An initialized Logger")
-    {
-      const int N = 100;
-      char buffer[N] = { 0 };
-      gsl::span<char> log{buffer, N};
-      Logger logger{log};
-
-      WHEN("3 entries are logged")
-      {
-        const std::string entry1{"First"};
-        const std::string entry2{"Second"};
-        const std::string entry3{"Third"};
-
-        logger.log(entry1);
-        logger.log(entry2);
-        logger.log(entry3);
-
-        THEN("The logger has 3 entries in the correct order")
-        {
-          auto entries = logger.entries();
-
-          EXPECT( entries.size() == 3 );
-
-          EXPECT( entries[0] == entry1 );
-          EXPECT( entries[1] == entry2 );
-          EXPECT( entries[2] == entry3 );
-
-          WHEN("Asking for 1 result")
-          {
-            entries = logger.entries(1);
-
-            THEN("Only the last (latest) entry is returned")
-            {
-              EXPECT( entries.size() == 1 );
-              EXPECT( entries[0] == entry3 );
-            }
+            EXPECT( entries.empty() );
           }
         }
       }
     }
-  },
-  CASE("Adding entries that wraps around")
-  {
-    GIVEN("An initialized Logger with two entries")
+    WHEN("An empty entry is logged")
     {
-      const int N = 14;
-      char buffer[N] = { 0 };
-      gsl::span<char> log{buffer, N};
-      Logger logger{log};
+      logger.log("");
 
-      const std::string entry1{"First"}; // 5 + 1
-      const std::string entry2{"Second"}; // 6 + 1
+      THEN("The entry is never logged")
+      {
+        auto entries = logger.entries();
+
+        EXPECT( entries.empty() );
+      }
+    }
+  }
+}
+CASE("Adding several entries")
+{
+  GIVEN("An initialized Logger")
+  {
+    const int N = 100;
+    char buffer[N] = { 0 };
+    gsl::span<char> log{buffer, N};
+    Logger logger{log};
+
+    WHEN("3 entries are logged")
+    {
+      const std::string entry1{"First"};
+      const std::string entry2{"Second"};
+      const std::string entry3{"Third"};
 
       logger.log(entry1);
       logger.log(entry2);
+      logger.log(entry3);
 
-      WHEN("The third entry logged is smaller or equal first entry")
+      THEN("The logger has 3 entries in the correct order")
       {
-        const std::string entry3{"Third"}; // 5 + 1
-        logger.log(entry3);
+        auto entries = logger.entries();
 
-        THEN("The oldest (first) entry will be overwritten, and the second and third entry will be returned")
+        EXPECT( entries.size() == 3 );
+
+        EXPECT( entries[0] == entry1 );
+        EXPECT( entries[1] == entry2 );
+        EXPECT( entries[2] == entry3 );
+
+        WHEN("Asking for 1 result")
         {
-          auto entries = logger.entries();
+          entries = logger.entries(1);
 
-          EXPECT( entries.size() == 2 );
-          EXPECT( entries[0] == entry2 );
-          EXPECT( entries[1] == entry3 );
-        }
-      }
-      WHEN("The third entry logged is bigger than the first entry")
-      {
-        const std::string entry3{"A big third"}; // 11 + 1
-        logger.log(entry3);
-
-        THEN("Both the current entries will be overwritten, and the third will be returned")
-        {
-          auto entries = logger.entries();
-
-          EXPECT( entries.size() == 1 );
-          EXPECT( entries[0] == entry3 );
-        }
-      }
-      WHEN("The third entry logged is bigger than the size of the buffer")
-      {
-        const std::string entry3{"This one is too big"}; // 19 + 1
-        logger.log(entry3);
-
-        THEN("Only the last part of the string will be in the entry")
-        {
-          auto offset = entry3.size() + 1 - logger.size(); // +1 to account for padding
-          const std::string substr{entry3.begin() + offset, entry3.end()};
-
-          auto entries = logger.entries();
-
-          EXPECT( entries.size() == 1 );
-          EXPECT( entries[0] == substr );
-        }
-      }
-      WHEN("An empty entry is logged")
-      {
-        logger.log("");
-
-        THEN("The entry is never logged")
-        {
-          auto entries = logger.entries();
-
-          EXPECT( entries.size() == 2 );
-        }
-      }
-    }
-  },
-  CASE("Adding a horde of log entries")
-  {
-    GIVEN("An initialized Logger")
-    {
-      const int N = 1024*1024;
-      char buffer[N] = { 0 };
-      gsl::span<char> log{buffer, N};
-      Logger logger{log};
-
-      WHEN("Adding shitloads of entries")
-      {
-        const std::string entry{"This is a string which will fill 42 bytes"};
-        const auto sz = entry.size() + 1;
-        const auto max_entries = logger.size() / sz;
-        const unsigned rotations = 3;
-
-        for(int i; i < max_entries * rotations; ++i)
-          logger.log(entry);
-
-        THEN("There isnt more than maximum possible entries and they are all correct")
-        {
-          auto entries = logger.entries();
-
-          EXPECT( entries.size() <= max_entries );
-
-          bool all_correct = true;
-          for(auto& ent : entries) {
-            if(ent != entry) {
-              all_correct = false;
-              break;
-            }
+          THEN("Only the last (latest) entry is returned")
+          {
+            EXPECT( entries.size() == 1 );
+            EXPECT( entries[0] == entry3 );
           }
-
-          EXPECT( all_correct );
         }
       }
     }
   }
-
-};
-
-int main(int argc, char * argv[])
+}
+CASE("Adding entries that wraps around")
 {
-  printf("Running Logger tests\n");
+  GIVEN("An initialized Logger with two entries")
+  {
+    const int N = 14;
+    char buffer[N] = { 0 };
+    gsl::span<char> log{buffer, N};
+    Logger logger{log};
 
-  int res = lest::run(test_logger, argc, argv);
+    const std::string entry1{"First"}; // 5 + 1
+    const std::string entry2{"Second"}; // 6 + 1
 
-  printf("Tests completed.\n");
+    logger.log(entry1);
+    logger.log(entry2);
 
-  return res;
+    WHEN("The third entry logged is smaller or equal first entry")
+    {
+      const std::string entry3{"Third"}; // 5 + 1
+      logger.log(entry3);
+
+      THEN("The oldest (first) entry will be overwritten, and the second and third entry will be returned")
+      {
+        auto entries = logger.entries();
+
+        EXPECT( entries.size() == 2 );
+        EXPECT( entries[0] == entry2 );
+        EXPECT( entries[1] == entry3 );
+      }
+    }
+    WHEN("The third entry logged is bigger than the first entry")
+    {
+      const std::string entry3{"A big third"}; // 11 + 1
+      logger.log(entry3);
+
+      THEN("Both the current entries will be overwritten, and the third will be returned")
+      {
+        auto entries = logger.entries();
+
+        EXPECT( entries.size() == 1 );
+        EXPECT( entries[0] == entry3 );
+      }
+    }
+    WHEN("The third entry logged is bigger than the size of the buffer")
+    {
+      const std::string entry3{"This one is too big"}; // 19 + 1
+      logger.log(entry3);
+
+      THEN("Only the last part of the string will be in the entry")
+      {
+        auto offset = entry3.size() + 1 - logger.size(); // +1 to account for padding
+        const std::string substr{entry3.begin() + offset, entry3.end()};
+
+        auto entries = logger.entries();
+
+        EXPECT( entries.size() == 1 );
+        EXPECT( entries[0] == substr );
+      }
+    }
+    WHEN("An empty entry is logged")
+    {
+      logger.log("");
+
+      THEN("The entry is never logged")
+      {
+        auto entries = logger.entries();
+
+        EXPECT( entries.size() == 2 );
+      }
+    }
+  }
+}
+CASE("Adding a horde of log entries")
+{
+  GIVEN("An initialized Logger")
+  {
+    const int N = 1024*1024;
+    char buffer[N] = { 0 };
+    gsl::span<char> log{buffer, N};
+    Logger logger{log};
+
+    WHEN("Adding shitloads of entries")
+    {
+      const std::string entry{"This is a string which will fill 42 bytes"};
+      const auto sz = entry.size() + 1;
+      const auto max_entries = logger.size() / sz;
+      const unsigned rotations = 3;
+
+      for(int i; i < max_entries * rotations; ++i)
+        logger.log(entry);
+
+      THEN("There isnt more than maximum possible entries and they are all correct")
+      {
+        auto entries = logger.entries();
+
+        EXPECT( entries.size() <= max_entries );
+
+        bool all_correct = true;
+        for(auto& ent : entries) {
+          if(ent != entry) {
+            all_correct = false;
+            break;
+          }
+        }
+
+        EXPECT( all_correct );
+      }
+    }
+  }
 }
