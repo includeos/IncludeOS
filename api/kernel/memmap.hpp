4fb676f298375537ac6d4599982f7fedf6339a80
fwsgonzo <fwsgonzo@hotmail.com>
2016-10-06 19:45:19 +0200

    memmap: Revert using asserts instead of exceptions

--- api/kernel/memmap.hpp
+++ api/kernel/memmap.hpp
@@ -54,8 +54,11 @@ public:
                      const std::string descr, In_use_delg in_use)
     : name_{name}, description_{descr}, in_use_{in_use}
   {
-    assert (begin < end && "Start is larger than end");
-    assert (end - begin + 1 <= span_max() && "Maximum range size exceeded");
+    if (begin > end)
+      throw Memory_range_exception("Start is larger than end: " +
+          std::to_string(begin) + " > " + std::to_string(end));
+    if (end - begin + 1 > span_max())
+      throw Memory_range_exception("Maximum range size is " + std::to_string(span_max()));
     range_ = Span((uint8_t*)begin, end - begin + 1);
   }
 
