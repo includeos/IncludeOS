bd60272f4d12c5530ef175ea472de8eca07f97fc
Simon Sandström <simon@nikanor.nu>
2016-10-07 22:07:24 +0200

    kernel: draft of cpuid rewrite

--- api/kernel/cpuid.hpp
+++ api/kernel/cpuid.hpp
@@ -19,10 +19,86 @@
 #ifndef KERNEL_CPUID_HPP
 #define KERNEL_CPUID_HPP
 
-struct CPUID {
-  static bool isAmdCpu();
-  static bool isIntelCpu();
-  static bool hasRDRAND();
-}; //< CPUID
+namespace CPUID
+{
+  enum class Feature
+  {
+    // ------------------------------------------------------------------------
+    // Processor Info and Feature Bits
+    // ------------------------------------------------------------------------
+    SSE3,              // Streaming SIMD Extensions 3
+    PCLMULQDQ,         // PCLMULQDQ Instruction
+    DTES64,            // 64-Bit Debug Store Area
+    MONITOR,           // MONITOR/MWAIT
+    DS_CPL,            // CPL Qualified Debug Store
+    VMX,               // Virtual Machine Extensions
+    SMX,               // Safer Mode Extensions
+    EST,               // Enhanced SpeedStep Technology
+    TM2,               // Thermal Monitor 2
+    SSSE3,             // Supplemental Streaming SIMD Extensions 3
+    CNXT_ID,           // L1 Context ID
+    FMA,               // Fused Multiply Add
+    CX16,              // CMPXCHG16B Instruction
+    XTPR,              // xTPR Update Control
+    PDCM,              // Perf/Debug Capability MSR
+    PCID,              // Process-context Identifiers
+    DCA,               // Direct Cache Access
+    SSE4_1,            // Streaming SIMD Extensions 4.1
+    SSE4_2,            // Streaming SIMD Extensions 4.2
+    X2APIC,            // Extended xAPIC Support
+    MOVBE,             // MOVBE Instruction
+    POPCNT,            // POPCNT Instruction
+    TSC_DEADLINE,      // Local APIC supports TSC Deadline
+    AES,               // AESNI Instruction
+    XSAVE,             // XSAVE/XSTOR States
+    OSXSAVE,           // OS Enabled Extended State Management
+    AVX,               // AVX Instructions
+    F16C,              // 16-bit Floating Point Instructions
+    RDRAND,            // RDRAND Instruction
+
+    FPU,               // Floating-Point Unit On-Chip
+    VME,               // Virtual 8086 Mode Extensions
+    DE,                // Debugging Extensions
+    PSE,               // Page Size Extension
+    TSC,               // Time Stamp Counter
+    MSR,               // Model Specific Registers
+    PAE,               // Physical Address Extension
+    MCE,               // Machine-Check Exception
+    CX8,               // CMPXCHG8 Instruction
+    APIC,              // APIC On-Chip
+    SEP,               // SYSENTER/SYSEXIT instructions
+    MTRR,              // Memory Type Range Registers
+    PGE,               // Page Global Bit
+    MCA,               // Machine-Check Architecture
+    CMOV,              // Conditional Move Instruction
+    PAT,               // Page Attribute Table
+    PSE_36,            // 36-bit Page Size Extension
+    PSN,               // Processor Serial Number
+    CLFLUSH,           // CLFLUSH Instruction
+    DS,                // Debug Store
+    ACPI,              // Thermal Monitor and Software Clock Facilities
+    MMX,               // MMX Technology
+    FXSR,              // FXSAVE and FXSTOR Instructions
+    SSE,               // Streaming SIMD Extensions
+    SSE2,              // Streaming SIMD Extensions 2
+    SS,                // Self Snoop
+    HTT,               // Multi-Threading
+    TM,                // Thermal Monitor
+    PBE,               // Pending Break Enable
+
+    // ------------------------------------------------------------------------
+    // Extended Processor Info and Feature Bits (not complete)
+    // ------------------------------------------------------------------------
+    SYSCALL,           // SYSCALL/SYSRET
+    NX,                // Execute Disable Bit
+    PDPE1GB,           // 1 GB Pages
+    RDTSCP,            // RDTSCP and IA32_TSC_AUX
+    LM,                // Long mode (64-bit Architecture)
+  };
+
+  bool isAmdCpu();
+  bool isIntelCpu();
+  bool hasFeature(Feature f);
+} //< CPUID
 
 #endif //< KERNEL_CPUID_HPP
