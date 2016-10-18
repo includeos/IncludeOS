80375d82d3fef014112d6fe209b7737772395918
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-10-18 13:36:45 +0200

    posix: UDP socket test now tests bind() and partially sendto()

--- test/posix/integration/udp/test.py
+++ test/posix/integration/udp/test.py
@@ -10,5 +10,22 @@ sys.path.insert(0,includeos_src + "/test")
 
 import vmrunner
 vm = vmrunner.vms[0]
+
+import socket
+def UDP_recv():
+  HOST, PORT = '', 4242
+  MESSAGE = "Only hipsters uses POSIX"
+  sock = socket.socket
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  sock.bind(('', PORT))
+
+  received = sock.recv(1024)
+  print received
+  return received == MESSAGE
+
+# Add custom event-handler
+vm.on_output("sendto()", UDP_recv)
+
 # Boot the VM, taking a timeout as parameter
 vm.make().boot(10)
