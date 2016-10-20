42a9fd49a42ffda10db5e9efa832a142749fb757
fwsgonzo <fwsgonzo@hotmail.com>
2016-10-20 09:33:15 +0200

    seed: Add all arguments to append

--- seed/run.sh
+++ seed/run.sh
@@ -3,9 +3,5 @@ set -e
 sudo ${INCLUDEOS_HOME-$HOME/IncludeOS_install}/etc/create_bridge.sh
 make -j
 
-export CMDLINE="-append ${1}"
-if [ $# -eq 0 ]
-then
-  export CMDLINE=""
-fi
+export CMDLINE="-append \"${*}\""
 source ${INCLUDEOS_HOME-$HOME/IncludeOS_install}/etc/run.sh `make kernelfile`
