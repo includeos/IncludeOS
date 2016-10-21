23cb6cb4ffc84851fd93d25c4537235488215498
Martin Nordsletten <mnordsletten@gmail.com>
2016-10-21 08:22:21 +0000

    binary_detect: Attempt to use jenkins env variable for target branch

--- test/miscellaneous/binary_check/test.sh
+++ test/miscellaneous/binary_check/test.sh
@@ -3,7 +3,7 @@
 # Script that checks for any binary blobs in the commit and raises errors if any are present
 
 # Make sure dev is fully updated and that we are standing in pull request
-target_branch=dev
+target_branch=${ghprbTargetBranch-dev}
 
 # Figure out how many files were changed
 files_changed=`git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $target_branch)`
