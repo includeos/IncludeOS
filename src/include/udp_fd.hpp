67316fb1aa15f1ce8ac7714568db92845411d727
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-10-25 16:36:13 +0200

    posix: Added buffer to UDP, supporting MSG_PEEK

--- src/include/udp_fd.hpp
+++ src/include/udp_fd.hpp
@@ -19,17 +19,23 @@
 #ifndef INCLUDE_UDP_FD_HPP
 #define INCLUDE_UDP_FD_HPP
 
-#include "fd.hpp"
+#include "sockfd.hpp"
 #include <net/inet4>
+#include <gsl/span>
+#include <deque>
 
-class UDP_FD : public FD {
+class UDP_FD : public SockFD {
 public:
+  static const size_t BUF_LIMIT;
   using id_t = int;
 
   explicit UDP_FD(const int id)
-    : FD(id), non_blocking_(false), broadcast_(false)
+    : SockFD(id),
+      buffer_(), sock(nullptr),
+      non_blocking_(false), broadcast_(false)
   {
     memset((char *)&peer_, 0, sizeof(peer_));
+    (void)non_blocking_;
   }
 
   int     read(void*, size_t) override;
@@ -46,18 +52,34 @@ public:
   ssize_t recv(void*, size_t, int fl) override;
   ssize_t recvfrom(void *__restrict__, size_t, int, struct sockaddr *__restrict__, socklen_t *__restrict__) override;
 
+  struct Message {
+    explicit Message(in_addr_t addr, in_port_t port,
+      char* buf, size_t len)
+      : data(buf, len)
+    {
+      src.sin_family      = AF_INET;
+      src.sin_port        = port;
+      src.sin_addr.s_addr = addr;
+    }
+    struct sockaddr_in src;
+    gsl::span<char> data;
+  };
 private:
-  net::UDPSocket* sock = nullptr;
-  bool non_blocking_;
-  bool broadcast_;
+  std::deque<Message> buffer_;
   // http://osr507doc.xinuos.com/en/netguide/disockD.connecting_datagrams.html
-  struct sockaddr_in peer_;
+  struct sockaddr_in  peer_;
+  net::UDPSocket*     sock;
+  bool                non_blocking_;
+  bool                broadcast_;
 
   void recv_to_buffer(net::UDPSocket::addr_t, net::UDPSocket::port_t, const char*, size_t);
   void set_default_recv();
+  int read_from_buffer(void*, size_t, int, struct sockaddr*, socklen_t*);
 
   bool is_connected() const
   { return peer_.sin_port != 0 && peer_.sin_addr.s_addr != 0; }
+
+
 };
 
 #endif
--- src/posix/udp_fd.cpp
+++ src/posix/udp_fd.cpp
@@ -18,14 +18,47 @@
 #include <udp_fd.hpp>
 #include <kernel/irq_manager.hpp>
 
+const size_t UDP_FD::BUF_LIMIT = 10;
+
 // return the "currently selected" networking stack
 static net::Inet4& net_stack() {
   return net::Inet4::stack<> ();
 }
 
-void UDP_FD::recv_to_buffer(net::UDPSocket::addr_t, net::UDPSocket::port_t, const char*, size_t)
+void UDP_FD::recv_to_buffer(net::UDPSocket::addr_t addr,
+  net::UDPSocket::port_t port, const char* buf, size_t len)
 {
-  // buffer somehow
+  // only recv to buffer if not full
+  if(buffer_.size() < BUF_LIMIT) {
+    // copy data into to-be Message buffer
+    char* data = new char[len];
+    memcpy(data, buf, len);
+    // emplace the message in buffer
+    buffer_.emplace_back(htonl(addr.whole), htons(port), data, len);
+  }
+}
+
+int UDP_FD::read_from_buffer(void* buffer, size_t len, int flags,
+  struct sockaddr* address, socklen_t* address_len)
+{
+  assert(!buffer_.empty() && "Trying to read from empty buffer");
+
+  auto& msg = buffer_.front();
+  auto& data = msg.data;
+
+  int bytes = std::min(len, (uint32_t)data.length());
+
+  memcpy(buffer, data.data(), bytes);
+
+  if(address != nullptr) {
+    memcpy(address, &msg.src, std::min(*address_len, (int)sizeof(struct sockaddr_in)));
+    *address_len = sizeof(struct sockaddr_in);
+  }
+
+  if(!(flags & MSG_PEEK))
+    buffer_.pop_back();
+
+  return bytes;
 }
 void UDP_FD::set_default_recv()
 {
@@ -62,6 +95,7 @@ int UDP_FD::bind(const struct sockaddr* address, socklen_t len)
   auto& udp = net_stack().udp();
   try {
     this->sock = (port) ? &udp.bind(ntohs(port)) : &udp.bind();
+    set_default_recv();
     return 0;
   } catch(const net::UDP::Port_in_use_exception&) {
     errno = EADDRINUSE;
@@ -78,8 +112,10 @@ int UDP_FD::connect(const struct sockaddr* address, socklen_t address_len)
 
   // If the socket has not already been bound to a local address,
   // connect() shall bind it to an address which is an unused local address.
-  if(this->sock == nullptr)
+  if(this->sock == nullptr) {
     this->sock = &net_stack().udp().bind();
+    set_default_recv();
+  }
 
   const auto& addr = *((sockaddr_in*)address);
   if (addr.sin_family == AF_UNSPEC)
@@ -117,6 +153,7 @@ ssize_t UDP_FD::sendto(const void* message, size_t len, int flags,
   // Bind a socket if we dont already have one
   if(this->sock == nullptr) {
     this->sock = &net_stack().udp().bind();
+    set_default_recv();
   }
   const auto& dest = *((sockaddr_in*)dest_addr);
   // If the socket protocol supports broadcast and the specified address is a broadcast address for the socket protocol,
@@ -148,12 +185,22 @@ ssize_t UDP_FD::recvfrom(void *__restrict__ buffer, size_t len, int flags,
     return -1;
   }
 
-  // Receiving
-  int bytes = 0;
-  bool done = false;
-  this->sock->on_read(
-    [&bytes, &done, buffer, len, address, address_len]
-    (net::UDPSocket::addr_t addr, net::UDPSocket::port_t port, const char* data, size_t data_len)
+  // Read from buffer if not empty
+  if(!buffer_.empty())
+  {
+    return read_from_buffer(buffer, len, flags, address, address_len);
+  }
+  // Else make a blocking receive
+  else
+  {
+    int bytes = 0;
+    bool done = false;
+
+    this->sock->on_read(
+    [&bytes, &done, this,
+      buffer, len, flags, address, address_len]
+    (net::UDPSocket::addr_t addr, net::UDPSocket::port_t port,
+      const char* data, size_t data_len)
     {
       bytes = std::min(len, data_len);
       memcpy(buffer, data, bytes);
@@ -167,12 +214,20 @@ ssize_t UDP_FD::recvfrom(void *__restrict__ buffer, size_t len, int flags,
         sender.sin_addr.s_addr  = htonl(addr.whole);
       }
       done = true;
+
+      // Store in buffer if PEEK
+      if(flags & MSG_PEEK)
+        recv_to_buffer(addr, port, data, data_len);
     });
 
-  while(!done) {
-    OS::halt();
-    IRQ_manager::get().process_interrupts();
-  }
 
-  return bytes;
+
+    // Block until (any) data is read
+    while(!done)
+      OS::block();
+
+    set_default_recv();
+
+    return bytes;
+  }
 }
