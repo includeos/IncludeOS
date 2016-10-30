38426a1bc92350c47c18e28d7b4b8f233549a293
fwsgonzo <fwsgonzo@hotmail.com>
2016-10-30 08:48:00 +0100

    boot: Make sure GDT is valid for multiboot

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -19,6 +19,8 @@ USE32
 extern kernel_start
 global _start
 
+%define BOOT_SEGMENT 0x07c0
+
 section .text
 ;; Multiboot places boot paramters on eax and ebx.
 _start:
@@ -26,7 +28,34 @@ _start:
         mov esp, 0xA0000
         mov ebp, esp
 
+        ;; With multiboot we get a possibly unusable GDT
+        ;; So load our own
+        lgdt [gdtr]
+
         ;;  Place multiboot parameters on stack
         push ebx
         push eax
         call kernel_start
+
+ALIGN 32
+gdtr:
+	dw gdt32_end - gdt32 - 1
+	dd gdt32
+ALIGN 32
+gdt32:
+	;; Entry 0x0: Null desriptor
+	dq 0x0
+	;; Entry 0x8: Code segment
+	dw 0xffff		       ;Limit
+  dw 0x0000		       ;Base 15:00
+	db 0x00			       ;Base 23:16
+	dw 0xcf9a		       ;Flags
+	db 0x00			       ;Base 32:24
+	;; Entry 0x10: Data segment
+	dw 0xffff		       ;Limit
+	dw 0x0000		       ;Base 15:00
+	db 0x00			       ;Base 23:16
+	dw 0xcf92		       ;Flags
+	db 0x00			       ;Base 32:24
+gdt32_end:
+	db `32`
