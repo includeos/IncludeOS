6080da3007ef5ddd4178d6d7ac02e90ebec0d9a3
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-11-02 15:58:41 +0100

    net: Added link layer driver which takes a link protocol

---
+++ api/net/link_layer.hpp
@@ -0,0 +1,49 @@
+
+
+#pragma once
+#ifndef NET_LINK_LAYER_HPP
+#define NET_LINK_LAYER_HPP
+
+#include <hw/nic.hpp>
+
+namespace net {
+
+template <class Protocol>
+class Link_layer : public hw::Nic {
+public:
+  using upstream    = hw::Nic::upstream;
+  using downstream  = hw::Nic::downstream;
+public:
+  explicit Link_layer(Protocol&& protocol, uint32_t bufstore_sz, uint16_t bufsz);
+
+  downstream create_link_downstream() override
+  { return {link_, &Protocol::transmit}; }
+
+  void set_ip4_upstream(upstream handler) override
+  { link_.set_ip4_upstream(handler); }
+
+  void set_ip6_upstream(upstream handler) override
+  { link_.set_ip6_upstream(handler); }
+
+  void set_arp_upstream(upstream handler) override
+  { link_.set_arp_upstream(handler); }
+
+protected:
+  /** Called by the underlying physical driver inheriting the Link_layer */
+  void receive(net::Packet_ptr pkt)
+  { link_.receive(std::move(pkt)); }
+
+private:
+  Protocol link_;
+};
+
+template <class Protocol>
+Link_layer<Protocol>::Link_layer(Protocol&& protocol, uint32_t bufstore_sz, uint16_t bufsz)
+  : hw::Nic(bufstore_sz + Protocol::header_size(), bufsz),
+    link_{std::forward<Protocol>(protocol)}
+{
+}
+
+} // < namespace net
+
+#endif
