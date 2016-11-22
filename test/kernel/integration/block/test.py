0fe47a08f5b14154182050740f15daee4ee51355
Gonzo <fwsgonzo@hotmail.com>
2016-11-22 17:15:02 +0100

    cmake: Add blocking calls test

--- test/kernel/integration/block/test.py
+++ test/kernel/integration/block/test.py
@@ -1,13 +1,14 @@
-#! /usr/bin/python
-
+#!/usr/bin/python
 import sys
 import os
 
-includeos_src = os.environ['INCLUDEOS_SRC']
+includeos_src = os.environ.get('INCLUDEOS_SRC',
+                               os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))).split('/test')[0])
 sys.path.insert(0,includeos_src + "/test")
 
-import vmrunner
+from subprocess import call
 
-vm = vmrunner.vms[0];
-vm.make()
-vm.boot(20)
+import vmrunner
+vm = vmrunner.vms[0]
+
+vm.cmake().boot(30).clean()
