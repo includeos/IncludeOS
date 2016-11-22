f8aee5c380e46558036487c2320073f08caeed99
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-11-22 14:22:39 +0000

    test: dhcp test now builds with cmake.

--- test/net/integration/dhcp/test.py
+++ test/net/integration/dhcp/test.py
@@ -20,19 +20,18 @@ vm = vmrunner.vms[0]
 ping_count = 3
 
 def DHCP_test(trigger_line):
-  print "<Test.py> Got IP"
+  print color.INFO("<Test.py>"),"Got IP"
   ip_string = vm.readline()
-  print "<Test.py> Assigned address: ", ip_string
-  print "<Test.py> Trying to ping"
+  print color.INFO("<Test.py>"), "Assigned address: ", ip_string
+  print color.INFO("<Test.py>"), "Trying to ping"
   time.sleep(1)
   try:
     command = ["ping", ip_string.rstrip(), "-c", str(ping_count), "-i", "0.2"]
     print color.DATA(" ".join(command))
     print subprocess.check_output(command)
-    print "<Test.py> Ping SUCCESS - Process returned 0 exit status"
-    vm.exit(0,"SUCCESS")
+    vm.exit(0,"<Test.py> Ping test passed. Process returned 0 exit status")
   except Exception as e:
-    print "<Test.py> Ping FAILED Process threw exception:"
+    print color.FAIL("<Test.py> Ping FAILED Process threw exception:")
     print e
     return False
 
@@ -41,4 +40,4 @@ def DHCP_test(trigger_line):
 vm.on_output("Got IP from DHCP", DHCP_test)
 
 # Boot the VM, taking a timeout as parameter
-vm.make().boot(20)
+vm.cmake().boot(20).clean()
