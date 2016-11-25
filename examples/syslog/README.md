0be5dcc1a850f2b93b5b278c2aa8b7fdf5a0eb17
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-11-25 14:58:48 +0100

    example: finished consolidation of syslog

--- examples/syslog/README.md
+++ examples/syslog/README.md
@@ -1,12 +1,15 @@
 # Syslog IncludeOS Plugin Service
 
-Sending syslog data over UDP port 6514.
+Sending syslog data over UDP port 6514. The message format corresponds to the format specified in RFC5424.
 
-The message format corresponds to the format specified in RFC5424.
+The default behavior for IncludeOS's syslog implementation is to send the data to printf, which again can be routed anywhere the service specifies. This is intentional since logging over a UDP/UNIX socket wouldn't necessarily work as one might think in IncludeOS, and there's unnecessary overhead using UDP if all you want is simple logging. Instead we provide an optional plugin for sending stadard syslog over UDP to an external network interface.
 
-Main in service.cpp corresponds to main in syslog_lubuntu/syslog_lubuntu.c.
+* To enable the UDP syslog plugin, simply `set(PLUGINS .., syslogd,..)` in CMakeLists.txt or turn on the libsyslogd cmake option. This will override the default.
 
-PLUGIN=syslogd must be defined in Makefile.
+# Compatibility with Linux:
+This example intends to show how the POSIX syslog interface works the same way in both Linux and IncludeOS. The CMake build creates an IncludeOS bootable image which inlcudes the program `syslog_example.c`. Service::start` in `service.cpp` calls `main` in `syslog_example.c`. The syslog example can also be built and run umodified under Linux:
+* `$ make -f Makefile_linux`
+Run locally by calling
+* `$ ./syslog_linux`
 
-(The default behavior for IncludeOS's syslog implementation is however to send the data to printf, which is
-what you get if you don't specify PLUGIN=syslogd in your Makefile (see the syslog_default integration test)).
+NOTE: The example will send various types of log messages, including `LOG_ALERT`, `LOG_EMERG` etc. Also note that the IncludeOS service will transmit UDP packets to a remote IP specified by the user. The user is in charge of pointing this IP to a valid syslog server.
--- examples/syslog/service.cpp
+++ examples/syslog/service.cpp
@@ -24,7 +24,9 @@
 /* For IncludeOS */
 // #include <syslogd>
 
-int main()
+extern "C" int main();
+
+void Service::start(const std::string&)
 {
   // DHCP on interface 0
   auto& inet = net::Inet4::ifconfig(10.0);
@@ -34,93 +36,13 @@ int main()
                       {  10,  0,  0,  1 },    // Gateway
                       {  10,  0,  0,  1} );   // DNS
 
+
+  // For now we have to specify the remote syslogd IP
+  // FIXME: instructions  here
+
   // Starts the python integration test:
   printf("Service IP address is %s\n", inet.ip_addr().str().c_str());
 
   /* ------------------------- POSIX syslog in IncludeOS ------------------------- */
-
-  int invalid_priority = -1;
-  syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  invalid_priority = 10;
-  syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  invalid_priority = 55;
-  syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  syslog(LOG_INFO, "(Info) No open has been called prior to this");
-  syslog(LOG_NOTICE, "(Notice) Program created with two arguments: %s and %s", "one", "two");
-
-  openlog("Prepended message", LOG_CONS | LOG_NDELAY, LOG_MAIL);
-
-  syslog(LOG_ERR, "(Err) Log after prepended message with one argument: %d", 44);
-  syslog(LOG_WARNING, "(Warning) Log number two after openlog set prepended message");
-
-  closelog();
-
-  syslog(LOG_WARNING, "(Warning) Log after closelog with three arguments. One is %u, another is %s, a third is %d", 33, "this", 4011);
-
-  openlog("Second prepended message", LOG_PID | LOG_CONS, LOG_USER);
-
-  syslog(LOG_EMERG, "Emergency log after openlog and new facility: user");
-  syslog(LOG_ALERT, "Alert log with the m argument: %m");
-
-  closelog();
-
-  syslog(LOG_CRIT, "Critical after cleared prepended message (closelog has been called)");
-
-  closelog();
-
-  openlog("Open after close prepended message", LOG_CONS, LOG_MAIL);
-
-  syslog(LOG_INFO, "Info after openlog with both m: %m and two hex arguments: 0x%x and 0x%x", 100, 50);
-
-  closelog();
-
-  /* ------------------------- Alternatively: IncludeOS syslog without POSIX wrapper -------------------------
-
-  invalid_priority = -1;
-  Syslog::syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  invalid_priority = 10;
-  Syslog::syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  invalid_priority = 55;
-  Syslog::syslog(invalid_priority, "Invalid %d", invalid_priority);
-
-  Syslog::syslog(LOG_INFO, "(Info) No open has been called prior to this");
-  Syslog::syslog(LOG_NOTICE, "(Notice) Program created with two arguments: %s and %s", "one", "two");
-
-  Syslog::openlog("Prepended message", LOG_CONS | LOG_NDELAY, LOG_MAIL);
-
-  Syslog::syslog(LOG_ERR, "(Err) Log after prepended message with one argument: %d", 44);
-  Syslog::syslog(LOG_WARNING, "(Warning) Log number two after openlog set prepended message");
-
-  Syslog::closelog();
-
-  Syslog::syslog(LOG_WARNING, "(Warning) Log after closelog with three arguments. One is %u, another is %s, a third is %d", 33, "this", 4011);
-
-  Syslog::openlog("Second prepended message", LOG_PID | LOG_CONS, LOG_USER);
-
-  Syslog::syslog(LOG_EMERG, "Emergency log after openlog and new facility: user");
-  Syslog::syslog(LOG_ALERT, "Alert log with the m argument: %m");
-
-  Syslog::closelog();
-
-  Syslog::syslog(LOG_CRIT, "Critical after cleared prepended message (closelog has been called)");
-
-  Syslog::closelog();
-
-  Syslog::openlog("Open after close prepended message", LOG_CONS, LOG_MAIL);
-
-  Syslog::syslog(LOG_INFO, "Info after openlog with both m: %m and two hex arguments: 0x%x and 0x%x", 100, 50);
-
-  */
-
-  return 0;
-}
-
-void Service::start(const std::string&)
-{
   main();
 }
