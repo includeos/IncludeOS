ac01a9c203189057ec0bba5f088793d187231807
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-11-28 14:54:08 +0100

    cmake: Build vmbuilder as external project, removed toolchain detection

--- cmake/toolchain_detection.cmake
+++
@@ -1,8 +0,0 @@
-cmake_minimum_required(VERSION 2.8.9)
-
-# Use custom toolchain if set
-set(toolchain $ENV{INCLUDEOS_TOOLCHAIN_FILE})
-if(DEFINED toolchain)
-  MESSAGE(STATUS "IncludeOS toolchain file set to: " ${toolchain})
-  set(CMAKE_TOOLCHAIN_FILE ${toolchain})
-endif(DEFINED toolchain)
