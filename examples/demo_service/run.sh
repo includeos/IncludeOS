3a09930f0ba7afba23f95c7a71c02152c0463bf0
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-11-28 17:22:38 +0100

    examples: Updated build and run of demo service

--- examples/demo_service/run.sh
+++ examples/demo_service/run.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-set -e
-sudo ${INCLUDEOS_PREFIX-/usr/local}/includeos/scripts/create_bridge.sh
-make -j
+set -
+INCLUDEOS_PREFIX=${INCLUDEOS_PREFIX-/usr/local}
+sudo $INCLUDEOS_PREFIX/includeos/scripts/create_bridge.sh
 
-export CMDLINE="-append ${1}"
-if [ $# -eq 0 ]
-then
-  export CMDLINE=""
-fi
-source ${INCLUDEOS_PREFIX-usr/local}/includeos/scripts/run.sh IncludeOS_example
+FILE=$1
+shift
+source $INCLUDEOS_PREFIX/includeos/scripts/run.sh $FILE ${*}
