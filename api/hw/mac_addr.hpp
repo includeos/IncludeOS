c68262bd841ddd77cbf758f4a3366fa7b7fc7f82
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-12-02 12:21:33 +0100

    hw: Added even more includes to MAC_addr..

--- api/hw/mac_addr.hpp
+++ api/hw/mac_addr.hpp
@@ -19,8 +19,11 @@
 #ifndef HW_MAC_ADDR_HPP
 #define HW_MAC_ADDR_HPP
 
-#include <stddef.h>
-#include <stdint.h>
+#include <stddef.h>   // size_t
+#include <stdint.h>   // uint
+#include <cstdio>     // snprintf
+#include <string>
+#include <cstring>    // strncmp
 
 namespace hw {
 // MAC address
