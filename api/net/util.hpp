c2d3a2d619ced78db02d3c3549b48cd33d347d3d
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2016-12-02 14:13:51 +0100

    net: Avoid crash with system defined endian swap (experimental)..

--- api/net/util.hpp
+++ api/net/util.hpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,30 +33,42 @@ namespace net {
     return (x >> ((p + 1) - n)) & ~(~0 << n);
   }
 
+  #ifndef ntohs
   constexpr inline uint16_t ntohs(const uint16_t n) noexcept {
     return __builtin_bswap16(n);
   }
+  #endif
 
+  #ifndef htons
   constexpr inline uint16_t htons(const uint16_t n) noexcept {
     return __builtin_bswap16(n);
   }
+  #endif
 
+  #ifndef ntohl
   constexpr inline uint32_t ntohl(const uint32_t n) noexcept {
     return __builtin_bswap32(n);
   }
+  #endif
 
+  #ifndef htonl
   constexpr inline uint32_t htonl(const uint32_t n) noexcept {
     return __builtin_bswap32(n);
   }
+  #endif
 
+  #ifndef ntohll
   constexpr inline uint64_t ntohll(const uint64_t n) noexcept {
     return __builtin_bswap64(n);
   }
+  #endif
 
+  #ifndef htonll
   constexpr inline uint64_t htonll(const uint64_t n) noexcept {
     return __builtin_bswap64(n);
   }
-  
+  #endif
+
 } //< namespace net
 
 #endif //< NET_UTIL_HPP
