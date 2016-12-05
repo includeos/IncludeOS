daf36c601ca99dcd6f480e59bde8ebaf2d427d7c
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-12-05 01:39:53 +0100

    rtc: print some info on init

--- src/kernel/rtc.cpp
+++ src/kernel/rtc.cpp
@@ -6,6 +6,8 @@
 #include <hw/cmos.hpp>
 #include <hertz>
 
+#define MYINFO(X,...) INFO("RTC", X, ##__VA_ARGS__)
+
 static int64_t  current_time  = 0;
 static uint64_t current_ticks = 0;
 
@@ -13,6 +15,7 @@ using namespace std::chrono;
 
 void RTC::init()
 {
+
   // Initialize CMOS
   cmos::init();
 
@@ -20,6 +23,7 @@ void RTC::init()
   current_time  = cmos::now().to_epoch();
   current_ticks = hw::CPU::rdtsc();
 
+  MYINFO("Enabling regular clock sync with CMOS ");
   // every minute recalibrate
   Timers::periodic(seconds(60), seconds(60),
   [] (uint32_t) {
