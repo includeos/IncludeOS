ed2d360654e6c3bb395420a0109d1204dcd53744
Gonzo <fwsgonzo@hotmail.com>
2017-01-13 17:02:37 +0100

    kernel: Rewrite stack switching to use fastcall, passing delegate on stack

--- src/kernel/context_asm.asm
+++ src/kernel/context_asm.asm
@@ -21,25 +21,35 @@ extern __context_switch_delegate
 section .text
 
 __context_switch:
+    ; retrieve stack location
+    ;pop  ebx
+    ; delegate reference
+    ;pop  ecx
+    
+    ; create stack frame
     push ebp
     mov  ebp, esp
+    
     ; save special purpose stuff
     push esi
     push edi
 
-    ; new stack location in EBX
-    mov ebx, [ebp+8]
-
     ; save current ESP for later
     mov  eax, esp
     ; change to new stack
-    mov  esp, ebx
+    mov  esp, ecx
+    and  esp, 0xfffffff0
+    ; room for arguments
+    sub  esp, 16
+    
     ; store old ESP on new stack
-    push eax
-    ; call function that sees the delegate
+    mov  DWORD [esp  ], eax
+    ; delegate
+    mov  DWORD [esp+4], edx
+    ; call function that can call delegates
     call __context_switch_delegate
     ; restore old stack
-    pop  esp
+    mov  esp, [esp]
 
     ; restore special stuff
     pop  edi
