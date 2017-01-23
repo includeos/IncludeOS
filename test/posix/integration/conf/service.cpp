195602e8d257d143c2a1745263796297f1e76c16
Ingve Vormestrand <ingvev@gmail.com>
2017-01-23 09:22:07 +0100

    test: add vm.json for posix conf test

--- test/posix/integration/conf/service.cpp
+++ test/posix/integration/conf/service.cpp
@@ -1,3 +1,20 @@
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2015-2017 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -23,16 +40,16 @@ int main(int argc, char *argv[]) {
 
   // test environment variables
   char* value = getenv("HOME");
-	printf("HOME: %s\n", value);
+  printf("HOME: %s\n", value);
 
-	char* new_env = "CPPHOME=/usr/home/cpp";
-	res = putenv(new_env);
-	value = getenv("CPPHOME");
-	printf("CPPHOME: %s\n", value);
+  char* new_env = "CPPHOME=/usr/home/cpp";
+  res = putenv(new_env);
+  value = getenv("CPPHOME");
+  printf("CPPHOME: %s\n", value);
 
-	res = setenv("INCLUDEOS_CORE_DUMP", "core.dump", 1);
-	value = getenv("INCLUDEOS_CORE_DUMP");
-	printf("INCLUDEOS_CORE_DUMP: %s\n", value);
+  res = setenv("INCLUDEOS_CORE_DUMP", "core.dump", 1);
+  value = getenv("INCLUDEOS_CORE_DUMP");
+  printf("INCLUDEOS_CORE_DUMP: %s\n", value);
 
   INFO("Conf", "SUCCESS");
 }
