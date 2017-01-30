9008ba295215883650dfc0ae5a02b97a2060a1ce
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2017-01-30 13:44:15 +0100

    posix: Clean up header mess

--- api/kernel/mman.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef KERNEL_MMAN_HPP
-#define KERNEL_MMAN_HPP
-
-#include <sys/mman.h>
-
-#endif
--- api/posix/sys/stdlib.h
+++ api/posix/sys/stdlib.h
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,19 +22,19 @@
 #include_next <stdlib.h>
 
 // More C11 requirements here
-#include "quick_exit"
+#include <quick_exit>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  
+
   //New stuff in C11, required by libunwind, compiler-rt etc. in llvm
-  
+
 void *aligned_alloc( size_t alignment, size_t size );
 
 #ifdef __cplusplus
 }
-#endif 
+#endif
 #endif //SYS_STDLIB_H
 
 
