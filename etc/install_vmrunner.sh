e4909d251be6e8102c82696d4eb9ccc32270a88d
Martin Nordsletten <mnordsletten@gmail.com>
2017-02-16 13:47:27 +0100

    vmrunner: Add three different ways of installing vmrunner

--- etc/install_vmrunner.sh
+++ etc/install_vmrunner.sh
@@ -3,14 +3,18 @@
 # Installs the vmrunner python package
 
 INCLUDEOS_SRC=${INCLUDEOS_SRC-$HOME/IncludeOS}
-VMRUNNER_GLOBAL_INSTALL=${VMRUNNER_GLOBAL_INSTALL-OFF}
+INCLUDEOS_VMRUNNER_INSTALL=${INCLUDEOS_VMRUNNER_INSTALL-LOCAL} 
 
 # Install vmrunner globally if specified
 pushd $INCLUDEOS_SRC/vmrunner
-if [ "$VMRUNNER_GLOBAL_INSTALL" = ON ]; then 
+if [ "$INCLUDEOS_VMRUNNER_INSTALL" = GLOBAL ]; then 
 	sudo python setup.py install
-else
+elif [ "$INCLUDEOS_VMRUNNER_INSTALL" = DEVELOP ]; then
+	python setup.py develop --user
+elif [ "$INCLUDEOS_VMRUNNER_INSTALL" = LOCAL ]; then
 	python setup.py install --user
+else
+	echo ">>>Error<<< \n Wrong INCLUDEOS_VMRUNNER_INSTALL variable, set to GLOBAL, DEVELOP OR LOCAL"
 fi
 popd
 
