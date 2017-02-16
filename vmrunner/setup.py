1c9339c5479a682059fe147945caad1a2ab25975
Martin Nordsletten <mnordsletten@gmail.com>
2017-02-16 10:58:19 +0100

    vmrunner: Moved boot to vmrunner package as script

--- vmrunner/setup.py
+++ vmrunner/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='vmrunner',
       version='0.1',
+      scripts=['vmrunner/boot'],
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False
