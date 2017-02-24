5371c21ecbde4ff4963b192cdda3145d7a2db52d
fwsgonzo <fwsgonzo@hotmail.com>
2017-02-24 21:00:59 +0100

    x86: Preserve eax in start.asm enable_sse

--- src/arch/x86/start.asm
+++ src/arch/x86/start.asm
@@ -60,6 +60,7 @@ _refresh_cs:
   ret
 
 enable_sse:
+  push eax        ;preserve eax for multiboot
   mov eax, cr0
   and ax, 0xFFFB  ;clear coprocessor emulation CR0.EM
   or ax, 0x2      ;set coprocessor monitoring  CR0.MP
@@ -67,6 +68,7 @@ enable_sse:
   mov eax, cr4
   or ax, 3 << 9   ;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
   mov cr4, eax
+  pop eax
   ret
 
 ALIGN 32
