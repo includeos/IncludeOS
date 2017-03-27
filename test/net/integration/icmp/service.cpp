3458b8a3b1b30edf2ab6440ba7b322cf451c4840
Annika Hammervoll <annika_limfe@hotmail.com>
2017-03-28 00:05:35 +0200

    ICMP integration test: Setting seconds to wait for ping reply and added ping with DNS resolution (in progress)

--- test/net/integration/icmp/service.cpp
+++ test/net/integration/icmp/service.cpp
@@ -25,7 +25,9 @@ void Service::start(const std::string&)
   auto& inet = Inet4::stack<0>();
   inet.network_config({  10,  0,  0, 45 },    // IP
                       { 255, 255, 0,  0 },    // Netmask
-                      {  10,  0,  0,  1 } );  // Gateway
+                      {  10,  0,  0,  1 },    // Gateway
+                      {   8,  8,  8,  8 }     // DNS
+  );
   printf("Service IP address is %s\n", inet.ip_addr().str().c_str());
 
   // ping gateway
@@ -36,15 +38,26 @@ void Service::start(const std::string&)
       printf("No reply received from gateway\n");
   });
 
+  /* TODO
+  // If IP forwarding on:
+  inet.icmp().ping("google.com", [](ICMP_view pckt) {
+    if (pckt)
+      printf("Received packet\n%s\n", pckt.to_string().c_str());
+    else
+      printf("No reply received from google.com. Identifier: %u. Sequence number: %u\n", pckt.id(), pckt.seq());
+  });*/
+
   /* If IP forwarding on:
   // ping google.com with callback
   inet.icmp().ping(IP4::addr{193,90,147,109}, [](ICMP_view pckt) {
-    if (pckt.is_reply()) // or pckt.type() != icmp4::Type::NO_REPLY
+    if (pckt)
       printf("Received packet\n%s\n", pckt.to_string().c_str());
     else
       printf("No reply received from 193.90.147.109. Identifier: %u. Sequence number: %u\n", pckt.id(), pckt.seq());
   });
   */
+
+  const int wait = 10;
 
   // No reply-pings
   // Waiting 30 seconds for reply
@@ -53,7 +66,7 @@ void Service::start(const std::string&)
       printf("Received packet from 10.0.0.42\n%s\n", pckt.to_string().c_str());
     else
       printf("No reply received from 10.0.0.42\n");
-  });
+  }, wait);
 
   // Waiting 30 seconds for reply
   inet.icmp().ping(IP4::addr{10,0,0,43}, [](ICMP_view pckt) {
@@ -61,8 +74,9 @@ void Service::start(const std::string&)
       printf("Received packet from 10.0.0.43\n%s\n", pckt.to_string().c_str());
     else
       printf("No reply received from 10.0.0.43\n");
-  });
+  }, wait);
 
   // Also possible without callback:
+  // inet.icmp().ping("google.com");
   // inet.icmp().ping(IP4::addr{193,90,147,109});  // google.com
 }
