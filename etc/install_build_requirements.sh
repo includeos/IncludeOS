a18d3627b91915ba4b4de03c4c7e284599d43cfc
Martin Nordsletten <mnordsletten@gmail.com>
2017-03-30 11:25:39 +0200

    install_dependencies: Added a visual check for dependency installation

--- etc/install_build_requirements.sh
+++ etc/install_build_requirements.sh
@@ -1,11 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Install dependencies
 
-SYSTEM=$1
-RELEASE=$2
+############################################################
+# OPTIONS:
+############################################################
 
-DEPENDENCIES="curl make clang cmake nasm bridge-utils qemu jq python-jsonschema python-psutil"
+BUILD_DEPENDENCIES="curl make clang cmake nasm bridge-utils qemu jq python-jsonschema python-psutil"
+TEST_DEPENDENCIES="g++ g++-multilib python-junit.xml"
+
+############################################################
+# COMMAND LINE PROPERTIES:
+############################################################
+
+# Initialize variables:
+SYSTEM=0
+RELEASE=0
+CHECK_INSTALLED=0
+DEPENDENCIES_TO_INSTALL=build
+
+while getopts "h?s:r:cd:" opt; do
+    case "$opt" in
+    h|\?)
+        printf "%s\n" "Options:"\
+                "-s System: What system to install on"\
+                "-r Release: What release of said system"\
+                "-c Check installed: Flag for checking if dependencies are installed"
+                "-d Dependencies to install: [build | test | all] are the options"
+        exit 0 ;;
+    s)  SYSTEM=$OPTARG ; shift 2 ;;
+    r)  RELEASE=$OPTARG ; shift 2 ;;
+    c)  CHECK_INSTALLED=1 ;;
+	d)  DEPENDENCIES_TO_INSTALL=$OPTARG ; shift 2 ;;
+    esac
+done
+
+# Figure out which dependencies to check
+case "$DEPENDENCIES_TO_INSTALL" in
+	build) ALL_DEPENDENCIES=$BUILD_DEPENDENCIES ;;
+	test) ALL_DEPENDENCIES=$TEST_DEPENDENCIES ;;
+	all) ALL_DEPENDENCIES="$BUILD_DEPENDENCIES $TEST_DEPENDENCIES" ;;
+esac
+
+############################################################
+# CHECK INSTALLED PACKAGES:
+############################################################
+
+if [ $CHECK_INSTALLED -eq 1 ]; then
+	printf "%-15s %-20s %s \n"\
+		   "Status" "Package" "Version"\
+		   "------" "-------" "-------"
+	for package in $ALL_DEPENDENCIES; do
+		dpkg-query -W $package > /dev/null 2>&1
+		if [ $? -eq 0 ]; then
+			printf '\e[32m%-15s\e[0m %-20s %s \n'\
+				"INSTALLED" $(dpkg-query -W $package)
+		else
+			printf '\e[31m%-15s\e[0m %-20s %s \n'\
+				"MISSING" $package
+			DEPENDENCIES="$DEPENDENCIES $package"
+		fi
+	done
+fi
+
+############################################################
+# INSTALL MISSING PACKAGES:
+############################################################
 
 case $SYSTEM in
     "Darwin")
