e1146af1e091d9163f9f3c25deba8f25f836c6af
Gonzo <fwsgonzo@hotmail.com>
2017-03-30 13:44:19 +0200

    service: Remove erroneous extern C from service name

--- src/service_name.cpp
+++ src/service_name.cpp
@@ -17,5 +17,5 @@
 
 #include <service>
 
-extern "C" const char* service_name__ = SERVICE_NAME;
-extern "C" const char* service_binary_name__ = SERVICE;
+const char* service_name__ = SERVICE_NAME;
+const char* service_binary_name__ = SERVICE;
