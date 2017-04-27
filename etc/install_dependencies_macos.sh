39e2983343254f60f2cdb8a22544b14953de43b9
Martin Nordsletten <mnordsletten@gmail.com>
2017-04-27 12:46:11 +0200

    install_mac: Now has proper fail checking for install on mac

--- etc/install_dependencies_macos.sh
+++ etc/install_dependencies_macos.sh
@@ -10,33 +10,128 @@ export INCLUDEOS_SRC=${INCLUDEOS_SRC:-"~/IncludeOS"}
 export INCLUDEOS_PREFIX=${INCLUDEOS_PREFIX:-"/usr/local"}
 export INCLUDEOS_BIN=$INCLUDEOS_PREFIX/includeos/bin # Where to link stuff 
 
-echo -e "\n###########################################"
-echo -e " IncludeOS dependency installation for Mac"
-echo -e "###########################################"
+INSTALLED_BREW=0
+INSTALLED_BREW_PACKAGES=0 
+INSTALLED_PIP=0
+INSTALLED_PIP_PACKAGES=0
+INSTALLED_BINUTILS=0
+ALL_DEPENDENCIES="llvm38 nasm cmake jq qemu Caskroom/cask/tuntap"
+PIP_MODS="jsonschema psutil"
 
-echo -e "\n# Prequisites:\n
-    - homebrew (Mac package manager - https://brew.sh)
-    - \`/usr/local\` directory with write access
-    - \`/usr/local/bin\` added to your PATH
-    - (Recommended) Xcode CLT (Command Line Tools)"
+############################################################
+# COMMAND LINE PROPERTIES:
+############################################################
+
+# Initialize variables:
+CHECK_ONLY=0
+PRINT_INSTALL_STATUS=0
+
+while getopts "h?cp" opt; do
+    case "$opt" in
+    h|\?)
+        printf "%s\n" "Options:"\
+				"-c Only check: Will only check what packages are needed (will always print status as well)"\
+                "-p Print install status: Flag for wheter or not to print dependency status"
+        exit 0 ;;
+    c)  CHECK_ONLY=1 ; PRINT_INSTALL_STATUS=1;;
+	p)  PRINT_INSTALL_STATUS=1 ;;
+    esac
+done
 
 ############################################################
 # BREW DEPENDENCIES:
 ############################################################
 
-BUILD_DEPENDENCIES="homebrew/versions/llvm38 nasm cmake jq qemu Caskroom/cask/tuntap"
-
-# Brew update
-if ! brew update; then
-    printf "%s\n" ">>> Sorry <<<"\
-		   "Cannot find brew! Visit http://brew.sh/ for how-to install. Aborting"
-	exit 1
+# Brew itself
+if brew help > /dev/null 2>&1; then
+	INSTALLED_BREW=1
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "Brew -> INSTALLED"
+	fi
+else
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "Brew -> MISSING Visit http://brew.sh/ for how-to install. Aborting"
+	fi
 fi
 
-echo ">>> Installing: $BUILD_DEPENDENCIES"
-for formula in $BUILD_DEPENDENCIES; do
-	brew install $formula
-done
+# Individual packages installed with brew
+if [ $INSTALLED_BREW -eq 1 ]; then
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "     Brew installed packages"
+		printf "     %-15s %-20s %s \n"\
+			   "Status" "Package" "Version"\
+			   "------" "-------" "-------"
+		for package in $ALL_DEPENDENCIES; do
+			brew ls $package > /dev/null 2>&1
+			if [ $? -eq 0 ]; then
+				printf '     \e[32m%-15s\e[0m %-20s %s \n'\
+					"INSTALLED" $(brew ls --versions $package)
+			elif brew cask ls $package; then
+				printf '     \e[32m%-15s\e[0m %-20s %s \n'\
+					"INSTALLED" $(brew cask ls --versions $package)
+			else
+				printf '     \e[31m%-15s\e[0m %-20s %s \n'\
+					"MISSING" $package
+				BREW_DEPENDENCIES_TO_INSTALL="$BREW_DEPENDENCIES_TO_INSTALL $package"
+			fi
+		done
+	fi
+	if [[ $CHECK_ONLY -eq 0 && ${#BREW_DEPENDENCIES_TO_INSTALL} -gt 1 ]]; then
+		echo ">>> Installing: $BREW_DEPENDENCIES_TO_INSTALL"
+		for formula in $BREW_DEPENDENCIES_TO_INSTALL; do
+			brew install $formula
+		done
+	fi
+fi
+
+	
+############################################################
+# PYTHON DEPENDENCIES:
+############################################################
+
+# pip itself
+if pip list >/dev/null 2>&1; then
+	INSTALLED_PIP=1
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "python pip -> INSTALLED"
+	fi
+else
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "python pip -> MISSING"
+	fi
+	if [ $CHECK_ONLY -eq 0 ]; then
+		curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+		sudo python get-pip.py
+		rm get-pip.py
+		INSTALLED_PIP=1
+	fi
+fi
+
+# Individual packages installed with pip
+if [ $INSTALLED_PIP -eq 1 ]; then
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "     pip installed packages"
+		printf "     %-15s %-20s %s \n"\
+			   "Status" "Package" "Version"\
+			   "------" "-------" "-------"
+		for package in $PIP_MODS; do
+			python -c "import $package"> /dev/null 2>&1
+			if [ $? -eq 0 ]; then
+				printf '     \e[32m%-15s\e[0m %-20s %s \n'\
+					"INSTALLED" $(pip list 2> /dev/null | grep $package)
+			else
+				printf '     \e[31m%-15s\e[0m %-20s %s \n'\
+					"MISSING" $package
+				PIP_MODS_TO_INSTALL="$PIP_MODS_TO_INSTALL $package"
+			fi
+		done
+	fi
+	if [[ $CHECK_ONLY -eq 0 && ${#PIP_MODS_TO_INSTALL} -gt 1 ]]; then
+		echo ">>> Installing: $PIP_MODS"
+		sudo pip install ${PIP_MODS[*]}
+	fi
+fi
+
 
 ############################################################
 # BINUTILS:
@@ -51,40 +146,20 @@ STRIP_INC=$BINUTILS_BIN"strip"
 RANLIB_INC=$BINUTILS_BIN"ranlib"
 
 if [[ -e  $LD_INC && -e $AR_INC && -e $OBJCOPY_INC && -e $STRIP_INC && -e $RANLIB_INC ]]; then
-	echo -e " > Binutils is present"
+	INSTALLED_BINUTILS=1
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "binutils -> INSTALLED"
+	fi
 else
-	# Assume script is called from root, else it won't work..
-	$INCLUDEOS_SRC/etc/install_binutils.sh
+	if [ $PRINT_INSTALL_STATUS -eq 1 ]; then
+		printf "%s\n" "binutils -> MISSING"
+	fi
+	if [ $CHECK_ONLY -eq 0 ]; then
+		# Assume script is called from root, else it won't work..
+		$INCLUDEOS_SRC/etc/install_binutils.sh
+		INSTALLED_BINUTILS=1
+	fi
 fi
-
-
-############################################################
-# PYTHON DEPENDENCIES:
-############################################################
-
-PIP_MODS="jsonschema psutil"
-echo -e "\n python pip\t - for installing necessary python modules used when booting services: ${PIP_MODS[*]}"
-
-## Check if pip is installed ##
-if command -v pip >/dev/null 2>&1; then
-	echo -e " > Found"
-	pip install ${PIP_MODS[*]}
-else
-	echo -e " > Not found"
-	echo -e "\n>> Installing pip (with sudo)"
-	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-	sudo python get-pip.py
-	rm get-pip.py
-fi
-
-############################################################
-# XCODE CLT:
-############################################################
-## WARN ABOUT XCODE CLT ##
-
-echo -e "\n NOTE: Cannot tell if Xcode Command Line Tools is installed - installation MAY fail if not installed."
-echo -e " > Install with: xcode-select --install"
-
 
 ############################################################
 # SYMLINK CROSSCOMPILE DEPENDENCIES:
@@ -111,6 +186,15 @@ echo -e ">> $SRC_NASM > $INCLUDEOS_BIN/nasm"
 
 echo -e "\n>>> Done symlinking dependencies to $INCLUDEOS_BIN"
 
-echo -e "\n###########################################"
-echo -e " Mac dependency installation done."
-echo -e "###########################################\n"
+
+############################################################
+# EXIT:
+############################################################
+
+
+
+echo -e Brew installed: $INSTALLED_BREW
+echo -e Brew dependencies installed: $INSTALLED_BREW
+echo -e Pip installed: $INSTALLED_PIP
+echo -e Pip installed: $INSTALLED_PIP
+echo -e Binutils installed: $INSTALLED_BINUTILS
