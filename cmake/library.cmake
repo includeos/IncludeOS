758144b461cf7c20b6f68de45548fe62c887b789
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2017-05-02 10:54:55 +0200

    cmake: Updated library cmake

--- cmake/library.cmake
+++ cmake/library.cmake
@@ -7,14 +7,28 @@ if (NOT DEFINED ENV{INCLUDEOS_PREFIX})
   set(ENV{INCLUDEOS_PREFIX} /usr/local)
 endif()
 
-# test compiler
-if(CMAKE_COMPILER_IS_GNUCC)
-	# currently gcc is not supported due to problems cross-compiling a unikernel
-	# (i.e., building a 32bit unikernel (only supported for now) on a 64bit system)
-	message(FATAL_ERROR "GCC is not currently supported, please clean-up build directory and configure for clang through CC and CXX environment variables")
-endif(CMAKE_COMPILER_IS_GNUCC)
+set(INSTALL_LOC $ENV{INCLUDEOS_PREFIX}/includeos)
 
-set(CMAKE_ASM_NASM_OBJECT_FORMAT "elf")
+# TODO: Verify that the OS libraries exist
+set(ARCH x86_64)
+if(DEFINED ENV{ARCH})
+  set(ARCH $ENV{ARCH})
+endif()
+message(STATUS "Target CPU architecture ${ARCH}")
+
+set(TRIPLE "${ARCH}-pc-linux-elf")
+set(CMAKE_CXX_COMPILER_TARGET ${TRIPLE})
+set(CMAKE_C_COMPILER_TARGET ${TRIPLE})
+message(STATUS "Target triple ${TRIPLE}")
+
+# Assembler
+if ("${ARCH}" STREQUAL "x86_64")
+  set (ARCH_INTERNAL "ARCH_X64")
+  set(CMAKE_ASM_NASM_OBJECT_FORMAT "elf64")
+else()
+  set (ARCH_INTERNAL "ARCH_X86")
+  set(CMAKE_ASM_NASM_OBJECT_FORMAT "elf")
+endif()
 enable_language(ASM_NASM)
 
 # defines $CAPABS depending on installation
@@ -31,6 +45,9 @@ option(debug "Build with debugging symbols (OBS: increases binary size)" OFF)
 option(minimal "Build for minimal size" OFF)
 option(stripped "reduce size" OFF)
 
+add_definitions(-DARCH_${ARCH})
+add_definitions(-DARCH="${ARCH}")
+
 set(OPTIMIZE "-O2")
 if (minimal)
   set(OPTIMIZE "-Os")
@@ -39,17 +56,22 @@ if (debug)
   set(CAPABS "${CAPABS} -g")
 endif()
 
-# these kinda work with llvm
-set(CMAKE_CXX_FLAGS "-MMD -target i686-elf ${CAPABS} ${OPTIMIZE} ${WARNS} -c -m32 -std=c++14 -D_LIBCPP_HAS_NO_THREADS=1")
-set(CMAKE_C_FLAGS "-MMD -target i686-elf ${CAPABS} ${OPTIMIZE} ${WARNS} -c -m32")
+if (CMAKE_COMPILER_IS_GNUCC)
+  set(CMAKE_CXX_FLAGS "-m32 -MMD ${CAPABS} ${WARNS} -nostdlib -fno-omit-frame-pointer -c -std=c++14 -D_LIBCPP_HAS_NO_THREADS=1")
+  set(CMAKE_C_FLAGS "-m32 -MMD ${CAPABS} ${WARNS} -nostdlib -fno-omit-frame-pointer -c")
+else()
+  # these kinda work with llvm
+  set(CMAKE_CXX_FLAGS "-MMD ${CAPABS} ${OPTIMIZE} ${WARNS} -nostdlib -nostdlibinc -fno-omit-frame-pointer -c -std=c++14 -D_LIBCPP_HAS_NO_THREADS=1")
+  set(CMAKE_C_FLAGS "-MMD ${CAPABS} ${OPTIMIZE} ${WARNS} -nostdlib -nostdlibinc -fno-omit-frame-pointer -c")
+endif()
 
 # includes
 include_directories(${LOCAL_INCLUDES})
-include_directories($ENV{INCLUDEOS_PREFIX}/includeos/api/posix)
-include_directories($ENV{INCLUDEOS_PREFIX}/includeos/include/libcxx)
-include_directories($ENV{INCLUDEOS_PREFIX}/includeos/include/newlib)
-include_directories($ENV{INCLUDEOS_PREFIX}/includeos/api)
-include_directories($ENV{INCLUDEOS_PREFIX}/includeos/include)
+include_directories(${INSTALL_LOC}/api/posix)
+include_directories(${INSTALL_LOC}/${ARCH}/include/libcxx)
+include_directories(${INSTALL_LOC}/${ARCH}/include/newlib)
+include_directories(${INSTALL_LOC}/api)
+include_directories(${INSTALL_LOC}/include)
 include_directories($ENV{INCLUDEOS_PREFIX}/include)
 
 add_library(${LIBRARY_NAME} STATIC ${SOURCES})
