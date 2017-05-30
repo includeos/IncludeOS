fb6fd2e3bd46f89a4a59e54f943f4654e6687e9c
fwsGonzo <fwsgonzo@hotmail.com>
2017-05-30 14:20:51 +0200

    WIP enable pthreads support for libcxxabi

--- api/posix/pthread.h
+++ api/posix/pthread.h
@@ -35,12 +35,15 @@ typedef struct {} pthread_barrier_t;
 typedef struct {} pthread_barrierattr_t;
 typedef struct {} pthread_cond_t;
 typedef struct {} pthread_condattr_t;
-typedef struct {} pthread_mutex_t;
 typedef struct {} pthread_mutexattr_t;
 typedef struct {} pthread_rwlock_t;
 typedef struct {} pthread_rwlockattr_t;
 typedef struct {} pthread_spinlock_t;
 
+typedef volatile int spinlock_t __attribute__((aligned(128)));
+typedef struct {
+  spinlock_t spinlock;
+} pthread_mutex_t;
 
 int pthread_cond_broadcast(pthread_cond_t* cond);
 int pthread_cond_signal(pthread_cond_t* cond);
@@ -49,6 +52,9 @@ int pthread_cond_timedwait(pthread_cond_t* cond,
                            const struct timespec* abstime);
 int pthread_cond_wait(pthread_cond_t* cond,
                       pthread_mutex_t* mutex);
+
+int pthread_create(pthread_t *, const pthread_attr_t *, void *(*)(void *), void *);
+int pthread_join(pthread_t, void **);
 int pthread_detach(pthread_t thread);
 int pthread_equal(pthread_t t1, pthread_t t2);
 
@@ -59,9 +65,16 @@ int pthread_mutexattr_destroy(pthread_mutexattr_t *attr);
 int pthread_mutexattr_init(pthread_mutexattr_t *attr);
 int pthread_mutexattr_gettype(const pthread_mutexattr_t *attr, int *type);
 int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int type);
-int pthread_mutex_destroy(pthread_mutex_t *mutex);
-int pthread_mutex_init(pthread_mutex_t* mutex,
-                         const pthread_mutexattr_t* attr);
-int pthread_create(pthread_t *, const pthread_attr_t *, void *(*)(void *), void *);
-int pthread_join(pthread_t, void **);
 
+typedef struct {} pthread_once_t;
+int pthread_once(pthread_once_t *once_control, void (*init_routine)(void));
+
+#define PTHREAD_MUTEX_INITIALIZER {0}
+#define PTHREAD_COND_INITIALIZER {}
+#define PTHREAD_ONCE_INIT {}
+
+int pthread_mutex_init(pthread_mutex_t* mutex, const pthread_mutexattr_t* attr);
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_trylock(pthread_mutex_t *mutex);
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+int pthread_mutex_destroy(pthread_mutex_t *mutex);
--- src/posix/pthread.cpp
+++ src/posix/pthread.cpp
@@ -17,12 +17,11 @@
 
 #include <pthread.h>
 #include <cstdio>
-#include <vector>
+
+/*
 #include <fiber>
 #include <unordered_map>
-
 std::unordered_map<pthread_t, Fiber> fibers;
-
 
 int pthread_create(pthread_t* th, const pthread_attr_t* attr, void *(*func)(void *), void* args) {
 
@@ -33,12 +32,89 @@ int pthread_create(pthread_t* th, const pthread_attr_t* attr, void *(*func)(void
   fibers[new_id].start();
 
   *th = new_id;
-
   return 0;
-};
+}
 
 int pthread_join(pthread_t thread, void **value_ptr) {
   void* retval = fibers[thread].ret<void*>();
   *value_ptr = retval;
   return 0;
-};
+}
+*/
+
+#include <smp>
+int pthread_mutex_init(pthread_mutex_t* mutex, const pthread_mutexattr_t* attr)
+{
+  if (mutex == nullptr) return 1;
+  mutex->spinlock = 0;
+}
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+  if (mutex == nullptr) return 1;
+  lock(mutex->spinlock);
+  SMP_PRINT("Locked %p\n", mutex);
+  return 0;
+}
+int pthread_mutex_trylock(pthread_mutex_t *mutex)
+{
+  if (mutex == nullptr) return 1;
+  if (mutex->spinlock) return 1;
+  lock(mutex->spinlock);
+  return 0;
+}
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+  if (mutex == nullptr) return 1;
+  SMP_PRINT("Unlocked %p\n", mutex);
+  unlock(mutex->spinlock);
+  return 0;
+}
+int pthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+  return 0;
+}
+
+int pthread_once(pthread_once_t*, void (*routine)())
+{
+  thread_local bool executed = false;
+  if (!executed) {
+    printf("Executing pthread_once routine %p\n", routine);
+    executed = true;
+    routine();
+  }
+  return 0;
+}
+
+int pthread_cond_wait(pthread_cond_t* cond, pthread_mutex_t* mutex)
+{
+  SMP_PRINT("pthread_cond_wait\n")
+  return 0;
+}
+int pthread_cond_broadcast(pthread_cond_t* cond)
+{
+  SMP_PRINT("pthread_cond_broadcast\n")
+  return 0;
+}
+
+#include <vector>
+std::vector<const void*> key_vec;
+spinlock_t             key_lock = 0;
+
+void* pthread_getspecific(pthread_key_t key)
+{
+  scoped_spinlock spinlock(key_lock);
+  return (void*) key_vec.at(key);
+}
+int pthread_setspecific(pthread_key_t key, const void *value)
+{
+  scoped_spinlock spinlock(key_lock);
+  key_vec.at(key) = value;
+  return 0;
+}
+int pthread_key_create(pthread_key_t *key, void (*destructor)(void*))
+{
+  scoped_spinlock spinlock(key_lock);
+  key_vec.push_back(nullptr);
+  *key = key_vec.size()-1;
+  return 0;
+}
