d34741bb93ae2e55bf75403e2e81ebcd9864cb75
Ricardo Koller <kollerr@us.ibm.com>
2017-06-16 14:50:54 +0000

    Adding solo5 version of __serial_print1

--- src/platform/x86_pc/serial1.cpp
+++ src/platform/x86_pc/serial1.cpp
@@ -1,17 +1,10 @@
 #include <hw/serial.hpp>
-extern "C" {
-int solo5_console_write(const char *buf, size_t n);
-}
 
 static const uint16_t port = 0x3F8; // Serial 1
 
 extern "C"
 void __serial_print1(const char* cstr)
 {
-  // XXX: some modules are calling this directly
-  //solo5_console_write(cstr, strlen(cstr));
-  //return;
-
   while (*cstr) {
     while (not (hw::inb(port + 5) & 0x20));
       hw::outb(port, *cstr++);
