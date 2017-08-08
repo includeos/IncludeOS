6362774d18b57667b010dededc2eb05e9b7f02f3
Gonzo <fwsgonzo@hotmail.com>
2017-08-08 11:17:45 +0200

    Remove extraneous clobbers

--- hotswap.cpp
+++ hotswap.cpp
@@ -1,7 +1,7 @@
 /**
  * Master thesis
  * by Alf-Andre Walla 2016-2017
- * 
+ *
 **/
 asm(".org 0x8000");
 #define SOFT_RESET_MAGIC   0xFEE1DEAD
@@ -13,7 +13,7 @@ void hotswap(const char* base, int len, char* dest, void* start, void* reset_dat
   for (int i = 0; i < len; i++)
     dest[i] = base[i];
   // jump to _start
-  asm volatile("jmp *%2" : : "a" (SOFT_RESET_MAGIC), "b" (reset_data), "c" (start) : "eax", "ebx", "ecx");
+  asm volatile("jmp *%2" : : "a" (SOFT_RESET_MAGIC), "b" (reset_data), "c" (start));
   asm volatile(
   ".global __hotswap_length;\n"
   "__hotswap_length:" );
