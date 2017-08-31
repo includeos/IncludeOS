835dae4a0249f44bfdfb1d0b1b620619456c8678
Gonzo <fwsgonzo@hotmail.com>
2017-08-31 15:49:46 +0200

    examples: Add LiveUpdate example

---
+++ examples/LiveUpdate/README.md
@@ -0,0 +1,9 @@
+### LiveUpdate Demo Service
+
+```
+boot .
+Ctrl+Shift+T
+./update.sh
+```
+
+This service should start an instance of IncludeOS that brings up a minimal web service on port 80 with static content. When running test.sh the service should be updated with itself.
---
+++ examples/LiveUpdate/server.hpp
@@ -0,0 +1,39 @@
+/**
+ * Master thesis
+ * by Alf-Andre Walla 2016-2017
+ *
+**/
+#pragma once
+#include <stdexcept>
+
+static inline
+void server(net::Inet<net::IP4>& inet,
+            const uint16_t port,
+            delegate<void(liu::buffer_t&)> callback)
+{
+  auto& server = inet.tcp().listen(port);
+  server.on_connect(
+  net::tcp::Connection::ConnectCallback::make_packed(
+  [callback, port] (auto conn)
+  {
+    auto* buffer = new liu::buffer_t;
+    buffer->reserve(3*1024*1024);
+    printf("Receiving blob on port %u\n", port);
+
+    // retrieve binary
+    conn->on_read(9000,
+    [conn, buffer] (net::tcp::buffer_t buf, size_t n)
+    {
+      buffer->insert(buffer->end(), buf.get(), buf.get() + n);
+    })
+    .on_disconnect(
+    net::tcp::Connection::DisconnectCallback::make_packed(
+    [buffer, callback] (auto conn, auto) {
+      printf("* Blob size: %u b  stored at %p\n",
+            (uint32_t) buffer->size(), buffer->data());
+      callback(*buffer);
+      delete buffer;
+      conn->close();
+    }));
+  }));
+}
