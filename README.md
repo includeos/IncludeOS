6bb340df4f4344b77152bd1bec4c4df2612267dd
fwsGonzo <fwsgonzo@hotmail.com>
2017-09-15 14:44:52 +0200

    Update to latest dev

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 
 Receive data from the instance:
 ```
-nc 10.0.0.42 1337 > bla.txt
+ncat 10.0.0.42 1337 --recv-only > bla.txt
 ```
 
 Send data to the instance:
 ```
-cat bla.txt | nc 10.0.0.42 1338
+cat bla.txt | ncat 10.0.0.42 1338 --send-only
 ```
 
 Configure by changing the variables at the top.
--- service.cpp
+++ service.cpp
@@ -15,12 +15,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <service>
-#include <cstdio>
+#include <os>
 #include <net/inet4>
 #include <statman>
 #include <profile>
-#include <os>
+#include <cstdio>
 
 using namespace net;
 using namespace net::tcp;
@@ -35,8 +34,6 @@ uint8_t   wscale{5};
 bool      timestamps{true};
 std::chrono::milliseconds dack{40};
 
-extern "C" void kernel_sanity_checks();
-
 void recv(size_t len)
 {
   received += len;
@@ -44,39 +41,35 @@ void recv(size_t len)
 
 uint64_t ts{0};
 
-void start_measure() {
+void start_measure()
+{
   received    = 0;
-  packets_rx  = Statman::get().get_by_name("eth0.packets_rx").get_uint64();
-  packets_tx  = Statman::get().get_by_name("eth0.packets_tx").get_uint64();
+  packets_rx  = Statman::get().get_by_name("eth0.ethernet.packets_rx").get_uint64();
+  packets_tx  = Statman::get().get_by_name("eth0.ethernet.packets_tx").get_uint64();
   printf("<Settings> DACK: %lli ms WSIZE: %u WS: %u CALC_WIN: %u TS: %s\n",
     dack.count(), winsize, wscale, winsize << wscale, timestamps ? "ON" : "OFF");
-
   ts          = OS::micros_since_boot();
-  printf("<START> %lld\n", ts);
 }
 
-void stop_measure() {
+void stop_measure()
+{
   auto diff   = OS::micros_since_boot() - ts;
 
-  kernel_sanity_checks();
-  packets_rx  = Statman::get().get_by_name("eth0.packets_rx").get_uint64() - packets_rx;
-  packets_tx  = Statman::get().get_by_name("eth0.packets_tx").get_uint64() - packets_tx;
-  printf("<STOP> %lld\n", OS::micros_since_boot());
+  StackSampler::print(15);
+
+  packets_rx  = Statman::get().get_by_name("eth0.ethernet.packets_rx").get_uint64() - packets_rx;
+  packets_tx  = Statman::get().get_by_name("eth0.ethernet.packets_tx").get_uint64() - packets_tx;
   printf("Packets RX [%llu] TX [%llu]\n", packets_rx, packets_tx);
   double durs   = ((double)diff) / 1000 / 1000;
   double mbits  = (received/(1024*1024)*8) / durs;
-  printf("Duration: %.2f s - Payload: %lld/%u MB - %.2f MBit/s\n", durs, received/(1024*1024), SIZE/(1024*1024), mbits);
-
-  StackSampler::print(30);
+  printf("Duration: %.2fs - Payload: %lld/%u MB - %.2f MBit/s\n",
+          durs, received/(1024*1024), SIZE/(1024*1024), mbits);
 }
 
-void Service::start() {
-  kernel_sanity_checks();
-}
+void Service::start() {}
 
 void Service::ready()
 {
-  kernel_sanity_checks();
   StackSampler::begin();
 
   auto& inet = Inet4::ifconfig();
@@ -98,23 +91,9 @@ void Service::ready()
 
   tcp.listen(1337).on_connect([](Connection_ptr conn)
   {
-    using namespace std::chrono;
-    auto id = Timers::periodic(100ms, 500ms,
-    [conn](uint32_t)
-    {
-      printf("State: %s, Writeq: %u bytes\n",
-            conn->state().to_string().c_str(), conn->sendq_remaining());
-    });
-
     printf("%s connected. Sending file %u MB\n", conn->remote().to_string().c_str(), SIZE/(1024*1024));
-    kernel_sanity_checks();
     start_measure();
 
-    conn->on_close([id]
-    {
-      Timers::stop(id);
-
-    });
     conn->on_disconnect([] (Connection_ptr self, Connection::Disconnect)
     {
       if(!self->is_closing())
@@ -129,7 +108,7 @@ void Service::ready()
     conn->close();
   });
 
-  tcp.listen(1338).on_connect([](Connection_ptr conn)
+  tcp.listen(1338).on_connect([](auto conn)
   {
     using namespace std::chrono;
 
@@ -141,8 +120,9 @@ void Service::ready()
     {
 
     });
-    conn->on_disconnect([] (Connection_ptr self, Connection::Disconnect)
+    conn->on_disconnect([] (auto self, auto reason)
     {
+      (void) reason;
       if(!self->is_closing())
         self->close();
 
