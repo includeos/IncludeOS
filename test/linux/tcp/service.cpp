8cca0f39a9810de3cbc083e0adcb17689823e1f7
Alfred Bratterud <alfred.bratterud@hioa.no>
2017-10-17 16:51:05 +0200

    test/linux: in-memory tcp2tcp test, gprof support

--- test/linux/tcp/service.cpp
+++ test/linux/tcp/service.cpp
@@ -20,21 +20,69 @@
 
 #include <os>
 #include <hw/devices.hpp>
+#include <kernel/events.hpp>
 #include <drivers/usernet.hpp>
 #include <net/inet4>
-#include <timers>
+
 
 using namespace std::chrono;
 
 inline UserNet& create_nic(){
   // the IncludeOS packet communicator
-  auto* usernet = new UserNet();
+  auto* usernet = new UserNet(1504);
   // register driver for superstack
   auto driver = std::unique_ptr<hw::Nic> (usernet);
   hw::Devices::register_device<hw::Nic> (std::move(driver));
 
   return *usernet;
 }
+
+const int chunksize = 1024 * 1024;
+const int num_chunks = 512;
+
+
+
+class Async_device {
+public:
+  using delg = delegate<void(net::Packet_ptr)>;
+
+  void transmit(net::Packet_ptr pckt) {
+
+  };
+
+  void receive(net::Packet_ptr pckt) {
+    queue.push_back(std::move(pckt));
+    Events::get().trigger_event(event_id);
+  };
+
+  Async_device (delegate<void(net::Packet_ptr)> dr)
+    : driver_receive(dr)
+  {
+    event_id = Events::get().subscribe([this]{
+        while(! queue.empty()) {
+          this->driver_receive(std::move(queue.front()));
+          queue.pop_front();
+        }
+      });
+  }
+
+private:
+  delegate<void(net::Packet_ptr)> driver_receive;
+  int event_id = 0;
+  std::deque<net::Packet_ptr> queue;
+};
+
+
+std::unique_ptr<Async_device> dev1;
+std::unique_ptr<Async_device> dev2;
+
+using namespace std::chrono;
+milliseconds time_start;
+
+inline auto now() {
+  return duration_cast< milliseconds >(system_clock::now().time_since_epoch());
+}
+
 
 
 void Service::start()
@@ -45,9 +93,15 @@ void Service::start()
   auto& nic1 = create_nic();
   auto& nic2 = create_nic();
 
+  delegate<void(net::Packet_ptr)> delg1 {&nic1, &UserNet::receive};
+  delegate<void(net::Packet_ptr)> delg2 {&nic2, &UserNet::receive};
+
+  dev1 = std::make_unique<Async_device>(delg1);
+  dev2 = std::make_unique<Async_device>(delg2);
+
   // Connect them with a wire
-  nic1.set_transmit_forward({&nic2, &UserNet::receive});
-  nic2.set_transmit_forward({&nic1, &UserNet::receive});
+  nic1.set_transmit_forward({dev2.get(), &Async_device::receive});
+  nic2.set_transmit_forward({dev1.get(), &Async_device::receive});
 
   // Create IP stacks on top of the nic's and configure them
   auto& inet_server = net::Super_stack::get<net::IP4>(0);
@@ -55,38 +109,50 @@ void Service::start()
   inet_server.network_config({10,0,0,42}, {255,255,255,0}, {10,0,0,1});
   inet_client.network_config({10,0,0,43}, {255,255,255,0}, {10,0,0,1});
 
-  // Print some useful netstats every 30 secs
-  Timers::periodic(5s, 30s,
-  [&inet_server] (uint32_t) {
-    printf("<Service> TCP STATUS:\n%s\n", inet_server.tcp().status().c_str());
-  });
 
   // Set up a TCP server on port 80
   auto& server = inet_server.tcp().listen(80);
 
+  auto buf =  net::tcp::construct_buffer(chunksize);
+
   // Add a TCP connection handler
   server.on_connect(
   [] (net::tcp::Connection_ptr conn) {
-    printf("<Service> @on_connect: Connection %s successfully established.\n",
-            conn->remote().to_string().c_str());
-    // read async with a buffer size of 1024 bytes
-    // define what to do when data is read
-    conn->on_read(1024,
-                  [conn] (auto buf)
-    {
-      const std::string data((const char*) buf->data(), buf->size());
-      printf("Server received data: \n%s\n", data.c_str());
-    });
+
+    conn->on_read(chunksize, [conn] (auto buf) {
+        static size_t count_bytes = 0;
+
+        //printf("chunksize: %zu \n", buf->size());
+        assert(buf->size() <= chunksize);
+        count_bytes += buf->size();
+
+        if (count_bytes >= num_chunks * chunksize) {
+
+          auto timediff = now() - time_start;
+          assert(count_bytes == num_chunks * chunksize);
+
+          double time_sec = timediff.count()/1000.0;
+          double mbps = ((count_bytes * 8) / (1024.0 * 1024.0)) / time_sec;
+
+          printf("Server reveived %zu Mb in %f sec. - %f Mbps \n",
+                 count_bytes / (1024 * 1024), time_sec,  mbps);
+          OS::shutdown();
+        }
+
+      });
   });
 
-  printf("*** Linux userspace library demo started ***\n");
+  printf("*** Linux userspace tcp demo started ***\n");
 
-  inet_client.tcp().connect({{"10.0.0.42"}, 80}, [](auto conn){
+  time_start = now();
+  inet_client.tcp().connect({{"10.0.0.42"}, 80}, [buf](auto conn){
       if (not conn)
         std::abort();
 
-      conn->write("Hello world\n");
+      for (int i = 0; i < num_chunks; i++)
+        conn->write(buf);
+
     });
 
-  fflush(stdout);
+
 }
