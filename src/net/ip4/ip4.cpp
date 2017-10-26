10725579bd8f3161b4ccb7bba578b925b37341be
Alfred Bratterud <alfred@includeos.org>
2017-10-26 17:04:34 +0200

    ip4: loopback check now uses is_valid_source

--- src/net/ip4/ip4.cpp
+++ src/net/ip4/ip4.cpp
@@ -140,7 +140,7 @@ namespace net {
 
 
     // Drop / forward if my ip address doesn't match dest. or broadcast
-    if(not is_for_me(packet->ip_dst())) {
+    if(not stack_.is_valid_source(packet->ip_dst())) {
       if (forward_packet_) {
         forward_packet_(stack_, std::move(packet));
         PRINT("Packet forwarded \n");
