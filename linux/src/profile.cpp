9476d3566e488e02b7b98f15f0f5888a3f229428
fwsGonzo <fwsgonzo@hotmail.com>
2017-11-09 11:44:45 +0100

    linux: Update linux platform and add support for thinLTO

---
+++ linux/src/profile.cpp
@@ -0,0 +1,11 @@
+#include <profile>
+
+void StackSampler::begin() {}
+uint64_t StackSampler::samples_total() noexcept {
+  return 1;
+}
+uint64_t StackSampler::samples_asleep() noexcept {
+  return 0;
+}
+void StackSampler::print(int) {}
+void StackSampler::set_mode(mode_t) {}
