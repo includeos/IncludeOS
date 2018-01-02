89fe9ed169e5a5ed6fc66a8d9de5a01f3a190457
Alfred Bratterud <alfred@includeos.org>
2018-01-02 11:41:07 +0100

    util: bitmask operators for enum class

---
+++ api/util/bitops.hpp
@@ -0,0 +1,118 @@
+// -*- C++ -*-
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2017 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UTIL_BITOPS_HPP
+#define UTIL_BITOPS_HPP
+
+#include <type_traits>
+#include <cstdint>
+
+namespace util {
+inline namespace bitops {
+
+//
+// Enabling bitmask ops for enum class etc.
+//
+
+template<typename E>
+struct enable_bitmask_ops{
+  using type = typename std::underlying_type<E>::type;
+  static constexpr bool enable=false;
+};
+
+template<typename E, typename F>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type
+operator|(E lhs,F rhs){
+  using type = typename enable_bitmask_ops<E>::type;
+  return static_cast<E>(static_cast<type>(lhs) | static_cast<type>(rhs));
+}
+
+template<typename E, typename F>
+typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type
+constexpr operator&(E lhs,F rhs){
+  using type = typename enable_bitmask_ops<E>::type;
+  return static_cast<E>(static_cast<type>(lhs) & static_cast<type>(rhs));
+}
+
+template<typename E, typename F>
+typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type
+constexpr operator^(E lhs,F rhs){
+  using type = typename enable_bitmask_ops<F>::type;
+  return static_cast<E>(static_cast<type>(lhs) ^ static_cast<type>(rhs));
+}
+
+
+template<typename E, typename F>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type
+operator|=(E& lhs,F rhs){
+  using type = typename enable_bitmask_ops<F>::type;
+  lhs = static_cast<E>(static_cast<type>(lhs) | static_cast<type>(rhs));
+  return lhs;
+}
+
+template<typename E, typename F>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type&
+operator&=(E& lhs,F rhs){
+  using type = typename enable_bitmask_ops<F>::type;
+  lhs = static_cast<E>(static_cast<type>(lhs) & static_cast<type>(rhs));
+  return lhs;
+}
+
+template<typename E, typename F>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable,E>::type&
+operator^=(E& lhs,F rhs){
+  using type = typename enable_bitmask_ops<E>::type;
+  lhs = static_cast<E>(static_cast<type>(lhs) ^ static_cast<type>(rhs));
+  return lhs;
+}
+
+template<typename E>
+typename std::enable_if<enable_bitmask_ops<E>::enable, E>::type
+constexpr operator~(E flag){
+  using base_type = typename enable_bitmask_ops<E>::type;
+  return static_cast<E>(~static_cast<base_type>(flag));
+}
+
+
+template<typename E>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable, bool>::type
+has_flag(E flag){
+  using base_type = typename std::underlying_type<E>::type;
+  return static_cast<base_type>(flag);
+}
+
+template<typename E>
+constexpr typename std::enable_if<enable_bitmask_ops<E>::enable, bool>::type
+has_flag(E field, E flags){
+  return (field & flags) == flags ;
+}
+
+template<>
+struct enable_bitmask_ops<uintptr_t> {
+  using type = uintptr_t;
+  static constexpr bool enable = true;
+};
+
+// Check for power of two
+inline bool is_pow2(uintptr_t i)
+{ return i && !(i & (i - 1)); }
+
+} // ns bitops
+} // ns util
+
+#endif
---
+++ test/util/unit/bitops.cpp
@@ -0,0 +1,101 @@
+// This file is a part of the IncludeOS unikernel - www.includeos.org
+//
+// Copyright 2017 Oslo and Akershus University College of Applied Sciences
+// and Alfred Bratterud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <common.cxx>
+#include <util/bitmask_ops.hpp>
+
+enum class Flags : uint8_t {
+  none = 0x0,
+  a = 0x1,
+  b = 0x2,
+  ab = 0x3,
+    c = 0x4,
+  d = 0x8,
+  e = 0x10,
+  f = 0x20,
+  g = 0x40,
+  h = 0x80,
+  all = a | b | c | d | e | f | g | h
+};
+
+using namespace util::bitops;
+
+// Enable bitmask ops for the Flags enum
+template<>
+struct enable_bitmask_ops<Flags> {
+  using type = std::underlying_type<Flags>::type;
+  static constexpr bool enable = true;
+};
+
+CASE ("Using bitmask ops for an enum")
+{
+
+  Flags f {};
+  EXPECT(f == Flags::none);
+
+  // and, or, not, xor
+  EXPECT((f & Flags::a) == Flags::none);
+  EXPECT((f | Flags::a | Flags::b) == Flags::ab);
+  EXPECT(~f == Flags::all);
+  EXPECT((f ^ Flags::c) == Flags::c);
+
+  // Compound assignment
+  f |= Flags::e;
+  EXPECT(f == Flags::e);
+  EXPECT((uint8_t)f == 0x10);
+
+  f &= Flags::d;
+  EXPECT(f == Flags::none);
+
+  f |= Flags::c | Flags::h;
+  EXPECT(f == (Flags::c | Flags::h));
+  EXPECT((uint8_t)f == 0x84);
+
+  f ^= Flags::c;
+  EXPECT(f == Flags::h);
+
+  // boolean flag tests
+  f |= Flags::c | Flags::h;
+  EXPECT(has_flag(f, Flags::c));
+  EXPECT(has_flag(f, Flags::h));
+  EXPECT(has_flag(f, Flags::h | Flags::c));
+  EXPECT(not has_flag(f, Flags::a));
+  EXPECT(not has_flag(f, Flags::all));
+
+}
+
+// Enable bitmask ops for int, to use in combination with other enabled types
+template<>
+struct enable_bitmask_ops<int> {
+  using type = int;
+  static constexpr bool enable = true;
+};
+
+
+CASE ("Using bitmask ops an enum and an integral")
+{
+
+  Flags f { Flags::e };
+  EXPECT((f & 0x10) == Flags::e);
+  EXPECT((f | 0x20) == (Flags::e | Flags::f));
+  EXPECT((f ^ 0x20) == (Flags::e | Flags::f));
+
+  EXPECT((0x10 & f) == 0x10);
+  EXPECT((0x20 | f) == (0x10 | 0x20));
+  EXPECT((0x20 ^ f) == (0x10 | 0x20));
+
+}
