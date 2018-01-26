ca39a28cfe50c690491dece66a2df07c42450737
Alfred Bratterud <alfred@includeos.org>
2018-01-26 19:25:00 +0100

    musl: add compatibility patch

--- etc/build_musl.sh
+++ etc/build_musl.sh
@@ -15,9 +15,12 @@ pushd musl
 # Replace syscall API
 cp $INCLUDEOS_SRC/api/syscalls.h src/internal/includeos_syscalls.h
 cp $INCLUDEOS_SRC/etc/musl/syscall.h src/internal/
-
 rm -f arch/x86_64/syscall_arch.h
 rm -f arch/i386/syscall_arch.h
+
+# Compatibility patch
+git apply $INCLUDEOS_SRC/etc/musl/musl.patch || true
+
 
 # ln -s $INCLUDEOS_SRC/api/arch/syscalls.h arch/x86_64/syscall_arch.h
 # ln -s $INCLUDEOS_SRC/api/arch/syscalls.h arch/i386/syscall_arch.h
@@ -27,6 +30,9 @@ git checkout $musl_version
 make distclean || true
 
 export CFLAGS="$CFLAGS -target $ARCH-pc-linux-elf"
-./configure --prefix=$TEMP_INSTALL_DIR/$TARGET  --disable-shared --enable-optimize=* --target=$TARGET
+./configure --prefix=$TEMP_INSTALL_DIR/$TARGET  --disable-shared --enable-debug --target=$TARGET #--enable-optimize=*
 make $num_jobs
 make install
+
+
+trap - EXIT
