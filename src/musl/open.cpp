4fd20ac122b3fb6b22f0a8b4c5842632a4726c08
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2018-02-19 12:48:16 +0100

    musl: Updated systemcalls to use FD

--- src/musl/open.cpp
+++ src/musl/open.cpp
@@ -1,7 +1,22 @@
 #include "stub.hpp"
 #include <sys/stat.h>
 
+#include <fs/vfs.hpp>
+
 int sys_open(const char *pathname, int flags, mode_t mode = 0) {
+  try {
+    auto& entry = fs::VFS::get<FD_compatible>(pathname);
+    auto& fd = entry.open_fd();
+    return fd.get_id();
+  }
+  catch(const fs::VFS_err&) {
+    // VFS error for one of many reasons (not mounted, not fd compatible etc)
+  }
+  catch(const std::bad_function_call&) {
+    // Open fd delegate not set
+  }
+
+  errno = ENOENT;
   return -1;
 }
 
--- src/musl/read.cpp
+++ src/musl/read.cpp
@@ -1,7 +1,18 @@
 #include "common.hpp"
+#include <fd_map.hpp>
+
+static ssize_t sys_read(int fd, void* buf, size_t len) {
+  try {
+    auto& fildes = FD_map::_get(fd);
+    return fildes.read(buf, len);
+  }
+  catch(const FD_not_found&) {
+    errno = EBADF;
+    return -1;
+  }
+}
 
 extern "C"
-long syscall_SYS_read() {
-  STUB("read");
-  return 0;
+ssize_t syscall_SYS_read(int fd, void *buf, size_t nbyte) {
+  return strace(sys_read, "read", fd, buf, nbyte);
 }
