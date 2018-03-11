5a5af60593f5133156238a1c8139fcec018677cd
fwsGonzo <fwsgonzo@hotmail.com>
2018-03-11 22:40:27 +0100

    x86: Init serial1 on first use

--- src/platform/x86_pc/serial1.cpp
+++ src/platform/x86_pc/serial1.cpp
@@ -1,11 +1,19 @@
 #include <hw/serial.hpp>
 
 static const uint16_t port = 0x3F8; // Serial 1
+static char initialized = 0xFF;
 
 extern "C"
 __attribute__((no_sanitize("all")))
 void __init_serial1()
 {
+  initialized = false;
+}
+
+static inline void init_if_needed()
+{
+  if (initialized) return;
+  initialized = true;
   // properly initialize serial port
   hw::outb(port + 1, 0x00);    // Disable all interrupts
   hw::outb(port + 3, 0x80);    // Enable DLAB (set baud rate divisor)
@@ -19,6 +27,7 @@ extern "C"
 __attribute__((no_sanitize("all")))
 void __serial_print1(const char* cstr)
 {
+  init_if_needed();
   while (*cstr) {
     while (not (hw::inb(port + 5) & 0x20));
     hw::outb(port, *cstr++);
@@ -27,6 +36,7 @@ void __serial_print1(const char* cstr)
 extern "C"
 void __serial_print(const char* str, size_t len)
 {
+  init_if_needed();
   for (size_t i = 0; i < len; i++) {
     while (not (hw::inb(port + 5) & 0x20));
     hw::outb(port, str[i]);
