ce5f9961595be4161732b19af98a0ed76a3479b8
fwsGonzo <fwsgonzo@hotmail.com>
2018-03-11 22:47:18 +0100

    x86: Don't verify RO area with paging enabled

--- src/platform/x86_pc/sanity_checks.cpp
+++ src/platform/x86_pc/sanity_checks.cpp
@@ -17,14 +17,12 @@
 
 #include <cassert>
 #include <cstdint>
-#include <common>
 #include <kprint>
 #include <util/crc32.hpp>
 #include <kernel/elf.hpp>
 #include <kernel/syscalls.hpp>
 
-// NOTE: crc_to MUST NOT be initialized to zero
-static uint32_t crc_ro = CRC32_BEGIN();
+//#define ENABLE_CRC_RO
 const auto* LOW_CHECK_SIZE = (volatile int*) 0x200;
 
 // Global constructors
@@ -34,6 +32,10 @@ static void self_test_gconstr() {
   gconstr_value = 1;
 }
 
+#ifdef ENABLE_CRC_RO
+// NOTE: crc_ro MUST NOT be initialized to zero
+static uint32_t crc_ro = CRC32_BEGIN();
+
 static uint32_t generate_ro_crc() noexcept
 {
   extern char _TEXT_START_;
@@ -42,6 +44,7 @@ static uint32_t generate_ro_crc() noexcept
   extern char _RODATA_END_;
   return crc32_fast(&_TEXT_START_, &_RODATA_END_ - &_TEXT_START_);
 }
+#endif
 
 extern "C"
 void __init_sanity_checks() noexcept
@@ -49,19 +52,24 @@ void __init_sanity_checks() noexcept
   // zero low memory
   for (volatile int* lowmem = NULL; lowmem < LOW_CHECK_SIZE; lowmem++)
       *lowmem = 0;
+#ifdef ENABLE_CRC_RO
   // generate checksum for read-only portions of kernel
   crc_ro = generate_ro_crc();
+#endif
 }
 
 extern "C"
 void kernel_sanity_checks()
 {
+#ifdef ENABLE_CRC_RO
   // verify checksum of read-only portions of kernel
   uint32_t new_ro = generate_ro_crc();
+
   if (crc_ro != new_ro) {
     kprintf("CRC mismatch %#x vs %#x\n", crc_ro, new_ro);
     panic("Sanity checks: CRC of kernel read-only area failed");
   }
+#endif
 
   // verify that Elf symbols were not overwritten
   bool symbols_verified = Elf::verify_symbols();
