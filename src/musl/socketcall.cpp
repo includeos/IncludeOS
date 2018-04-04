1074c0ad834d8fe150c1582f5944d284901e950a
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2018-04-04 10:55:50 +0200

    musl: stubbed socket sendmsg

--- src/musl/socketcall.cpp
+++ src/musl/socketcall.cpp
@@ -1,3 +1,5 @@
+#include "common.hpp"
+#include "stub.hpp"
 #include <errno.h>
 
 extern "C"
@@ -15,3 +17,15 @@ long socketcall_shutdown()
 {
   return -ENOSYS;
 }
+
+static long sock_sendmsg(int /*sockfd*/, const struct msghdr */*msg*/, int /*flags*/)
+{
+  return -ENOSYS;
+}
+
+extern "C"
+long socketcall_sendmsg(int sockfd, const struct msghdr *msg, int flags)
+{
+  return stubtrace(sock_sendmsg, "sendmsg", sockfd, msg, flags);
+}
+
