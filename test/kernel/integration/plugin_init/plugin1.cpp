dacfac23c7235c8529f95c0b4b1b9a8e2ef5706b
Alfred Bratterud <alfred@includeos.org>
2018-04-10 18:46:04 +0200

    test: change plugin test to run directly in ctors

--- test/kernel/integration/plugin_init/plugin1.cpp
+++ test/kernel/integration/plugin_init/plugin1.cpp
@@ -1,16 +1,22 @@
 #include <os>
 
+extern bool example_plugin_registered;
+extern bool example_plugin_run;
 int my_plugin_functions = 0;
 int f1_data = 0;
 
-void func1(){
+__attribute__((constructor))
+static void func1(){
+
+
+  // NOTE: since this plugin is a part of the service, its ctors
+  // will be called after the installed plugin ctors. Therefore we can
+  // depend on installed plugins being run / registered.
+
+  CHECKSERT(example_plugin_registered, "Example plugin registered");
+  CHECKSERT(example_plugin_run, "Example plugin ran");
+
   INFO("Plugin 1","initialization function 1");
   f1_data = 0xf1;
   my_plugin_functions++;
 }
-
-struct Autoreg {
-  Autoreg() {
-    OS::register_plugin(func1, "Plugin 1");
-  }
-} autoregister;
