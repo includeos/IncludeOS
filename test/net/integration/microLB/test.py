752984f360e6076832f8f52de7d6a3956a285bd8
Alfred Bratterud <alfred@includeos.org>
2018-04-11 09:59:50 +0200

    test: make microLB always kill node.js and start after init

--- test/net/integration/microLB/test.py
+++ test/net/integration/microLB/test.py
@@ -4,6 +4,7 @@ import signal
 import sys
 import subprocess
 import thread
+import atexit
 
 includeos_src = os.environ.get('INCLUDEOS_SRC',
                                os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))).split('/test')[0])
@@ -21,6 +22,7 @@ def validateRequest(expected = ""):
 pro = subprocess.Popen(["node", "server.js"], stdout=subprocess.PIPE)
 
 def startBenchmark(line):
+    print "<test.py> starting test "
     assert validateRequest("6001")
     assert validateRequest("6002")
     assert validateRequest("6003")
@@ -30,16 +32,21 @@ def startBenchmark(line):
     assert validateRequest("6002")
     assert validateRequest("6003")
     assert validateRequest("6004")
-    # stop nodeJS
-    pro.kill()
     vm._on_success("SUCCESS")
     return True
+
+@atexit.register
+def cleanup():
+    print "<test.py> Stopping node server"
+    # stop nodeJS
+    pro.kill()
+
 
 # Get an auto-created VM from the vmrunner
 vm = vmrunner.vms[0]
 
 # Add custom event for testing server
-vm.on_output("Running [ Micro Load Balancer ]", startBenchmark)
+vm.on_output("MicroLB ready for test", startBenchmark)
 
 # Boot the VM, taking a timeout as parameter
 vm.cmake().boot(20).clean()
