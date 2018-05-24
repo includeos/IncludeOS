13f68ec63b49b01ebcce09053d14b25b76dffba3
Alf-André Walla <fwsgonzo@hotmail.com>
2018-05-24 15:51:35 +0200

    test: GRUB test now uses xorriso/grub-mkrescue

---
+++ test/kernel/integration/grub/grubiso.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LOCAL_DISK=temp_disk
+SERVICE=$1
+GRUBIMG=build/grub.iso
+set -e
+
+echo "Building $GRUBIMG..."
+# create grub.iso
+mkdir -p $LOCAL_DISK/boot/grub
+cp $SERVICE $LOCAL_DISK/boot/service
+cp grub.cfg $LOCAL_DISK/boot/grub
+grub-mkrescue -d /usr/lib/grub/i386-pc -o $GRUBIMG $LOCAL_DISK
