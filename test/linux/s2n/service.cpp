00a42a942439d499dd23fbd8815a49759a3cd86b
Alf-André Walla <fwsgonzo@hotmail.com>
2018-10-17 17:24:57 +0200

    linux: Update S2N test service

--- test/linux/s2n/service.cpp
+++ test/linux/s2n/service.cpp
@@ -18,6 +18,7 @@
 #include <service>
 #include <memdisk>
 #include <net/inet>
+#include <net/interfaces>
 #include <https>
 static http::Server* server = nullptr;
 extern http::Response_ptr handle_request(const http::Request&);
@@ -27,7 +28,7 @@ void Service::start()
   extern void create_network_device(int N, const char* route, const char* ip);
   create_network_device(0, "10.0.0.0/24", "10.0.0.1");
 
-  auto& inet = net::Super_stack::get(0);
+  auto& inet = net::Interfaces::get(0);
   inet.network_config(
       {  10, 0,  0, 42 },  // IP
       { 255,255,255, 0 },  // Netmask
@@ -38,7 +39,7 @@ void Service::start()
   [] (fs::error_t err, fs::File_system&) {
     assert(!err);
   });
-  
+
   auto& filesys = fs::memdisk().fs();
   auto ca_cert = filesys.read_file("/test.pem");
   assert(ca_cert.is_valid());
@@ -47,17 +48,17 @@ void Service::start()
   auto srv_key = filesys.read_file("/server.key");
   assert(srv_key.is_valid());
   printf("Loaded certificates and keys\n");
-  
+
   server = new http::S2N_server(
           ca_cert.to_string(), ca_key.to_string(), inet.tcp());
-  
+
   server->on_request(
     [] (http::Request_ptr request,
         http::Response_writer_ptr response_writer) {
       response_writer->set_response(handle_request(*request));
       response_writer->write();
     });
-  
+
   server->listen(443);
   printf("Using S2N for HTTPS transport\n");
 }
