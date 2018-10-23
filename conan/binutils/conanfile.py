8aa00943c9968325fd6d9e43b3a91c8355ec40c5
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2018-10-23 22:03:11 +0200

    build: conanfiles

---
+++ conan/binutils/conanfile.py
@@ -0,0 +1,33 @@
+#binutils recepie first take!!
+#todo figure out to get a build directory ?
+#todo use shutil to move versioned to unversioned ?
+
+import os
+from conans import ConanFile,tools,AutoToolsBuildEnvironment
+
+class BinutilsConan(ConanFile):
+    settings= "compiler","arch","build_type","os"
+    name = "binutils"
+    version = "2.31"
+    url = "TODO: https://ftp.gnu.org/gnu/binutils"
+    def source(self):
+        zip_name="binutils-%s.tar.gz"%self.version
+        tools.download("https://ftp.gnu.org/gnu/binutils/%s" % zip_name,zip_name)
+        tools.unzip(zip_name)
+        os.unlink(zip_name)
+
+    def build(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        env_build.configure(configure_dir="binutils-%s"%self.version,target=str(self.settings.arch)+"-elf",args=["--disable-nls","--disable-werror"]) #what goes in here preferably
+        env_build.make()
+        env_build.install()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+#        self.copy("*hello.lib", dst="lib", keep_path=False)
+#        self.copy("*.dll", dst="bin", keep_path=False)
+#        self.copy("*.so", dst="lib", keep_path=False)
+#        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+    def package_info(self):
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
