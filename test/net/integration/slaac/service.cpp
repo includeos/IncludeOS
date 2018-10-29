44cb1895f536e4e864a5cd5bf36834b547a5ece9
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2018-10-29 10:33:10 +0100

    ip6: Fixed stuff not building

--- test/net/integration/slaac/service.cpp
+++ test/net/integration/slaac/service.cpp
@@ -18,19 +18,19 @@
 //#define DEBUG // Debug supression
 
 #include <os>
-#include <list>
-#include <net/inet>
+#include <net/interfaces>
 
 using namespace net;
 
 void Service::start(const std::string&)
 {
-  net::Inet::ifconfig6([](bool completed) {
+  static auto& inet = net::Interfaces::get(0);
+  inet.autoconf_v6(1, [](bool completed) {
     if (!completed) {
        panic("Auto-configuration of IP address failed");
     }
     INFO("Slaac test", "Got IP from Auto-configuration");
-    printf("%s\n", net::Inet::stack<0>().ip6_addr().str().c_str());
+    printf("%s\n", inet.ip6_addr().str().c_str());
   });
   INFO("Slaac test", "Waiting for Auto-configuration\n");
 }
