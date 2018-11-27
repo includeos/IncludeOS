c6179d83bdf855a506ff206a3718df19035eb8cf
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2018-11-27 12:49:43 +0100

    conan: NaCL full package

--- conan/nacl/conanfile.py
+++ conan/nacl/conanfile.py
@@ -2,9 +2,8 @@
 from conans import ConanFile,tools
 
 class NaClConan(ConanFile):
-    #settings = ''
     name = 'nacl'
-    version="v0.1.0"
+    version="v0.2.0"
     license = 'Apache-2.0'
     description='NaCl is a configuration language for IncludeOS that you can use to add for example interfaces and firewall rules to your service.'
     url='https://github.com/includeos/NaCl'
@@ -15,12 +14,18 @@ class NaClConan(ConanFile):
     def build(self):
         #you need antlr4 installed to do this
         self.run("antlr4 -Dlanguage=Python2 NaCl.g4 -visitor")
+
     def package(self):
-        self.copy('NaCl.tokens',dst="NaCl",src=".")
-        self.copy('NaClLexer.py',dst="NaCl",src=".")
-        self.copy('NaClLexer.tokens',dst="NaCl",src=".")
-        self.copy('NaClListener.py',dst="NaCl",src=".")
-        self.copy('NaClParser.py',dst="NaCl",src=".")
-        self.copy('NaClVisitory.py',dst="NaCl",src=".")
+        name='NaCl'
+        self.copy('*',dst=name+"/subtranspilers",src="subtranspilers")
+        self.copy('*',dst=name+"/type_processors",src="type_processors")
+        self.copy('*.py',dst=name,src=".")
+        self.copy('cpp_template.mustache',dst=name,src='.')
+        self.copy('NaCl.tokens',dst=name,src=".")
+        self.copy('NaClLexer.tokens',dst=name,src=".")
+
+    def package_id(self):
+        self.info.header_only()
+        
     def deploy(self):
         self.copy("*",dst="NaCl",src="NaCl")
