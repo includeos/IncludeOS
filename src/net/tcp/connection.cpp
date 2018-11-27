565afc7386d891b21649f022e88393e852a6b797
Andreas Åkesson <andreas.jm.akesson@gmail.com>
2018-11-27 10:58:04 +0100

    tcp: Change RCV WND value from a static value to a dynamic value based on global mem avail

--- src/net/tcp/connection.cpp
+++ src/net/tcp/connection.cpp
@@ -36,6 +36,7 @@ Connection::Connection(TCP& host, Socket local, Socket remote, ConnectCallback c
     cb{host_.window_size()},
     read_request(nullptr),
     writeq(),
+    recv_wnd_getter{TCP::global_recv_wnd},
     on_connect_{std::move(callback)},
     on_disconnect_({this, &Connection::default_on_disconnect}),
     rtx_timer({this, &Connection::rtx_timeout}),
@@ -323,7 +324,9 @@ Packet_view_ptr Connection::create_outgoing_packet()
   // Set Destination (remote)
   packet->set_destination(remote_);
 
-  packet->set_win(std::min((cb.RCV.WND >> cb.RCV.wind_shift), (uint32_t)default_window_size));
+  const auto recv_wnd = recv_wnd_getter();
+  //printf("recv_wnd %u\n", recv_wnd);
+  packet->set_win(cb.RCV.WND >> cb.RCV.wind_shift);
 
   if(cb.SND.TS_OK)
     packet->add_tcp_option_aligned<Option::opt_ts_align>(host_.get_ts_value(), cb.get_ts_recent());
@@ -683,6 +686,15 @@ void Connection::recv_data(const Packet_view& in)
 {
   Expects(in.has_tcp_data());
 
+  // just drop the packet if we don't have a recv wnd.
+  // this is really awful and probably unnecesseary,
+  // since it could be that we already preallocated that memory in our vector.
+  // i also think we shouldn't reach this point due to State::check_seq checking
+  // if we're inside the window. if packet is out of order tho we can change the RCV wnd (i think).
+  if(recv_wnd_getter() == 0)
+    drop(in, Drop_reason::RCV_WND_ZERO);
+
+
   // Keep track if a packet is being sent during the async read callback
   const auto snd_nxt = cb.SND.NXT;
 
@@ -719,6 +731,8 @@ void Connection::recv_data(const Packet_view& in)
       const auto recv = read_request->insert(in.seq(), in.tcp_data(), length, in.isset(PSH));
       // this ensures that the data we ACK is actually put in our buffer.
       Ensures(recv == length);
+      // adjust the rcv wnd to (maybe) new value
+      cb.RCV.WND = recv_wnd_getter();
     }
   }
   // Packet out of order
