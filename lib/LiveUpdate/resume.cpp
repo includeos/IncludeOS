b2c68a7c0c8a67a3755f8ddab00ee073f4409cc3
fwsGonzo <fwsgonzo@hotmail.com>
2018-12-07 13:35:02 +0100

    liveupdate: Add Restore::is_stream()

--- lib/LiveUpdate/resume.cpp
+++ lib/LiveUpdate/resume.cpp
@@ -116,6 +116,10 @@ bool Restore::is_marker() const noexcept
 {
   return get_type() == TYPE_MARKER;
 }
+bool Restore::is_stream() const noexcept
+{
+  return get_type() == TYPE_STREAM;
+}
 
 int Restore::as_int() const
 {
