492d1bdbe6cba3e86d47c805ed8c9b364cda14ef
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2018-12-27 17:17:53 +0100

    test: added RAII paging setup to be able to run it standalone

--- test/kernel/unit/unit_liveupdate.cpp
+++ test/kernel/unit/unit_liveupdate.cpp
@@ -1,7 +1,16 @@
 #include <common.cxx>
 #include <liveupdate.hpp>
+#include <arch/x86/paging.hpp>
 #include <elf.h>
+#include <os>
 using namespace liu;
+
+// #define DEBUG_UNIT
+#ifdef DEBUG_UNIT
+#define MYINFO(X,...) INFO("<test os::mem>", X, ##__VA_ARGS__)
+#else
+#define MYINFO(X,...)
+#endif
 
 static buffer_t not_a_kernel;
 static void* storage_area = nullptr;
@@ -11,6 +20,35 @@ static struct {
   bool boolean        = false;
 } stored;
 
+extern void  __arch_init_paging();
+extern x86::paging::Pml4* __pml4;
+// Default page setup RAII
+class Default_paging {
+public:
+  ~Default_paging()
+  {
+    clear_paging();
+  }
+
+  Default_paging()
+  {
+    clear_paging();
+    MYINFO("Initializing default paging \n");
+    __arch_init_paging();
+  }
+
+
+  static void clear_paging() {
+    using namespace x86::paging;
+    MYINFO("Clearing default paging \n");
+    if (__pml4 != nullptr) {
+      __pml4->~Pml4();
+      free(__pml4);
+      __pml4 = nullptr;
+      OS::memory_map().clear();
+    }
+  }
+};
 static void store_something(Storage& store, const buffer_t*)
 {
   store.add_int(0, 1234);
@@ -26,6 +64,7 @@ static void restore_something(Restore& thing)
 
 CASE("Setup LiveUpdate and perform no-op update")
 {
+  Default_paging p{};
   storage_area = new char[16*1024*1024];
 
   not_a_kernel.resize(164);
@@ -48,6 +87,7 @@ CASE("Setup LiveUpdate and perform no-op update")
 
 CASE("Store some data and restore it")
 {
+  Default_paging p{};
   LiveUpdate::register_partition("test", store_something);
   EXPECT_THROWS_AS(LiveUpdate::exec(not_a_kernel, storage_area), liveupdate_exec_success);
 
