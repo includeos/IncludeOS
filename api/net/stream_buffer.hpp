b6b381da16ed3ecca24c9a7f2f3744cc0e60344e
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2019-01-15 16:25:31 +0100

    0.13.x: if reset callbacks is called in loop exit

--- api/net/stream_buffer.hpp
+++ api/net/stream_buffer.hpp
@@ -205,6 +205,7 @@ namespace net {
           // Pop each time, in case callback leads to another call here.
           m_send_buffers.pop_front();
           m_on_read(buf);
+          if (m_on_read == nullptr) { break; } //if calling m_on_read reset the callbacks exit
         }
       }
     }
