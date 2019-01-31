51a3c6084bceb7656e41e01f3d9b33c94fd9d414
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2019-01-31 20:47:28 +0100

    conan: fixed libgcc/libcompiler to be sanë́

--- conan/libgcc/conanfile.py
+++ conan/libgcc/conanfile.py
@@ -3,7 +3,7 @@ from six import StringIO
 from conans import ConanFile
 
 class LibgccConan(ConanFile):
-    settings= "compiler","arch","build_type","os"
+    settings= "arch","os"
     name = "libgcc"
     version = "1.0"
     license = 'GPL3'
@@ -13,10 +13,9 @@ class LibgccConan(ConanFile):
     def build(self):
         iobuf = StringIO()
         extra=''
-        if (str(self.settings.arch) =="x86"):
+        if (str(self.settings.arch) in ["x86","armv8_32"]):
             extra="-m32"
-        #gcc=str(self.settings.arch)+"-pc-linux-gnu-gcc"
-        self.run("gcc "+extra+" --print-libgcc-file-name", output=iobuf)
+        self.run("${CC} "+extra+" --print-libgcc-file-name", output=iobuf)
         src=iobuf.getvalue().rstrip('\n')
         print ("source "+src)
         #a bit nasty but it works
@@ -25,12 +24,9 @@ class LibgccConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs=['compiler']
         #which compiler is in use doesnt really matter
-        del self.settings.compiler
-        del self.settings.os
-        del self.settings.build_type
 
     def package(self):
         self.copy("*.a",dst="lib",src="./")
 
     def deploy(self):
-        self.copy("*.a",dst="lib",src="libgcc")
+        self.copy("*.a",dst="lib",src="lib")
