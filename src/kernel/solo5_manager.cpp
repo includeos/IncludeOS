0ed95c7b8403f2b0fc29fa22c3d966da68720305
Alf-André Walla <fwsgonzo@hotmail.com>
2019-02-07 15:22:35 +0100

    solo5: Update to new HAL api

--- src/kernel/solo5_manager.cpp
+++ src/kernel/solo5_manager.cpp
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <assert.h>
+#include <cassert>
 #include <common>
 #include <delegate>
 #include <vector>
