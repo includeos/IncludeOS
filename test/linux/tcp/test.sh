cc31e66bca796821e8e8acb36a560bd5c59acb01
fwsGonzo <fwsgonzo@hotmail.com>
2019-02-13 17:59:25 +0100

    userspace: Update run scripts, use PGO with TCP benchmark

--- test/linux/tcp/test.sh
+++ test/linux/tcp/test.sh
@@ -2,7 +2,7 @@
 set -e
 export CC=gcc-7
 export CXX=g++-7
-$INCLUDEOS_PREFIX/bin/lxp-run | grep 'Server received'
+$INCLUDEOS_PREFIX/bin/lxp-pgo | grep 'Server received'
 
 if [ $? == 0 ]; then
   echo ">>> Linux Userspace TCP test success!"
