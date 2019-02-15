30faf60259f71a8ae6fc96c2b742c55f33bbffcf
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2019-02-15 14:41:37 +0100

    conan: fixed s2n recipe to link against correct openssl lib

--- conan/s2n/conanfile.py
+++ conan/s2n/conanfile.py
@@ -10,21 +10,15 @@ class S2nConan(ConanFile):
     settings="os","compiler","build_type","arch"
     name = "s2n"
     version = "1.1.1" ##if we remove this line we can specify it from outside this script!! ps ps
-    options = {"threads":[True, False]}
-    #tag="OpenSSL_"+version.replace('.','_')
-    default_options = {"threads": False}
-    #options = {"shared":False}
-    #branch = "version"+version
+    options = {
+        "threads":[True, False]
+    }
+    default_options = {
+        "threads": False
+    }
     license = 'Apache 2.0'
     description = 's2n : an implementation of the TLS/SSL protocols'
     url = "https://www.openssl.org"
-    #exports_sources=['protobuf-options.cmake']
-    #keep_imports=True
-    #TODO handle build_requrements
-    #def build_requirements(self):
-        #self.build_requires("binutils/2.31@includeos/stable")
-        #self.build_requires("musl/v1.1.18@includeos/stable")
-        #self.build_requires("llvm/5.0@includeos/stable")## do we need this or just headers
     def configure(self):
         #TODO fix the FORTIFY_SOURCE ISSUE IN RELEASE
         del self.settings.build_type
@@ -37,7 +31,6 @@ class S2nConan(ConanFile):
     def requirements(self):
         self.requires("openssl/1.1.1@{}/{}".format(self.user,self.channel))
 
-
     def source(self):
         repo = tools.Git(folder="s2n")
         repo.clone("https://github.com/fwsGonzo/s2n.git")
@@ -45,7 +38,8 @@ class S2nConan(ConanFile):
     def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["NO_STACK_PROTECTOR"]='ON'
-        cmake.definitions["S2N_UNSAFE_FUZZING_MODE"]=''
+        cmake.definitions["S2N_UNSAFE_FUZZING_MODE"]=False
+        cmake.definitions["CMAKE_PREFIX_PATH"]=self.deps_cpp_info["openssl"].rootpath
         cmake.configure(source_folder="s2n")
         return cmake
 
@@ -56,6 +50,9 @@ class S2nConan(ConanFile):
     def package(self):
         cmake=self._configure_cmake()
         cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs=['s2n']
 
     def deploy(self):
         self.copy("*.a",dst="lib",src="lib")
