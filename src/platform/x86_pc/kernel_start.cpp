3eb845e5ea120070c3593a260d24cc1f39c98f94
Alf-André Walla <fwsgonzo@hotmail.com>
2019-02-28 13:17:19 +0100

    x86: Initialize libc in own module

--- src/platform/x86_pc/kernel_start.cpp
+++ src/platform/x86_pc/kernel_start.cpp
@@ -19,17 +19,9 @@
 #include <boot/multiboot.h>
 #include <kprint>
 #include <debug>
-#include <util/elf_binary.hpp>
-#include <arch/x86/cpu.hpp>
-#include <kernel/auxvec.h>
-#include <kernel/cpuid.hpp>
-#include <kernel/service.hpp>
 
-#include <version.h>
 #include "idt.hpp"
-
-#undef Expects
-#define Expects(X) if (!(X)) { kprint("Expect failed: " #X "\n");  asm("cli;hlt"); }
+#include "init_libc.hpp"
 
 //#define KERN_DEBUG 1
 #ifdef KERN_DEBUG
@@ -45,22 +37,11 @@ extern "C" {
   uintptr_t _move_symbols(uintptr_t loc);
   void _init_elf_parser();
   void _init_syscalls();
+  void __elf_validate_section(const void*);
 }
 
 uintptr_t _multiboot_free_begin(uintptr_t bootinfo);
 uintptr_t _multiboot_memory_end(uintptr_t bootinfo);
-
-extern char _ELF_START_;
-extern char _ELF_END_;
-extern char _INIT_START_;
-extern char _FINI_START_;
-
-thread_local int __tl1__ = 42;
-
-uint32_t __grub_magic = 0xc001;
-uint32_t __grub_addr  = 0x7001;
-
-static volatile int __global_ctors_ok = 0;
 
 __attribute__((no_sanitize("all")))
 void _init_bss()
@@ -69,59 +50,17 @@ void _init_bss()
   __builtin_memset(&_BSS_START_, 0, &_BSS_END_ - &_BSS_START_);
 }
 
-__attribute__((constructor))
-static void global_ctor_test(){
-  __global_ctors_ok = 42;
-}
-
-
 static os::Machine* __machine = nullptr;
 os::Machine& os::machine() noexcept {
-  Expects(__machine != nullptr);
+  LL_ASSERT(__machine != nullptr);
   return *__machine;
 }
 
-int kernel_main(int, char * *, char * *) {
-  PRATTLE("<kernel_main> libc initialization complete \n");
-  Expects(__global_ctors_ok == 42);
-  kernel::state().libc_initialized = true;
-
-  Expects(__tl1__ == 42);
-  Elf_binary<Elf64> elf{{(char*)&_ELF_START_, &_ELF_END_ - &_ELF_START_}};
-  Expects(elf.is_ELF() && "ELF header intact");
-
-  PRATTLE("<kernel_main> OS start \n");
-
-  // Initialize early OS, platform and devices
-  kernel::start(__grub_magic, __grub_addr);
-
-  // verify certain read-only sections in memory
-  // NOTE: because of page protection we can choose to stop checking here
-  kernel_sanity_checks();
-
-  PRATTLE("<kernel_main> post start \n");
-  // Initialize common subsystems and call Service::start
-  kernel::post_start();
-
-  // Starting event loop from here allows us to profile OS::start
-  os::event_loop();
-  return 0;
-}
-
-// Musl entry
-extern "C"
-int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv);
-
-extern "C" uintptr_t __syscall_entry();
-extern "C" void __elf_validate_section(const void*);
-
+// x86 kernel start
 extern "C"
 __attribute__((no_sanitize("all")))
 void kernel_start(uint32_t magic, uint32_t addr)
 {
-  __grub_magic = magic;
-  __grub_addr  = addr;
-
   PRATTLE("\n//////////////////  IncludeOS kernel start ////////////////// \n");
   PRATTLE("* Booted with magic 0x%x, grub @ 0x%x \n",
           magic, addr);
@@ -176,100 +115,5 @@ void kernel_start(uint32_t magic, uint32_t addr)
   PRATTLE("* Init CPU exceptions\n");
   x86::idt_initialize_for_cpu(0);
 
-  PRATTLE("* Thread local1: %i\n", __tl1__);
-
-  PRATTLE("* Elf start: %p\n", &_ELF_START_);
-  auto* ehdr = (Elf64_Ehdr*)&_ELF_START_;
-  auto* phdr = (Elf64_Phdr*)((char*)ehdr + ehdr->e_phoff);
-  Expects(phdr);
-  Elf_binary<Elf64> elf{{(char*)&_ELF_START_, &_ELF_END_ - &_ELF_START_}};
-  Expects(elf.is_ELF());
-  Expects(phdr[0].p_type == PT_LOAD);
-
-#ifdef KERN_DEBUG
-  PRATTLE("* Elf ident: %s, program headers: %p\n", ehdr->e_ident, ehdr);
-  size_t size =  &_ELF_END_ - &_ELF_START_;
-  PRATTLE("\tElf size: %zu \n", size);
-  for (int i = 0; i < ehdr->e_phnum; i++)
-  {
-    PRATTLE("\tPhdr %i @ %p, va_addr: 0x%lx \n", i, &phdr[i], phdr[i].p_vaddr);
-  }
-#endif
-
-  // Build AUX-vector for C-runtime
-  std::array<auxv_t, 38> aux;
-  PRATTLE("* Initializing aux-vector @ %p\n", aux);
-
-  int i = 0;
-  aux[i++].set_long(AT_PAGESZ, 4096);
-  aux[i++].set_long(AT_CLKTCK, 100);
-
-  // ELF related
-  aux[i++].set_long(AT_PHENT, ehdr->e_phentsize);
-  aux[i++].set_ptr(AT_PHDR, ((uint8_t*)ehdr) + ehdr->e_phoff);
-  aux[i++].set_long(AT_PHNUM, ehdr->e_phnum);
-
-  // Misc
-  aux[i++].set_ptr(AT_BASE, nullptr);
-  aux[i++].set_long(AT_FLAGS, 0x0);
-  aux[i++].set_ptr(AT_ENTRY, (void*) &kernel_main);
-  aux[i++].set_long(AT_HWCAP, 0);
-  aux[i++].set_long(AT_UID, 0);
-  aux[i++].set_long(AT_EUID, 0);
-  aux[i++].set_long(AT_GID, 0);
-  aux[i++].set_long(AT_EGID, 0);
-  aux[i++].set_long(AT_SECURE, 1);
-
-  const char* plat = "x86_64";
-  aux[i++].set_ptr(AT_PLATFORM, plat);
-
-  if (STACK_PROTECTOR_VALUE == 0) {
-    const unsigned long canary = __arch_rand32() | ((uint64_t) __arch_rand32() << 32);
-    aux[i++].set_long(AT_RANDOM, canary);
-    kprintf("* Stack protector value (random): %#lx\n", canary);
-  }
-  else {
-    aux[i++].set_long(AT_RANDOM, STACK_PROTECTOR_VALUE);
-    kprintf("* Stack protector value (fixed): %#lx\n", STACK_PROTECTOR_VALUE);
-  }
-
-  const size_t canary_slot = i-1;
-  aux[i++].set_ptr(AT_RANDOM, 0);
-  const size_t entropy_slot = i-1;
-  aux[i++].set_long(AT_NULL, 0);
-
-  std::array<char*, 6 + 38> argv;
-
-  // Parameters to main
-  argv[0] = (char*) Service::name();
-  argv[1] = 0x0;
-  int argc = 1;
-
-  // Env vars
-  argv[2] = strdup("LC_CTYPE=C");
-  argv[3] = strdup("LC_ALL=C");
-  argv[4] = strdup("USER=root");
-  argv[5] = 0x0;
-
-  memcpy(&argv[6], aux.data(), sizeof(aux));
-
-  auxv_t* auxp = (auxv_t*) &argv[6];
-  void* canary_addr = &auxp[canary_slot].a_un.a_val;
-  auxp[entropy_slot].set_ptr(AT_RANDOM, canary_addr);
-
-#if defined(__x86_64__)
-  PRATTLE("* Initialize syscall MSR (64-bit)\n");
-  uint64_t star_kernel_cs = 8ull << 32;
-  uint64_t star_user_cs   = 8ull << 48;
-  uint64_t star = star_kernel_cs | star_user_cs;
-  x86::CPU::write_msr(IA32_STAR, star);
-  x86::CPU::write_msr(IA32_LSTAR, (uintptr_t)&__syscall_entry);
-#elif defined(__i386__)
-  PRATTLE("Initialize syscall intr (32-bit)\n");
-  #warning Classical syscall interface missing for 32-bit
-#endif
-
-  // GDB_ENTRY;
-  PRATTLE("* Starting libc initialization\n");
-  __libc_start_main(kernel_main, argc, argv.data());
+  x86::init_libc(magic, addr);
 }
