65db6cef1f00f8e37fca6eb67c821710b07bd990
Kristian Jerpetjoen <kristian.jerpetjoen@gmail.com>
2019-03-22 21:52:11 +0100

    test: improved integration test build time by running conan install only once this also enables us to do conan install and use virtualenv
    test: Fixed issue where the activate.sh is no longer in the test folder

--- test/integration/run_test.sh
+++ test/integration/run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 timeout=$1
-shift
+test_script=$2
+shift 2
 arguments=$@
 source activate.sh
 #sudo prior to timeout. in case its needed inside
 sudo echo "sudo trigger"
-
-timeout -s 2 $timeout python2 -u test.py $arguments || echo "Test timed out"; sleep 1
+timeout -s 2 $timeout python2 -u ${test_script} $arguments || echo "Test timed out"; sleep 1
 source deactivate.sh
