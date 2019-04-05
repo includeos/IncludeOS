352dc077047890a1c384a9c5082b9aaa400794e9
Hans klabbers <hans.klabbers@rabobank.nl>
2019-04-05 12:15:31 +0200

    Moved Vagrantfile to etc and installed additional packages.

--- etc/install_on_vagrant.sh
+++ etc/install_on_vagrant.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
-
+export PATH="$HOME/.local/bin:$PATH"
 sudo locale-gen "en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 sudo tee -a /etc/environment > /dev/null << EOT
 LC_ALL="en_US.UTF-8"
 EOT
-export INCLUDEOS_PREFIX=/usr/local
-export PATH=$PATH:$INCLUDEOS_PREFIX/includeos/bin
-export CC="/usr/bin/clang-6.0"
-export CXX="/usr/bin/clang++-6.0"
-sudo tee -a /etc/environment > /dev/null << EOT
-INCLUDEOS_PREFIX=/usr/local
-CC="/usr/bin/clang-6.0"
-CXX="/usr/bin/clang++-6.0"
-EOT
-sudo tee -a /etc/profile > /dev/null << EOT
+#Install clang, nasm, cmake, qemu and bridge-utils
+sudo apt install -y clang nasm cmake qemu bridge-utils
 
-PATH=$PATH:$INCLUDEOS_PREFIX/includeos/bin
-EOT
-
-#Install pip and conan using pip
+#Install pip, conan and python dependencies
 sudo apt install -y python3-pip
-pip3 install conan
+pip3 install conan pystache antlr4-python3-runtime jsonschema psutil filemagic
 
 #Install conan profiles and remotes
 conan config install https://github.com/includeos/conan_config.git
-conan profile list
-conan profile show clang-6.0-linux-x86_64
-conan remote list
+export CONAN_DEFAULT_PROFILE_PATH="$HOME/.conan/profiles/clang-6.0-linux-x86_64"
+sudo tee -a /etc/environment > /dev/null << EOT
+CONAN_DEFAULT_PROFILE_PATH="$HOME/.conan/profiles/clang-6.0-linux-x86_64"
+EOT
 
-#Install python dependencies
-pip3 install pystache antlr4-python3-runtime
-pip3 install jsonschema psutil filemagic
+cd /IncludeOS
+mkdir -p build
+git checkout -b dev origin/dev
+VERSION=`conan inspect -a version .. | cut -d " " -f 2`
+conan editable add . includeos/$VERSION@includeos/latest --layout=etc/layout.txt
+conan install -if build . 
+conan build -bf build .
 
-#Build includeos
-cd ../..
-conan create IncludeOS includeos/test -pr clang-6.0-linux-x86_64
