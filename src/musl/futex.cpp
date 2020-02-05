18992c99ad59f71be69a67dfa8c280ba2e2b6a72
Magnus Skjegstad <magnus@skjegstad.com>
2020-02-05 09:55:43 +0100

    Yield on FUTEX_WAKE
    Previously we ignored FUTEX_WAKE, which is probably fine as the
    FUTEX_WAIT loop detects the change. This only adds a yield to allow the
    waiting thread to run.

--- src/musl/futex.cpp
+++ src/musl/futex.cpp
@@ -16,14 +16,18 @@
 #define FUTEX_CLOCK_REALTIME 256
 
 static int sys_futex(int *uaddr, int futex_op, int val,
-                      const struct timespec *timeout, int /*val3*/)
+                      const struct timespec* /* timeout */, int /*val3*/)
 {
-  if ((futex_op & 0xF) == FUTEX_WAIT)
-  {
-	if (*uaddr != val) return -EAGAIN;
-	// we have to yield here because of cooperative threads
-	// TODO: potential for sleeping here
-    while (*uaddr == val) __thread_yield();
+  switch (futex_op & 0xF) {
+	  case FUTEX_WAIT: if (*uaddr != val) return -EAGAIN;
+			    // we have to yield here because of cooperative threads
+			    // TODO: potential for sleeping here
+			    while (*uaddr == val) __thread_yield();
+			    break;
+	  case FUTEX_WAKE: // We can't wake up anything specific yet, so just yield
+			    __thread_yield();
+	  default:
+			    break;
   }
   return 0;
 }
