f0dcc4342832eb9ce4f5ce9288bc1a37a1618b14
Alfred Bratterud <alfred.bratterud@gmail.com>
2024-04-17 08:48:37 +0200

    Build s2n with conan2

--- deps/s2n/conanfile.py
+++ deps/s2n/conanfile.py
@@ -1,10 +1,15 @@
+import os
 import shutil
+
+from conan import ConanFile,tools
+from conan.tools.scm import Git
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
+from conan.tools.files import copy
+
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
-
-from conans import ConanFile,CMake,tools
 
 class S2nConan(ConanFile):
     settings="os","compiler","build_type","arch"
@@ -21,43 +26,46 @@ class S2nConan(ConanFile):
     description = 's2n : an implementation of the TLS/SSL protocols'
     url = "https://www.openssl.org"
 
-    @property
-    def default_channel(self):
-        return "stable"
-
-    def configure(self):
-        #TODO fix the FORTIFY_SOURCE ISSUE IN RELEASE
-        del self.settings.build_type
-
     def imports(self):
         self.copy("*",dst="target",src=".")
 
     def requirements(self):
-        self.requires("openssl/1.1.1@{}/{}".format(self.user,self.channel))
+        self.requires("openssl/1.1.1".format(self.user,self.channel))
 
     def source(self):
-        repo = tools.Git(folder="s2n")
-        repo.clone("https://github.com/fwsGonzo/s2n.git", branch="0.8")
+        repo = Git(self)
+        args = ["--branch", self.version]
+        repo.clone("https://github.com/fwsGonzo/s2n.git", target="s2n", args=args)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["NO_STACK_PROTECTOR"]='ON'
-        cmake.definitions["S2N_UNSAFE_FUZZING_MODE"]=False
-        cmake.definitions["CMAKE_PREFIX_PATH"]=self.deps_cpp_info["openssl"].rootpath
-        cmake.configure(source_folder="s2n")
-        return cmake
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()        
+        tc = CMakeToolchain(self)
+        tc.variables["NO_STACK_PROTECTOR"]='ON'
+        tc.variables["S2N_UNSAFE_FUZZING_MODE"]=False
+        tc.variables["BUILD_TESTING"]='OFF'
+        tc.variables["CMAKE_C_FLAGS"]="-Wno-error=strict-prototypes -fno-sanitize=undefined"
+        
+        print("Openssl:\n{} ".format(self.dependencies["openssl"].cpp_info.serialize()))
 
+        # This property doesn't exist in conan2
+        #tc.variables["CMAKE_PREFIX_PATH"]=self.dependencies["openssl"].cpp_info.rootpath
+        
+        tc.generate()
+        
     def build(self):
-        cmake=self._configure_cmake()
-        cmake.build()
+        cmake = CMake(self)        
+        cmake.configure(build_script_folder=self.source_folder+"/s2n")
+        
+        # Note: The tests won't build due to a linker error, missing symbols for
+        #       some of the ubsan sanitizers. We shouldn't need the tests though, so
+        #       building s2n specifically instad of all should be enough.        
+        cmake.build(target="s2n")
 
     def package(self):
-        cmake=self._configure_cmake()
-        cmake.install()
+        cmake = CMake(self)
+        cmake.install()        
 
     def package_info(self):
-        self.cpp_info.libs=['s2n']
+        self.cpp_info.libs=['s2n'] 
 
-    def deploy(self):
-        self.copy("*.a",dst="lib",src="lib")
-        self.copy("*.h",dst="include",src="include")
