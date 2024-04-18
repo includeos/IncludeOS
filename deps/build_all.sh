2b1f1faec8aead4c769f5f2f730fc45e0c5becba
Alfred Bratterud <alfred.bratterud@gmail.com>
2024-04-18 06:15:48 +0200

    Build standard libs in order first

--- deps/build_all.sh
+++ deps/build_all.sh
@@ -2,17 +2,47 @@
 
 echo "Creating all conan packages:"
 
+# These must be built in order
+PLATFORM=(musl
+	  llvm_source
+	  libunwind
+	  libcxxabi
+	  libcxx
+	 )
 
-DIRS=`ls`
-echo $DIRS
-echo 
+dry_run=false
+if [[ "$1" == "--dry" ]]
+then
+    dry_run=true
+    echo "Dry run"
+fi
 
-for DIR in $DIRS
-do    
+echo "Building standard libs"
+for DIR in "${PLATFORM[@]}"
+do
     if [ -d "$DIR" ]; then
 	pushd $DIR
 	echo "Creating package $DIR"
-	conan create .
+	if [ "$dry_run" = false ]; then
+	    conan create .
+	fi
+	popd
+    fi
+done
+
+echo "Building everything else"
+echo
+
+ALL=(*)
+
+for DIR in "${ALL[@]}"
+do    
+    if [[ -d "$DIR" && ! "${PLATFORM[*]}" =~ "$DIR" ]]; then
+	pushd $DIR
+	echo "Creating package $DIR"
+	if [ "$dry_run" = false ]; then
+	    conan create .
+	fi
 	popd
     fi
 done
