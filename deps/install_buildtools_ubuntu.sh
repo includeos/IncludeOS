750ff460e17d0600086db8ea5b91d915050d3b65
Alfred Bratterud <alfred.bratterud@gmail.com>
2024-04-19 06:45:27 +0200

    detect default conan profile and make -j8

--- deps/install_buildtools_ubuntu.sh
+++ deps/install_buildtools_ubuntu.sh
@@ -3,5 +3,9 @@ sudo apt install -y emacs cmake python3-pip clang-18 libc++-18-dev libc++abi-18-
 
 echo "Installing conan"
 pip install conan
+CONAN="$HOME/.local/bin/conan"
+
+# Conan requires a host profile and will use the default
+$CONAN profile detect
 
 echo "NOTE: You might want to export the conan binary path to PATH"
