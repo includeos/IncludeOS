ae12a0a4badf278bd01644c432ca2d0c62102274
Alfred Bratterud <alfred.bratterud@gmail.com>
2024-05-02 06:45:06 +0000

    Add url and sha256 for nixpkgs

--- deps/musl/default.nix
+++ deps/musl/default.nix
@@ -1,5 +1,8 @@
 { nixpkgs ?
-  builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz"
+  builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz";
+    sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
+  }
 , pkgs ? import nixpkgs { config = {}; overlays = []; }
 }:
 with pkgs;
