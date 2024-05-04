52d2b14e18e99fc4e4f2f916622dacf503e16b7d
Magnus Skjegstad <magnus@skjegstad.com>
2024-05-05 01:26:56 +0200

    nix: Add http-parser default.nix
    - Add default.nix to deps/http-parser
    - Use recent version of packages to get static build support

---
+++ deps/http-parser/default.nix
@@ -0,0 +1,23 @@
+{
+  stdenv
+}:
+let
+  # Add needed $out/include/http-parser directory to match IncludeOS' use of
+  # "#include <http-parser/http_parser.h>".
+  # TODO: Upstream doesn't use that subdir though, so better fix IncludeOS
+  # sources.
+  #
+  # Uses a more recent version of nixpkgs to get support for static builds
+  nixpkgsHttpfix = builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/33f464b661f939689aa56af6b6e27b504c5afb93.tar.gz";
+      sha256 = "15bdlccjg14qa7lwkcc7pikvi386ig108ca62hbxfas5wyw1fr62";
+  };
+  pkgsHttpfix = import nixpkgsHttpfix { };
+in
+  pkgsHttpfix.pkgsStatic.http-parser.overrideAttrs (oldAttrs: {
+    inherit stdenv;
+    postInstall = (oldAttrs.postInstall or "") + ''
+      mkdir "$out/include/http-parser"
+      ln -sr "$out/include/http_parser.h" "$out/include/http-parser"
+    '';
+  })
