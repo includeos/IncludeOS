906a3c76903bac0ee596be2df97a5f39f7b2f5e7
Bjørn Forsman <bjorn.forsman@gmail.com>
2024-05-11 22:37:46 +0200

    nix: create a new scope for IncludeOS packages
    Instead of manually inheriting stdenv all over, introduce a nixpkgs
    overlay and a new package set/scope called pkgsIncludeOS, where stdenv
    is set once and automatically used by callPackage.
    
    I created the overlay in a separate file (overlay.nix) in case we want
    to re-use it for other `import nixpkgs { ... }` calls.
    
    Since the Nix files currently become part of the input to the IncludeOS
    build, this creates a new derivation (build), but the output content is
    the same as before (checked with diffoscope).

---
+++ overlay.nix
@@ -0,0 +1,89 @@
+final: prev: {
+  pkgsIncludeOS = prev.lib.makeScope prev.newScope (self: {
+    # self.callPackage will use this stdenv.
+    stdenv = prev.llvmPackages_16.libcxxStdenv;
+
+    # Deps
+    musl-includeos = self.callPackage ./deps/musl/default.nix { };
+    uzlib = self.callPackage ./deps/uzlib/default.nix { };
+    botan2 = self.callPackage ./deps/botan/default.nix { };
+    microsoft_gsl = self.callPackage ./deps/GSL/default.nix { };
+    s2n-tls = self.callPackage ./deps/s2n/default.nix { };
+    http-parser = self.callPackage ./deps/http-parser/default.nix { };
+    # prev.cmake fails on unknown argument --disable-shared. Override configurePhase to not use the flag.
+    cmake = prev.cmake.overrideAttrs (oldAttrs: {
+      inherit (self) stdenv;
+      useSharedLibraries = false;
+      isMinimalBuild = true;
+      # Override configure phase, otherwise it will fail on unsupported flags,.
+      # Add some manual flags taken from cmake.nix.
+      configurePhase = ''
+        ./configure --prefix=$out --parallel=''${NIX_BUILD_CORES:-1} CC=$CC_FOR_BUILD CXX=$CXX_FOR_BUILD CXXFLAGS=-Wno-elaborated-enum-base --no-system-libs
+      '';
+    });
+
+    # IncludeOS
+    includeos = self.stdenv.mkDerivation rec {
+      pname = "includeos";
+
+      version = "dev";
+
+      src = prev.lib.cleanSource ./.;
+
+      # If you need to patch, this is the place
+      postPatch = '''';
+
+      nativeBuildInputs = [
+        self.cmake
+        prev.nasm
+      ];
+
+      buildInputs = [
+
+        # TODO:
+        # including musl here makes the compiler pick up musl's libc headers
+        # before libc++'s libc headers, which doesn't work. See e.g. <cstdint>
+        # line 149;
+        # error <cstdint> tried including <stdint.h> but didn't find libc++'s <stdint.h> header.
+        # #ifndef _LIBCPP_STDINT_H
+        #   error <cstdint> tried including <stdint.h> but didn't find libc++'s <stdint.h> header. \
+        #   This usually means that your header search paths are not configured properly. \
+        #   The header search paths should contain the C++ Standard Library headers before \
+        #   any C Standard Library, and you are probably using compiler flags that make that \
+        #   not be the case.
+        # #endif
+        #
+        # With this commented out IncludeOS builds with Nix libc++, which is great,
+        # but might bite us later because it then uses a libc we didn't patch.
+        # Best case we case we can adapt our own musl to be identical, use nix static
+        # musl for compilation, and includeos-musl only for linking.
+        #
+        # musl-includeos  👈 this has to come in after libc++ headers.
+        self.botan2
+        self.http-parser
+        self.microsoft_gsl
+        prev.openssl
+        prev.rapidjson
+        #self.s2n-tls          👈 This is postponed until we can fix the s2n build.
+        self.uzlib
+      ];
+
+      # Add some pasthroughs, for easily building the depdencies (for debugging):
+      # $ nix-build -A NAME
+      passthru = {
+        inherit (self) uzlib;
+        inherit (self) http-parser;
+        inherit (self) botan2;
+        #inherit (self) s2n-tls;
+        inherit (self) musl-includeos;
+        inherit (self) cmake;
+      };
+
+      meta = {
+        description = "Run your application with zero overhead";
+        homepage = "https://www.includeos.org/";
+        license = prev.lib.licenses.asl20;
+      };
+    };
+  });
+}
