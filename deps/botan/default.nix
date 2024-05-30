65117c49c159386888282cbf2edbb35da5cc1fa4
Magnus Skjegstad <magnus@skjegstad.com>
2024-05-30 12:18:52 +0000

    nix: support 32bit, add -j to make

--- deps/botan/default.nix
+++ deps/botan/default.nix
@@ -1,21 +1,25 @@
 {
   pkgs
 }:
+let
+  cpuFlag = if pkgs.stdenv.system == "i686-linux" then "x86_32" else "x86_64";
+in
 pkgs.botan2.overrideAttrs (oldAttrs: {
+
   postInstall = (oldAttrs.postInstall or "") + ''
     ln -sr "$out/include/botan-2/botan" "$out/include"
   '';
 
   configurePhase = ''
     runHook preConfigure
-    python3 configure.py --prefix=$out --with-bzip2 --with-zlib --build-targets=static
+    python3 configure.py --prefix=$out --with-bzip2 --with-zlib --build-targets=static --cpu=${cpuFlag} --os=linux
     runHook postConfigure
   '';
 
   buildPhase = ''
     runHook preBuild
 
-    make
+    make -j $NIX_BUILD_CORES
 
     runHook postBuild
   '';
