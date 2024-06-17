954fad5005524ed1fa9b23458032fb1fa669e3a4
Magnus Skjegstad <magnus@skjegstad.com>
2024-06-17 15:15:51 +0200

    musl 1.1.24: update nix files

--- deps/musl-unpatched/default.nix
+++ deps/musl-unpatched/default.nix
@@ -1,19 +1,15 @@
-{ nixpkgs ?
-  builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz";
-    sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
-  }
-, stdenv
-, pkgs ? import nixpkgs { config = {}; overlays = []; crossSystem = { config = stdenv.targetPlatform.config; }; }
+{
+stdenv
+, pkgs
 , linuxHeaders ? null
 }:
 stdenv.mkDerivation rec {
   pname = "musl-unpatched";
-  version = "1.1.18";
+  version = "1.1.24";
 
   src = fetchGit {
     url = "git://git.musl-libc.org/musl";
-    rev = "eb03bde2f24582874cb72b56c7811bf51da0c817";
+    rev = "ea9525c8bcf6170df59364c4bcd616de1acf8703";
   };
 
   enableParallelBuilding = true;
