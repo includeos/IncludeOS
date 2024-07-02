6f565ca12e673848d99c23716cb89ed51a77912f
Magnus Skjegstad <magnus@skjegstad.com>
2024-07-02 11:11:09 +0200

    example: shut down service after last printf

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -3,5 +3,7 @@
 
 void Service::start(const std::string& args){
   printf("Args = %s\n", args.c_str());
-  printf("Try giving the service less memory, eg. 5MB in vm.json\n");
+  printf("Try giving the service less memory, eg. 10MB in vm.json\n");
+  printf("Service done. Shutting down...\n");
+  os::shutdown();
 }
