7723f034e143a33665aac8a343521ad4b67d46a6
Magnus Skjegstad <magnus@skjegstad.com>
2024-08-22 11:41:18 +0200

    tests: enable stacktrace check in exception integration test

--- test/kernel/integration/exception/test.py
+++ test/kernel/integration/exception/test.py
@@ -8,7 +8,7 @@ from vmrunner import vmrunner
 vm = vmrunner.vms[0]
 
 counter = 0
-expected = 3 # TODO: Change to 5 when we have builds with backtrace.
+expected = 5
 def is_good(line):
     global counter
     counter += 1
@@ -20,8 +20,7 @@ def is_good(line):
 vm.on_output("\\x15\\x07\\t\*\*\*\* PANIC \*\*\*\*", is_good)
 vm.on_output("Divide-by-zero Error", is_good)
 
-# TODO: Re-enable these when we have test builds with backtrace
-# vm.on_output("__cpu_exception", is_good)
-# vm.on_output("Service::start()", is_good)
+vm.on_output("__cpu_exception", is_good)
+vm.on_output("Service::start()", is_good)
 
-vm.boot(20,image_name='kernel_exception.elf.bin')
+vm.boot(20,image_name='kernel_exception')
