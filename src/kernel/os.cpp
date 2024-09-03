6dd4a3b1adbc656233cdb371b752c88750409aad
Bhavesh9908 <madhavibhavesh558@gmail.com>
2024-09-03 23:04:02 +0530

    Bhavesh9908

--- src/kernel/os.cpp
+++ src/kernel/os.cpp
@@ -1,10 +1,10 @@
-
 #include <os.hpp>
 #include <kernel.hpp>
 
 bool os::is_booted() noexcept {
   return kernel::is_booted();
 }
+
 const char* os::arch() noexcept {
   return Arch::name;
 }
@@ -17,16 +17,16 @@ void os::set_panic_action(Panic_action action) noexcept {
   kernel::set_panic_action(action);
 }
 
-os::Span_mods os::modules()
-{
+os::Span_mods os::modules() {
   auto* bootinfo_ = kernel::bootinfo();
-  if (bootinfo_ and bootinfo_->flags & MULTIBOOT_INFO_MODS and bootinfo_->mods_count) {
+  if (bootinfo_ && (bootinfo_->flags & MULTIBOOT_INFO_MODS) && bootinfo_->mods_count) {
 
     Expects(bootinfo_->mods_count < std::numeric_limits<int>::max());
 
     return os::Span_mods {
       reinterpret_cast<os::Module*>(bootinfo_->mods_addr),
-        static_cast<int>(bootinfo_->mods_count) };
+      static_cast<int>(bootinfo_->mods_count)
+    };
   }
   return {};
 }
