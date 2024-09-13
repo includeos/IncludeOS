d0a23fee749787bae7925e25a3d768e4bae47798
Magnus Skjegstad <magnus@skjegstad.com>
2024-09-13 14:14:25 +0200

    example: update to use find_package
    Use find_package to configure these variables automatically

--- example.nix
+++ example.nix
@@ -22,18 +22,5 @@ includeos.stdenv.mkDerivation rec {
     includeos
   ];
 
-  # TODO:
-  # We currently need to explicitly pass in because we link with a linker script
-  # and need to control linking order.
-  # This can be moved to os.cmake eventually, once we figure out how to expose
-  # them to cmake from nix without having to make cmake depend on nix.
-  # * Maybe we should make symlinks from the includeos package to them.
-
-  cmakeFlags = [
-    "-DINCLUDEOS_PACKAGE=${includeos}"
-    "-DARCH=x86_64"
-    "-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
-  ];
-
   version = "dev";
 }
