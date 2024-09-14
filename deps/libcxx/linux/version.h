37b2775aafd2eb6ff7bd561d677860c600ffcae5
Magnus Skjegstad <magnus@skjegstad.com>
2024-09-14 12:40:35 +0200

    Remove unused build files and scripts
    Removes conanfiles and various scripts that depend on the old build
    system, including the Dockerfile and Jenkinsfile, as well as vagrant
    setup.
    
    If we want to recreate them they have to be mostly rewritten to use the
    new build system.

--- deps/libcxx/linux/version.h
+++
@@ -1,5 +0,0 @@
-#define LINUX_VERSION_CODE 132650
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
-#define LINUX_VERSION_MAJOR 2
-#define LINUX_VERSION_PATCHLEVEL 6
-#define LINUX_VERSION_SUBLEVEL 42
