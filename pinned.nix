02a39969a8c56175882f918e61721d23295dcf3c
Mazunki Hoksaas <rolferen@gmail.com>
2025-09-23 22:57:05 +0200

    update pinned hash of nixpkgs 25.04

--- pinned.nix
+++ pinned.nix
@@ -1,6 +1,6 @@
 import (
   builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-    sha256 = "1r4fhp3apf1qggsrm60ni820gxzpm04q9xdk1w3dap9qymi6bpdk";
+    sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
   }
 )
