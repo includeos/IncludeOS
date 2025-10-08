02cbf15619cabcb2846be9789bc42f3c9c687c2a
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-08 14:33:31 +0200

    fix printf-type specifiers

--- src/kernel/multiboot.cpp
+++ src/kernel/multiboot.cpp
@@ -22,18 +22,27 @@
 #include <boot/multiboot.h>
 #include <kernel/memory.hpp>
 
+#include <format>
+template<class... Args>
+static inline void _kfmt(const char* prefix, std::format_string<Args...> fmt, Args&&... args) {
+  char buf[512];
+  auto res = std::format_to_n(buf, sizeof(buf) - 1, fmt, std::forward<Args>(args)...);
+  *res.out = '\0';
+  kprintf("%s%s", prefix, buf);
+}
+
 #define DEBUG_MULTIBOOT
 #if defined(DEBUG_MULTIBOOT)
 #undef debug
-#define debug(X,...)  kprintf(X,##__VA_ARGS__);
-#define MYINFO(X,...) kprintf("<Multiboot>" X "\n", ##__VA_ARGS__)
+#define debug(fmt, ...)  _kfmt("", fmt, ##__VA_ARGS__)
+#undef MYINFO
+#define MYINFO(fmt, ...) _kfmt("< multiboot >", fmt "\n", ##__VA_ARGS__)
 #undef INFO2
-#define INFO2(X,...) kprintf("\t" X "\n", ##__VA_ARGS__)
+#define INFO2(fmt, ...) _kfmt("\t", fmt "\n", ##__VA_ARGS__)
 #else
-#define debug(X,...)
-#define MYINFO(X,...) INFO("Kernel", X, ##__VA_ARGS__)
+#define debug(...)  ((void) 0)
+#define MYINFO(fmt, ,...) INFO("Kernel", X, ##__VA_ARGS__)
 #endif
-
 
 extern uintptr_t _end;
 
@@ -73,11 +82,11 @@ uintptr_t _multiboot_free_begin(uintptr_t boot_addr)
   auto* info = bootinfo(boot_addr);
   uintptr_t multi_end = reinterpret_cast<uintptr_t>(&_end);
 
-  debug("* Multiboot begin: 0x%lx \n", (uintptr_t)info);
+  debug("* Multiboot begin: {:#x}\n", (uintptr_t)info);
   if (info->flags & MULTIBOOT_INFO_CMDLINE
       and info->cmdline > multi_end)
   {
-    debug("* Multiboot cmdline @ 0x%x: %s \n", info->cmdline, reinterpret_cast<char*>(info->cmdline));
+    debug("* Multiboot cmdline @ 0x{:08x}: {}\n", info->cmdline, reinterpret_cast<char*>(info->cmdline));
     // We can't use a cmdline that's either insde our ELF or pre-ELF area
     Expects(info->cmdline > multi_end
             or info->cmdline < 0x100000);
@@ -89,27 +98,27 @@ uintptr_t _multiboot_free_begin(uintptr_t boot_addr)
     }
   }
 
-  debug("* Multiboot end: 0x%lx \n", multi_end);
+  debug("* Multiboot end: {:#x}\n", multi_end);
   if (info->mods_count == 0)
       return multi_end;
 
   auto* mods_list = (multiboot_module_t*) (uintptr_t) info->mods_addr;
-  debug("* Module list @ %p \n",mods_list);
+  debug("* Module list @ {}\n", static_cast<const void*>(mods_list));
 
   for (multiboot_module_t* mod = mods_list;
        mod < mods_list + info->mods_count;
        mod ++) {
 
-    debug("\t * Module @ %#x \n", mod->mod_start);
-    debug("\t * Args: %s \n ", (char*) (uintptr_t) mod->cmdline);
-    debug("\t * End: %#x \n ", mod->mod_end);
+    debug("\t * Module @ {:#x}\n", mod->mod_start);
+    debug("\t * Args: {}\n ", (char*) (uintptr_t) mod->cmdline);
+    debug("\t * End: {:#x}\n", mod->mod_end);
 
     if (mod->mod_end > multi_end)
       multi_end = mod->mod_end;
 
   }
 
-  debug("* Multiboot end: 0x%lx \n", multi_end);
+  debug("* Multiboot end: {} \n", multi_end);
   return multi_end;
 }
 
@@ -117,7 +126,7 @@ void kernel::multiboot(uint32_t boot_addr)
 {
   MYINFO("Booted with multiboot");
   auto* info = ::bootinfo(boot_addr);
-  INFO2("* Boot flags: %#x", info->flags);
+  INFO2("* Boot flags: {:#x}", info->flags);
 
   if (info->flags & MULTIBOOT_INFO_MEMORY) {
     uint32_t mem_low_start = 0;
@@ -127,11 +136,9 @@ void kernel::multiboot(uint32_t boot_addr)
     uint32_t mem_high_end = mem_high_start + (info->mem_upper * 1024) - 1;
     uint32_t mem_high_kb = info->mem_upper;
 
-    INFO2("* Valid memory (%i Kib):", mem_low_kb + mem_high_kb);
-    INFO2("  0x%08x - 0x%08x (%i Kib)",
-          mem_low_start, mem_low_end, mem_low_kb);
-    INFO2("  0x%08x - 0x%08x (%i Kib)",
-          mem_high_start, mem_high_end, mem_high_kb);
+    INFO2("* Valid memory (%i KiB):", mem_low_kb + mem_high_kb);
+    INFO2("  0x{:08x} - 0x{:08x} ({} KiB)", mem_low_start, mem_low_end, mem_low_kb);
+    INFO2("  0x{:08x} - 0x{:08x} ({} KiB)", mem_high_start, mem_high_end, mem_high_kb);
     INFO2("");
   }
   else {
@@ -140,14 +147,14 @@ void kernel::multiboot(uint32_t boot_addr)
 
   if (info->flags & MULTIBOOT_INFO_CMDLINE) {
     const auto* cmdline = (const char*) (uintptr_t) info->cmdline;
-    INFO2("* Booted with parameters @ %p: %s", cmdline, cmdline);
+    INFO2("* Booted with parameters @ {}: {}", (const void*)(uintptr_t)info->cmdline, cmdline);
     kernel::state().cmdline = std::pmr::string(cmdline).data();
   }
 
   if (info->flags & MULTIBOOT_INFO_MEM_MAP) {
-    INFO2("* Multiboot provided memory map  (%zu entries @ %p)",
+    INFO2("* Multiboot provided memory map  ({} entries @ {})",
           info->mmap_length / sizeof(multiboot_memory_map_t),
-          (void*) (uintptr_t) info->mmap_addr);
+          (const void*)(uintptr_t)info->mmap_addr);
     std::span<multiboot_memory_map_t> mmap {
         reinterpret_cast<multiboot_memory_map_t*>(info->mmap_addr),
         static_cast<size_t>(info->mmap_length / sizeof(multiboot_memory_map_t))
@@ -158,8 +165,7 @@ void kernel::multiboot(uint32_t boot_addr)
       const char* str_type = map.type & MULTIBOOT_MEMORY_AVAILABLE ? "FREE" : "RESERVED";
       const uintptr_t addr = map.addr;
       const uintptr_t size = map.len;
-      INFO2("  0x%010zx - 0x%010zx %s (%zu Kb.)",
-            addr, addr + size - 1, str_type, size / 1024 );
+      INFO2("  {:#x} - {:#x} {} ({} KiB)", addr, addr + size - 1, str_type, size / 1024);
 
       if (not (map.type & MULTIBOOT_MEMORY_AVAILABLE)) {
 
@@ -184,9 +190,9 @@ void kernel::multiboot(uint32_t boot_addr)
   auto mods = os::modules();
 
   if (not mods.empty()) {
-    MYINFO("OS loaded with %zu modules", mods.size());
+    MYINFO("OS loaded with {} modules", mods.size());
     for (auto mod : mods) {
-      INFO2("* %s @ 0x%x - 0x%x, size: %ib",
+      INFO2("* {} @ 0x{:08x} - 0x{:08x}, size: {} B",
             reinterpret_cast<char*>(mod.params),
             mod.mod_start, mod.mod_end, mod.mod_end - mod.mod_start);
     }
