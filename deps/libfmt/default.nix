40d55261d3cf1de15b642de3a7af153729eb18bd
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-14 20:30:05 +0200

    add libfmt as a dependency

---
+++ deps/libfmt/default.nix
@@ -0,0 +1,29 @@
+# note that there is also `<nixpkgs>.fmt`
+# so this entire file could just be `{ pkgs }: pkgs.fmt`
+{
+  pkgs,
+  stdenv ? pkgs.stdenv,
+  cmake ? pkgs.cmake
+}:
+stdenv.mkDerivation rec {
+  pname = "fmt";
+  version = "12.0.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "fmtlib";
+    repo  = "fmt";
+    rev   = "12.0.0";
+    hash = "sha256-AZDmIeU1HbadC+K0TIAGogvVnxt0oE9U6ocpawIgl6g=";
+  };
+
+  nativeBuildInputs = [ cmake ];
+
+  cmakeFlags = [
+    "-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY"
+    "-DBUILD_SHARED_LIBS=OFF"
+
+    "-DFMT_TEST=OFF"
+    "-DFMT_DOC=OFF"
+    "-DFMT_INSTALL=ON"
+  ];
+}
