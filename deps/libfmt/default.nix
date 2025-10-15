ed25751b1856732203c7e106f866d01f414e28a3
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-15 15:54:24 +0200

    allow editors to find libfmt declarations

--- deps/libfmt/default.nix
+++ deps/libfmt/default.nix
@@ -5,25 +5,31 @@
   stdenv ? pkgs.stdenv,
   cmake ? pkgs.cmake
 }:
-stdenv.mkDerivation rec {
-  pname = "fmt";
-  version = "12.0.0";
+let
+  libfmt = stdenv.mkDerivation rec {
+    pname = "fmt";
+    version = "12.0.0";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "fmtlib";
-    repo  = "fmt";
-    rev   = "12.0.0";
-    hash = "sha256-AZDmIeU1HbadC+K0TIAGogvVnxt0oE9U6ocpawIgl6g=";
+    src = pkgs.fetchFromGitHub {
+      owner = "fmtlib";
+      repo  = "fmt";
+      rev   = "12.0.0";
+      hash = "sha256-AZDmIeU1HbadC+K0TIAGogvVnxt0oE9U6ocpawIgl6g=";
+    };
+
+    nativeBuildInputs = [ cmake ];
+
+    cmakeFlags = [
+      "-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY"
+      "-DBUILD_SHARED_LIBS=OFF"
+
+      "-DFMT_TEST=OFF"
+      "-DFMT_DOC=OFF"
+      "-DFMT_INSTALL=ON"
+    ];
   };
-
-  nativeBuildInputs = [ cmake ];
-
-  cmakeFlags = [
-    "-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY"
-    "-DBUILD_SHARED_LIBS=OFF"
-
-    "-DFMT_TEST=OFF"
-    "-DFMT_DOC=OFF"
-    "-DFMT_INSTALL=ON"
-  ];
-}
+in
+  libfmt // {
+    include = "${libfmt}/include";
+    lib = "${libfmt}/lib";
+  }
