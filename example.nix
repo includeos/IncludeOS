0eb051a60681a703291cdbad948a41b9c6b8ae89
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-16 18:38:24 +0200

    move example unikernel building from shell.nix to example.nix

--- example.nix
+++ example.nix
@@ -1,12 +1,24 @@
-{ withCcache ? false,
+{
 
-  doCheck ? true, # boot unikernel after building it
-  includeos ? import ./default.nix { inherit withCcache; },
+  # The unikernel to build
+  unikernel ? ./example,
+
+  # Boot unikernel after building it
+  doCheck ? true,
+
+  # Enable multicore suport.
+  smp ? false,
+
+  # Enable ccache support. See overlay.nix for details.
+  withCcache ? false,
+
+  # The includeos library to build and link against
+  includeos ? import ./default.nix { inherit withCcache; inherit smp; },
 }:
 
 includeos.stdenv.mkDerivation rec {
   pname = "includeos_example";
-  src = includeos.pkgs.lib.cleanSource ./example;
+  src = includeos.pkgs.lib.cleanSource "${unikernel}";
   dontStrip = true;
   inherit doCheck;
 
@@ -20,13 +32,22 @@ includeos.stdenv.mkDerivation rec {
     includeos.chainloader
   ];
 
+  cmakeFlags = [
+    "-DARCH=x86_64"
+    "-DINCLUDEOS_PACKAGE=${includeos}"
+    "-DCMAKE_MODULE_PATH=${includeos}/cmake"
+    "-DFOR_PRODUCTION=OFF"
+  ];
+
   nativeCheckInputs = [
     includeos.vmrunner
     includeos.pkgs.qemu
   ];
 
   checkPhase = ''
+    runHook preCheck
     boot *.elf.bin
+    runHook postCheck
   '';
 
   version = "dev";
