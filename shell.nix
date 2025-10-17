f5b6605003a2098b5a1f2d28e45a9c8052a0318a
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-17 17:09:31 +0200

    clarify network privileges, expose paths to underlying tooling

--- shell.nix
+++ shell.nix
@@ -44,25 +44,44 @@ includeos.pkgs.mkShell.override { inherit (includeos) stdenv; } rec {
   ];
 
   shellHook = ''
-    echo -e "\n====================== IncludeOS nix-shell ====================="
-    echo -e "\nThe C++ compiler set to:"
-    echo $(which $CXX)
-    echo -e "\nIncludeOS package:"
-    echo ${includeos}
-    echo -e "\n---------------------- Network privileges  ---------------------"
-    echo "The vmrunner for IncludeOS tests requires bridged networking for full functionality."
-    echo "The following commands requiring sudo privileges can be used to set this up:"
-    echo "1. the qemu-bridge-helper needs sudo to create a bridge. Can be enabled with:"
-    echo "   sudo chmod u+s ${includeos.pkgs.qemu}/libexec/qemu-bridge-helper"
-    echo "2. bridge43 must exist. Can be set up with vmrunner's create_bridge.sh script:"
-    echo "   ${vmrunnerPkg.create_bridge}"
-    echo "3. /etc/qemu/bridge.conf must contain this line:"
-    echo "   allow bridge43"
-    echo ""
-    echo "Some tests require ping, which requires premissions to send raw packets. On some hosts"
-    echo "this is not enabled by default for iputils provided by nix. It can be enabled with:"
-    echo "4. sudo setcap cap_net_raw+ep ${includeos.pkgs.iputils}/bin/ping"
-    echo " "
-    echo
+    cat <<-EOF
+================================== IncludeOS nix-shell ==================================
+Packages:
+  IncludeOS:       ${includeos}
+  vmrunner:        ${vmrunnerPkg}
+  chainloader:     ${includeos.chainloader}
+
+Tooling:
+  CXX              $(command -v $CXX)
+  cmake:           $(command -v cmake)
+  nasm:            $(command -v nasm)
+  qemu-system-x86: $(command -v qemu-system-x86_64)
+  grub-mkrescue:   $(command -v grub-mkrescue)
+  xorriso:         $(command -v xorriso)
+  ping:            $(command -v ping)
+
+---------------------------------- Network privileges  ----------------------------------
+The vmrunner for IncludeOS tests requires bridged networking for full functionality.
+The following checklist can be used to set this up from the host:
+
+1. The qemu-bridge-helper needs root escalation to manipulate bridges. You can provide this
+   either through capabilities or through root execution. Pick one:
+       sudo chmod u+s ${includeos.pkgs.qemu}/libexec/qemu-bridge-helper
+       sudo setcap cap_net_admin+ep ${includeos.pkgs.qemu}/libexec/qemu-bridge-helper
+
+2. bridge43 must exist. Can be set up with vmrunner's create_bridge.sh script (not as root):
+       ${vmrunnerPkg.create_bridge}
+
+3. /etc/qemu/bridge.conf must contain this line:
+       allow bridge43
+   Also note that /etc/qemu needs specific permissions, so it might be easiest to install
+   qemu on the host to generate these directories for you, despite not using its executable here.
+
+4. Some tests also perform ICMP pings, which requires permissions to send raw packets. On some
+   hosts this is not enabled by default for iputils provided by nix.
+   It can be enabled with:
+       sudo setcap cap_net_raw+ep ${includeos.pkgs.iputils}/bin/ping
+
+EOF
   '';
 }
