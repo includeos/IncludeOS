1edeba9c022251830d1fd807871edadae3fb1281
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-28 19:35:21 +0100

    prefer using std::format over snprintf

--- src/kernel/elf.cpp
+++ src/kernel/elf.cpp
@@ -61,12 +61,9 @@ static const uintptr_t ELF_START = reinterpret_cast<uintptr_t>(&_ELF_START_);
 extern "C" char *
 __cxa_demangle(const char *name, char *buf, size_t *n, int *status);
 
-template <typename N>
-static std::string to_hex_string(N n)
-{
-  char buffer[16];
-  int len = snprintf(buffer, sizeof(buffer), "%#x", n);
-  return std::string(buffer, len);
+template <std::integral N>
+[[nodiscard]] static std::string to_hex_string(N n) {
+  return std::format("{:#x}", n);
 }
 
 static ElfEhdr& elf_header() {
