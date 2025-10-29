46bd0da51b3dc8469098c370d8c6816ef70410bb
Mazunki Hoksaas <rolferen@gmail.com>
2025-10-29 11:22:06 +0100

    consistent let..in as a preemptive measure

--- deps/lest/default.nix
+++ deps/lest/default.nix
@@ -2,27 +2,34 @@
   pkgs,
   stdenv
 }:
-stdenv.mkDerivation rec {
-  pname = "lest";
-  version = "1.36.0";
+let
+  self = stdenv.mkDerivation rec {
+    pname = "lest";
+    version = "1.36.0";
 
-  meta = {
-    description = "A tiny C++11 test framework – lest errors escape testing.";
-    homepage = "https://github.com/martinmoene/lest";
-    license = pkgs.lib.licenses.boost;
+    meta = {
+      description = "A tiny C++11 test framework – lest errors escape testing.";
+      homepage = "https://github.com/martinmoene/lest";
+      license = pkgs.lib.licenses.boost;
+    };
+
+    src = fetchGit {
+      url = "https://github.com/martinmoene/lest.git";
+      ref = "refs/tags/v${version}";
+      rev = "57197f32f2c7d3f3d3664a9010d3ff181a40f6ca";
+    };
+
+    cmakeBuildType = "Debug";
+
+    postBuild = ''
+        mkdir -p "$out/include"
+        cp -r include "$out/"
+      '';
   };
 
-  src = fetchGit {
-    url = "https://github.com/martinmoene/lest.git";
-    ref = "refs/tags/v${version}";
-    rev = "57197f32f2c7d3f3d3664a9010d3ff181a40f6ca";
-  };
+  dev = pkgs.lib.getDev self;
+  lib = pkgs.lib.getLib self;
+in
+  self.overrideAttrs (oldAttrs: {
+  })
 
-  cmakeBuildType = "Debug";
-
-  postBuild = ''
-      mkdir -p "$out/include"
-      cp -r include "$out/"
-    '';
-
-}
